9c944a77b72be23a866d62eb4cee5e1656de373a
Yan <yans@yancomm.net>
2014-07-07 16:52:44 -0700

    combined expressions into one class

--- claripy/expressions/expression.py
+++ claripy/expressions/expression.py
@@ -10,38 +10,47 @@ class Expression(object):
 	A base class to wrap Z3 objects.
 	'''
 
-	def __init__(self, variables, symbolic, uuid):
-		self._uuid = uuid
+	def __init__(self, backends, variables=None, symbolic=None, uuid=None, obj=None):
+		if (uuid is None) and (variables is None or symbolic is None and obj is None):
+			raise ValueError("invalid arguments passed to Expression()")
+
 		self.variables = variables
 		self.symbolic = symbolic
 
+		self._uuid = uuid
+		self._obj = obj
+		self._backends = backends
+
+	def __repr__(self):
+		return "<Expression uuid=%s obj=%s>" % (self._uuid, self._obj)
+
 	def _do_op(self, op_name, args):
-		raise NotImplementedError("_do_op must be overloaded.")
+		for b in self._backends:
+			try:
+				return b.call(op_name, (self,)+args)
+			except BackendError:
+				continue
 
-	@staticmethod
-	def _op_args(backend, op_name, args, obj=None):
-		'''
-		Returns the function object (representing the operation) and the
-		arguments to pass to that function object.
-		'''
+		raise Exception("no backend can handle operation %s", op_name)
 
-		if '__' in op_name:
-			if obj is None:
-				op = getattr(args[0], op_name)
-				op_args = args[1:]
-			else:
-				op = getattr(obj, op_name)
-				op_args = args
-		else:
-			if obj is None:
-				op = backend.op(op_name)
-				op_args = args
-			else:
-				op = backend.op(op_name)
-				op_args = [ obj ] + args
+	def actualize(self, new_backends=None):
+		if not isinstance(self._obj, AbstractCall):
+			raise Exception("actualize() called with a non-abstract obj")
 
-		return op, op_args
+		if new_backends is not None:
+			self._backends = new_backends
 
+		for b in self._backends:
+			try:
+				r = self._obj.apply(b)
+				self._obj = r._obj
+				self.variables = r.variables
+				self.symbolic = r.symbolic
+				return
+			except BackendError:
+				continue
+
+		raise Exception("actualization failed with available backends")
 
 #
 # Wrap stuff
@@ -66,3 +75,6 @@ def make_methods(cls):
 	for name in ops:
 		wrap_operator(cls, name)
 make_methods(Expression)
+
+from ..backends.backend import BackendError
+from ..backends.abstract_call import AbstractCall
