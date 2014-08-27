ce649d984784fba5b317439e1b7517b569a9a174
Yan <yans@yancomm.net>
2014-08-27 15:35:59 -0700

    results serialization

--- claripy/result.py
+++ claripy/result.py
@@ -15,6 +15,13 @@ class Result(object):
         r.min_cache = dict(self.min_cache)
         r.max_cache = dict(self.max_cache)
 
+    def __getstate__(self):
+        return ( self.sat, self.model, self.eval_cache, self.min_cache, self.max_cache )
+
+    def __setstate__(self, state):
+        ( self.sat, self.model, self.eval_cache, self.min_cache, self.max_cache ) = state
+        self.backend_model = None
+
 class UnsatError(Exception):
     pass
 
