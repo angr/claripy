8dd03558f2ba149e731623b45d3f33af0d90f073
Yan <yans@yancomm.net>
2014-09-14 18:30:39 -0700

    create empty resolution cache on Result deserialization

--- claripy/result.py
+++ claripy/result.py
@@ -23,4 +23,5 @@ class Result(object):
 
     def __setstate__(self, state):
         ( self.sat, self.model, self.eval_cache, self.min_cache, self.max_cache ) = state
+        self.resolve_cache = collections.defaultdict(weakref.WeakKeyDictionary)
         self.backend_model = None
