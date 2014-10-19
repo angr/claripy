8f264fbdf98e502d9b739fd4e24a2fceea3bbf85
Fish <fish@cs.ucsb.edu>
2014-10-19 01:46:33 -0700

    Implemented union() operation for VS. Create unique names for VS.

--- claripy/vsa/valueset.py
+++ claripy/vsa/valueset.py
@@ -1,4 +1,5 @@
 import functools
+import itertools
 
 def normalize_types(f):
     @functools.wraps(f)
@@ -15,37 +16,51 @@ def normalize_types(f):
 
     return normalizer
 
+vs_id_ctr = itertools.count()
+
 class ValueSet(object):
-    def __init__(self, region=None, bits=None, val=None):
-        self._si_dict = {}
+    def __init__(self, name=None, region=None, bits=None, val=None):
+        self._name = name
+        if self._name is None:
+            self._name = 'VS_%d' % vs_id_ctr.next()
+
+        self._regions = {}
 
         self._reversed = False
 
         if region is not None and bits is not None and val is not None:
             self.set_si(region, StridedInterval(bits=bits, stride=0, lower_bound=val, upper_bound=val))
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def regions(self):
+        return self._regions
+
     @normalize_types
     def set_si(self, region, si):
-        self._si_dict[region] = si
+        self._regions[region] = si
 
     def get_si(self, region):
-        if region in self._si_dict:
-            return self._si_dict[region]
+        if region in self._regions:
+            return self._regions[region]
         # TODO: Should we return a None, or an empty SI instead?
         return None
 
     def items(self):
-        return self._si_dict.items()
+        return self._regions.items()
 
     def size(self):
         return len(self)
 
     @normalize_types
     def merge_si(self, region, si):
-        if region not in self._si_dict:
+        if region not in self._regions:
             self.set_si(region, si)
         else:
-            self._si_dict[region] = self._si_dict[region].union(si)
+            self._regions[region] = self._regions[region].union(si)
 
     @normalize_types
     def remove_si(self, region, si):
@@ -53,22 +68,22 @@ class ValueSet(object):
 
     def __repr__(self):
         s = ""
-        for region, si in self._si_dict.items():
+        for region, si in self._regions.items():
             s = "%s: %s" % (region, si)
         return "(" + s + ")"
 
     def __len__(self):
         if self.is_empty():
             return 0
-        return len(self._si_dict.items()[0][1])
+        return len(self._regions.items()[0][1])
 
     def __add__(self, other):
         if type(other) is ValueSet:
             raise NotImplementedError()
         else:
             new_vs = ValueSet()
-            for region, si in self._si_dict.items():
-                new_vs._si_dict[region] = si + other
+            for region, si in self._regions.items():
+                new_vs._regions[region] = si + other
 
             return new_vs
 
@@ -77,8 +92,8 @@ class ValueSet(object):
             raise NotImplementedError()
         else:
             new_vs = ValueSet()
-            for region, si in self._si_dict.items():
-                new_vs._si_dict[region] = si - other
+            for region, si in self._regions.items():
+                new_vs._regions[region] = si - other
 
             return new_vs
 
@@ -88,15 +103,15 @@ class ValueSet(object):
             assert False
 
         new_vs = ValueSet()
-        for region, si in self._si_dict.items():
-            new_vs._si_dict[region] = si.__and__(other)
+        for region, si in self._regions.items():
+            new_vs._regions[region] = si.__and__(other)
 
         return new_vs
 
     def eval(self, n):
         results = []
 
-        for _, si in self._si_dict.items():
+        for _, si in self._regions.items():
             if len(results) < n:
                 results.extend(si.eval(n))
 
@@ -104,24 +119,24 @@ class ValueSet(object):
 
     def copy(self):
         vs = ValueSet()
-        vs._si_dict = self._si_dict.copy()
+        vs._regions = self._regions.copy()
         vs._reversed = self._reversed
 
         return vs
 
     def reverse(self):
+        print "valueset.reverse is not properly implemented"
         vs = self.copy()
-
         vs._reversed = not vs._reversed
 
         return vs
 
     def is_empty(self):
-        return len(self._si_dict) == 0
+        return len(self._regions) == 0
 
     def extract(self, high_bit, low_bit):
         new_vs = ValueSet()
-        for region, si in self._si_dict.items():
+        for region, si in self._regions.items():
             new_vs.set_si(region, si.extract(high_bit, low_bit))
 
         return new_vs
@@ -129,15 +144,20 @@ class ValueSet(object):
     def concat(self, b):
         new_vs = ValueSet()
         # TODO: This logic is obviously flawed. Correct it later :-(
-        for region, si in self._si_dict.items():
+        for region, si in self._regions.items():
             new_vs.set_si(region, si.concat(b.get_si(region)))
 
         return new_vs
 
-    def reverse(self):
-        #import ipdb; ipdb.set_trace()
-        print "valueset.reverse is not implemented"
-        return self
+    def union(self, b):
+        merged_vs = self.copy()
+        for region, si in b.regions.items():
+            if region not in merged_vs._regions:
+                merged_vs._regions[region] = si
+            else:
+                merged_vs._regions[region] = merged_vs._regions[region].union(si)
+
+        return merged_vs
 
 from ..ast import A
 from .strided_interval import StridedInterval
