4fe844bfe564133b31d2d3225712fe0854d7c693
Andrew Dutcher <andrew@andrewdutcher.com>
2015-07-01 19:22:53 -0700

    Refactor tests for new system

---
+++ tests/test_simplify.py
@@ -0,0 +1,30 @@
+import claripy
+import nose
+
+def test_simplification():
+    clrp = claripy.Claripies['SerialZ3']
+
+    def assert_identical(a, b):
+        nose.tools.assert_true(a.identical(b))
+
+    x, y, z = (clrp.BV(name, 32) for name in ('x', 'y', 'z'))
+
+    # test extraction of concatted values
+    concatted = clrp.Concat(x, y, z)
+
+    assert_identical(concatted[95:64].reduced, x)
+    assert_identical(concatted[63:32].reduced, y)
+    assert_identical(concatted[31:0].reduced, z)
+
+    assert_identical(concatted[95:32].reduced, clrp.Concat(x, y))
+    assert_identical(concatted[63:0].reduced, clrp.Concat(y, z))
+
+    assert_identical(concatted[95:0].reduced, concatted)
+
+    assert_identical(concatted[47:0].reduced, clrp.Concat(y, z)[47:0])
+    assert_identical(concatted[70:0].reduced, concatted[70:0])
+    assert_identical(concatted[70:15].reduced, concatted[70:15])
+    assert_identical(concatted[70:35].reduced, clrp.Concat(x, y)[38:3])
+
+if __name__ == '__main__':
+    test_simplification()
