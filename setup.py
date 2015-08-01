9e789cddfaa527524b7372cfbab4b188eb13b6d0
Yan <yans@yancomm.net>
2015-07-31 18:41:30 -0700

    update setup script

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from distutils.core import setup
-setup(name='claripy', version='0.01', packages=['claripy', 'claripy.backends', 'claripy.solvers', 'claripy.vsa'])
+setup(name='claripy', version='0.01', packages=['claripy', 'claripy.backends', 'claripy.frontends', 'claripy.vsa', 'claripy.ast'])
