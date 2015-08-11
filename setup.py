2dff39a9acef7dab1aa142763d68e03024dee19d
Yan <yans@yancomm.net>
2015-08-11 02:06:23 -0700

    merge

--- setup.py
+++ setup.py
@@ -6,10 +6,6 @@ setup(
     packages=['claripy', 'claripy.backends', 'claripy.frontends', 'claripy.vsa', 'claripy.ast'],
     install_requires=[
         'ana',
-        'z3',
-    ],
-    dependency_links=[
-        'git+https://github.com/zardus/ana.git#egg=ana-0.1',
-        'https://github.com/zardus/z3/archive/pypy-and-setup.zip#egg=z3-0.1',
+        'angr-z3',
     ],
 )
