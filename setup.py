4f76f3a6b74a730cba44679626d59c5c9140c827
Yan <yans@yancomm.net>
2015-08-11 17:05:14 -0700

    bump version for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='claripy',
-    version='0.9.0',
+    version='0.9.0.1',
     packages=['claripy', 'claripy.backends', 'claripy.frontends', 'claripy.vsa', 'claripy.ast'],
     install_requires=[
         'ana',
