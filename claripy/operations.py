60e2bc45a206b44d209194e59fcd510ace042c14
Fish <fish@cs.ucsb.edu>
2015-08-14 19:19:59 -0700

    Reenable eq/ne_simplifier()

--- claripy/operations.py
+++ claripy/operations.py
@@ -374,8 +374,8 @@ simplifiers = {
     'If': if_simplifier,
     '__lshift__': lshift_simplifier,
     '__rshift__': rshift_simplifier,
-    #'__eq__': eq_simplifier,
-    #'__ne__': ne_simplifier,
+    '__eq__': eq_simplifier,
+    '__ne__': ne_simplifier,
 }
 
 #
