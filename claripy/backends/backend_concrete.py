3d92e6882dbccfd70508965f234a1e83bc948e3a
Yan <yans@yancomm.net>
2015-08-15 02:07:47 -0700

    implement support for concrete If operations when the arguments are all booleans

--- claripy/backends/backend_concrete.py
+++ claripy/backends/backend_concrete.py
@@ -8,13 +8,14 @@ from ..backend import BackendError, Backend
 class BackendConcrete(Backend):
     def __init__(self):
         Backend.__init__(self)
-        self._make_raw_ops(set(backend_operations) - { 'BitVec' }, op_module=bv)
+        self._make_raw_ops(set(backend_operations) - { 'BitVec', 'If' }, op_module=bv)
         self._make_raw_ops(backend_fp_operations, op_module=fp)
-        self._op_raw_result['BitVec'] = self.BitVec
+        self._op_raw_result['BitVec'] = self._BitVec
+        self._op_raw_result['If'] = self._If
         self._z_true = z3.BoolVal(True)
         self._z_false = z3.BoolVal(False)
 
-    def BitVec(self, name, size, result=None): #pylint:disable=W0613,R0201
+    def _BitVec(self, name, size, result=None): #pylint:disable=W0613,R0201
         if result is None:
             l.debug("BackendConcrete can only handle BitVec when we are given a model")
             raise BackendError("BackendConcrete can only handle BitVec when we are given a model")
@@ -23,6 +24,12 @@ class BackendConcrete(Backend):
             raise BackendError("BackendConcrete needs variable %s in the model" % name)
         else:
             return result.model[name]
+
+    def _If(self, b, t, f, result=None): #pylint:disable=no-self-use,unused-argument
+        if not isinstance(b, bool):
+            raise BackendError("BackendConcrete can't handle non-bool condition in If.")
+        else:
+            return t if b else f
 
     def _size(self, e, result=None):
         if type(e) in { bool, long, int }:
