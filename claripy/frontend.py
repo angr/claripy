63e9795173d02445429dbc81b7eead6863e84312
Yan <yans@yancomm.net>
2015-09-04 15:08:53 -0700

    detab :-(

--- claripy/frontend.py
+++ claripy/frontend.py
@@ -8,283 +8,283 @@ import ana
 #pylint:disable=unidiomatic-typecheck
 
 class Frontend(ana.Storable):
-	def __init__(self, solver_backend):
-		self._solver_backend = solver_backend
-		self.result = None
-		self._simplified = False
+    def __init__(self, solver_backend):
+        self._solver_backend = solver_backend
+        self.result = None
+        self._simplified = False
 
-	#
-	# Storable support
-	#
+    #
+    # Storable support
+    #
 
-	@property
-	def uuid(self):
-		return self.ana_uuid
+    @property
+    def uuid(self):
+        return self.ana_uuid
 
-	def _ana_getstate(self):
-		if not self._simplified: self.simplify()
-		return self._solver_backend.__class__.__name__, self.result
+    def _ana_getstate(self):
+        if not self._simplified: self.simplify()
+        return self._solver_backend.__class__.__name__, self.result
 
-	def _ana_setstate(self, s):
-		solver_backend_name, self.result = s
-		self._solver_backend = _backends[solver_backend_name]
-		self._simplified = True
+    def _ana_setstate(self, s):
+        solver_backend_name, self.result = s
+        self._solver_backend = _backends[solver_backend_name]
+        self._simplified = True
 
-	#
-	# Constraint management
-	#
+    #
+    # Constraint management
+    #
 
-	@staticmethod
-	def _split_constraints(constraints):
-		'''
-		Returns independent constraints, split from this Frontend's constraints.
-		'''
+    @staticmethod
+    def _split_constraints(constraints):
+        '''
+        Returns independent constraints, split from this Frontend's constraints.
+        '''
 
-		splitted = [ ]
-		for i in constraints:
-			splitted.extend(i.split(['And']))
+        splitted = [ ]
+        for i in constraints:
+            splitted.extend(i.split(['And']))
 
-		l.debug("... splitted of size %d", len(splitted))
+        l.debug("... splitted of size %d", len(splitted))
 
-		variable_connections = { }
-		constraint_connections = { }
-		for n,s in enumerate(splitted):
-			l.debug("... processing constraint with %d variables", len(s.variables))
+        variable_connections = { }
+        constraint_connections = { }
+        for n,s in enumerate(splitted):
+            l.debug("... processing constraint with %d variables", len(s.variables))
 
-			connected_variables = set(s.variables)
-			connected_constraints = { n }
+            connected_variables = set(s.variables)
+            connected_constraints = { n }
 
-			if len(connected_variables) == 0:
-				connected_variables.add('CONCRETE')
+            if len(connected_variables) == 0:
+                connected_variables.add('CONCRETE')
 
-			for v in s.variables:
-				if v in variable_connections:
-					connected_variables |= variable_connections[v]
-				if v in constraint_connections:
-					connected_constraints |= constraint_connections[v]
+            for v in s.variables:
+                if v in variable_connections:
+                    connected_variables |= variable_connections[v]
+                if v in constraint_connections:
+                    connected_constraints |= constraint_connections[v]
 
-			for v in connected_variables:
-				variable_connections[v] = connected_variables
-				constraint_connections[v] = connected_constraints
+            for v in connected_variables:
+                variable_connections[v] = connected_variables
+                constraint_connections[v] = connected_constraints
 
-		unique_constraint_sets = set()
-		for v in variable_connections:
-			unique_constraint_sets.add((frozenset(variable_connections[v]), frozenset(constraint_connections[v])))
+        unique_constraint_sets = set()
+        for v in variable_connections:
+            unique_constraint_sets.add((frozenset(variable_connections[v]), frozenset(constraint_connections[v])))
 
-		results = [ ]
-		for v,c_indexes in unique_constraint_sets:
-			results.append((set(v), [ splitted[c] for c in c_indexes ]))
-		return results
+        results = [ ]
+        for v,c_indexes in unique_constraint_sets:
+            results.append((set(v), [ splitted[c] for c in c_indexes ]))
+        return results
 
-	def _constraint_filter(self, ec):
-		fc = [ ]
-		for e in ec if type(ec) in (list, tuple, set) else (ec,):
-			#e_simp = self._claripy.simplify(e)
-			e_simp = e
-			for b in _eager_backends + [ self._solver_backend ]:
-				try:
-					o = b.convert(e_simp)
-					if b._is_false(o):
-						#filter_false += 1
-						raise UnsatError("expressions contain False")
-					elif b._has_true(o):
-						#filter_true +=1
-						break
-					else:
-						l.warning("Frontend._constraint_filter got non-boolean from model_backend")
-						raise ClaripyFrontendError()
-				except BackendError:
-					pass
-			else:
-				fc.append(e_simp)
+    def _constraint_filter(self, ec):
+        fc = [ ]
+        for e in ec if type(ec) in (list, tuple, set) else (ec,):
+            #e_simp = self._claripy.simplify(e)
+            e_simp = e
+            for b in _eager_backends + [ self._solver_backend ]:
+                try:
+                    o = b.convert(e_simp)
+                    if b._is_false(o):
+                        #filter_false += 1
+                        raise UnsatError("expressions contain False")
+                    elif b._has_true(o):
+                        #filter_true +=1
+                        break
+                    else:
+                        l.warning("Frontend._constraint_filter got non-boolean from model_backend")
+                        raise ClaripyFrontendError()
+                except BackendError:
+                    pass
+            else:
+                fc.append(e_simp)
 
-		return tuple(fc)
+        return tuple(fc)
 
-	def branch(self):
-		s = self.__class__(self._solver_backend)
-		s.result = self.result
-		s._simplified = self._simplified
-		return s
+    def branch(self):
+        s = self.__class__(self._solver_backend)
+        s.result = self.result
+        s._simplified = self._simplified
+        return s
 
-	#
-	# Stuff that should be implemented by subclasses
-	#
+    #
+    # Stuff that should be implemented by subclasses
+    #
 
-	def _add_constraints(self, constraints, invalidate_cache=True):
-		raise NotImplementedError("_add_constraints() is not implemented")
+    def _add_constraints(self, constraints, invalidate_cache=True):
+        raise NotImplementedError("_add_constraints() is not implemented")
 
-	def _simplify(self):
-		raise NotImplementedError("_simplify() is not implemented")
+    def _simplify(self):
+        raise NotImplementedError("_simplify() is not implemented")
 
-	def _solve(self, extra_constraints=()):
-		raise NotImplementedError("_solve() is not implemented")
+    def _solve(self, extra_constraints=()):
+        raise NotImplementedError("_solve() is not implemented")
 
-	def _eval(self, e, n, extra_constraints=()):
-		raise NotImplementedError("_eval() is not implemented")
+    def _eval(self, e, n, extra_constraints=()):
+        raise NotImplementedError("_eval() is not implemented")
 
-	def _min(self, e, extra_constraints=()):
-		raise NotImplementedError("_min() is not implemented")
+    def _min(self, e, extra_constraints=()):
+        raise NotImplementedError("_min() is not implemented")
 
-	def _max(self, e, extra_constraints=()):
-		raise NotImplementedError("_max() is not implemented")
+    def _max(self, e, extra_constraints=()):
+        raise NotImplementedError("_max() is not implemented")
 
-	def _solution(self, e, v, extra_constraints=()):
-		raise NotImplementedError("_solution() is not implemented")
+    def _solution(self, e, v, extra_constraints=()):
+        raise NotImplementedError("_solution() is not implemented")
 
-	def finalize(self):
-		raise NotImplementedError("finalize() is not implemented")
+    def finalize(self):
+        raise NotImplementedError("finalize() is not implemented")
 
-	def merge(self, others, merge_flag, merge_values):
-		raise NotImplementedError("merge() is not implemented")
+    def merge(self, others, merge_flag, merge_values):
+        raise NotImplementedError("merge() is not implemented")
 
-	def combine(self, others):
-		raise NotImplementedError("combine() is not implemented")
+    def combine(self, others):
+        raise NotImplementedError("combine() is not implemented")
 
-	def split(self):
-		raise NotImplementedError("split() is not implemented")
+    def split(self):
+        raise NotImplementedError("split() is not implemented")
 
-	#
-	# Solving
-	#
+    #
+    # Solving
+    #
 
-	def add(self, constraints, invalidate_cache=True):
-		if type(constraints) not in (list, tuple):
-			constraints = [ constraints ]
+    def add(self, constraints, invalidate_cache=True):
+        if type(constraints) not in (list, tuple):
+            constraints = [ constraints ]
 
-		if len(constraints) == 0:
-			return [ ]
+        if len(constraints) == 0:
+            return [ ]
 
-		try:
-			to_add = self._constraint_filter(constraints)
-		except UnsatError:
-			self.result = UnsatResult()
-			to_add = [ false ]
+        try:
+            to_add = self._constraint_filter(constraints)
+        except UnsatError:
+            self.result = UnsatResult()
+            to_add = [ false ]
 
-		for c in to_add:
-			c.make_uuid()
-			if not isinstance(c, Bool):
-				raise ClaripyTypeError('constraint is not a boolean expression!')
+        for c in to_add:
+            c.make_uuid()
+            if not isinstance(c, Bool):
+                raise ClaripyTypeError('constraint is not a boolean expression!')
 
-		if self.result is not None and invalidate_cache:
-			all_true = True
-			for c in to_add:
-				try:
-					v = LightFrontend._eval.im_func(self, c, 1)[0]
-					all_true &= v
-				except ClaripyFrontendError:
-					all_true = False
-					break
-		else:
-			all_true = False
+        if self.result is not None and invalidate_cache:
+            all_true = True
+            for c in to_add:
+                try:
+                    v = LightFrontend._eval.im_func(self, c, 1)[0]
+                    all_true &= v
+                except ClaripyFrontendError:
+                    all_true = False
+                    break
+        else:
+            all_true = False
 
-		self._add_constraints(to_add, invalidate_cache=invalidate_cache)
-		self._simplified = False
+        self._add_constraints(to_add, invalidate_cache=invalidate_cache)
+        self._simplified = False
 
-		if invalidate_cache and self.result is not None and self.result.sat:
-			if all_true:
-				new_result = SatResult()
-				new_result.model.update(self.result.model)
-				self.result = new_result
-			else:
-				self.result = None
+        if invalidate_cache and self.result is not None and self.result.sat:
+            if all_true:
+                new_result = SatResult()
+                new_result.model.update(self.result.model)
+                self.result = new_result
+            else:
+                self.result = None
 
-		return to_add
+        return to_add
 
-	def simplify(self):
-		if self._simplified:
-			return
+    def simplify(self):
+        if self._simplified:
+            return
 
-		s = self._simplify()
-		self._simplified = True
-		return s
+        s = self._simplify()
+        self._simplified = True
+        return s
 
-	def solve(self, extra_constraints=()):
-		l.debug("%s.solve() running with %d extra constraints...", self.__class__.__name__, len(extra_constraints))
+    def solve(self, extra_constraints=()):
+        l.debug("%s.solve() running with %d extra constraints...", self.__class__.__name__, len(extra_constraints))
 
-		if self.result is not None:
-			if not self.result.sat or len(extra_constraints) == 0:
-				l.debug("... returning cached result (sat: %s)", self.result.sat)
-				return self.result
-		else:
-			l.debug("... no cached result")
+        if self.result is not None:
+            if not self.result.sat or len(extra_constraints) == 0:
+                l.debug("... returning cached result (sat: %s)", self.result.sat)
+                return self.result
+        else:
+            l.debug("... no cached result")
 
-		try:
-			extra_constraints = self._constraint_filter(extra_constraints)
-		except UnsatError:
-			l.debug("... returning unsat result due to false extra_constraints")
-			return UnsatResult()
+        try:
+            extra_constraints = self._constraint_filter(extra_constraints)
+        except UnsatError:
+            l.debug("... returning unsat result due to false extra_constraints")
+            return UnsatResult()
 
-		l.debug("... conferring with the solver")
-		r = self._solve(extra_constraints=extra_constraints)
-		if len(extra_constraints) == 0 or (self.result is None and r.sat):
-			l.debug("... caching result (sat: %s)", r.sat)
-			self.result = r
-		return r
+        l.debug("... conferring with the solver")
+        r = self._solve(extra_constraints=extra_constraints)
+        if len(extra_constraints) == 0 or (self.result is None and r.sat):
+            l.debug("... caching result (sat: %s)", r.sat)
+            self.result = r
+        return r
 
-	def satisfiable(self, extra_constraints=()):
-		return self.solve(extra_constraints=extra_constraints).sat
+    def satisfiable(self, extra_constraints=()):
+        return self.solve(extra_constraints=extra_constraints).sat
 
-	def eval(self, e, n, extra_constraints=()):
-		extra_constraints = self._constraint_filter(extra_constraints)
+    def eval(self, e, n, extra_constraints=()):
+        extra_constraints = self._constraint_filter(extra_constraints)
 
-		if not isinstance(e, Base):
-			raise ValueError("Expressions passed to eval() MUST be Claripy ASTs (got %s)" % type(e))
+        if not isinstance(e, Base):
+            raise ValueError("Expressions passed to eval() MUST be Claripy ASTs (got %s)" % type(e))
 
-		return self._eval(e, n, extra_constraints=extra_constraints)
+        return self._eval(e, n, extra_constraints=extra_constraints)
 
-	def max(self, e, extra_constraints=()):
-		extra_constraints = self._constraint_filter(extra_constraints)
+    def max(self, e, extra_constraints=()):
+        extra_constraints = self._constraint_filter(extra_constraints)
 
-		if not isinstance(e, Base):
-			raise ValueError("Expressions passed to max() MUST be Claripy ASTs (got %s)" % type(e))
+        if not isinstance(e, Base):
+            raise ValueError("Expressions passed to max() MUST be Claripy ASTs (got %s)" % type(e))
 
-		if len(extra_constraints) == 0 and self.result is not None and e.uuid in self.result.max_cache:
-			#cached_max += 1
-			return self.result.max_cache[e.uuid]
+        if len(extra_constraints) == 0 and self.result is not None and e.uuid in self.result.max_cache:
+            #cached_max += 1
+            return self.result.max_cache[e.uuid]
 
-		m = self._max(e, extra_constraints=extra_constraints)
-		if len(extra_constraints) == 0:
-			if self.result is not None: self.result.max_cache[e.uuid] = m
-			self.add([ULE(e, m)], invalidate_cache=False)
-		return m
+        m = self._max(e, extra_constraints=extra_constraints)
+        if len(extra_constraints) == 0:
+            if self.result is not None: self.result.max_cache[e.uuid] = m
+            self.add([ULE(e, m)], invalidate_cache=False)
+        return m
 
-	def min(self, e, extra_constraints=()):
-		extra_constraints = self._constraint_filter(extra_constraints)
+    def min(self, e, extra_constraints=()):
+        extra_constraints = self._constraint_filter(extra_constraints)
 
-		if not isinstance(e, Base):
-			raise ValueError("Expressions passed to min() MUST be Claripy ASTs (got %s)" % type(e))
+        if not isinstance(e, Base):
+            raise ValueError("Expressions passed to min() MUST be Claripy ASTs (got %s)" % type(e))
 
-		if len(extra_constraints) == 0 and self.result is not None and e.uuid in self.result.min_cache:
-			#cached_min += 1
-			return self.result.min_cache[e.uuid]
+        if len(extra_constraints) == 0 and self.result is not None and e.uuid in self.result.min_cache:
+            #cached_min += 1
+            return self.result.min_cache[e.uuid]
 
-		m = self._min(e, extra_constraints=extra_constraints)
-		if len(extra_constraints) == 0:
-			if self.result is not None: self.result.min_cache[e.uuid] = m
-			self.add([UGE(e, m)], invalidate_cache=False)
-		return m
+        m = self._min(e, extra_constraints=extra_constraints)
+        if len(extra_constraints) == 0:
+            if self.result is not None: self.result.min_cache[e.uuid] = m
+            self.add([UGE(e, m)], invalidate_cache=False)
+        return m
 
-	def solution(self, e, v, extra_constraints=()):
-		try:
-			extra_constraints = self._constraint_filter(extra_constraints)
-		except UnsatError:
-			return False
+    def solution(self, e, v, extra_constraints=()):
+        try:
+            extra_constraints = self._constraint_filter(extra_constraints)
+        except UnsatError:
+            return False
 
-		if not isinstance(e, Base):
-			raise ValueError("Expressions passed to solution() MUST be Claripy ASTs (got %s)" % type(e))
+        if not isinstance(e, Base):
+            raise ValueError("Expressions passed to solution() MUST be Claripy ASTs (got %s)" % type(e))
 
-		b = self._solution(e, v, extra_constraints=extra_constraints)
-		if b is False and len(extra_constraints) > 0:
-			self.add([e != v], invalidate_cache=False)
-		return b
+        b = self._solution(e, v, extra_constraints=extra_constraints)
+        if b is False and len(extra_constraints) > 0:
+            self.add([e != v], invalidate_cache=False)
+        return b
 
-	#
-	# Serialization and such.
-	#
+    #
+    # Serialization and such.
+    #
 
-	def downsize(self): #pylint:disable=R0201
-		if self.result is not None:
-			self.result.downsize()
+    def downsize(self): #pylint:disable=R0201
+        if self.result is not None:
+            self.result.downsize()
 
 from .frontends import LightFrontend
 from .result import UnsatResult, SatResult
--- claripy/frontends/light_frontend.py
+++ claripy/frontends/light_frontend.py
@@ -6,154 +6,154 @@ l = logging.getLogger("claripy.frontends.light_frontend")
 from ..frontend import Frontend
 
 class LightFrontend(Frontend):
-	def __init__(self, solver_backend):
-		Frontend.__init__(self, solver_backend)
-		self.constraints = [ ]
-		self.variables = set()
-		self._finalized = False
+    def __init__(self, solver_backend):
+        Frontend.__init__(self, solver_backend)
+        self.constraints = [ ]
+        self.variables = set()
+        self._finalized = False
 
-	#
-	# Storable support
-	#
+    #
+    # Storable support
+    #
 
-	def _ana_getstate(self):
-		if not self._simplified: self.simplify()
-		self.finalize()
-		return self.constraints, self.variables, Frontend._ana_getstate(self)
+    def _ana_getstate(self):
+        if not self._simplified: self.simplify()
+        self.finalize()
+        return self.constraints, self.variables, Frontend._ana_getstate(self)
 
-	def _ana_setstate(self, s):
-		self.constraints, self.variables, base_state = s
-		Frontend._ana_setstate(self, base_state)
-		self._finalized = True
+    def _ana_setstate(self, s):
+        self.constraints, self.variables, base_state = s
+        Frontend._ana_setstate(self, base_state)
+        self._finalized = True
 
-	#
-	# Constraint management
-	#
+    #
+    # Constraint management
+    #
 
-	def independent_constraints(self):
-		return self._split_constraints(self.constraints)
+    def independent_constraints(self):
+        return self._split_constraints(self.constraints)
 
-	#
-	# Light functionality
-	#
+    #
+    # Light functionality
+    #
 
-	def _add_constraints(self, constraints, invalidate_cache=True):
-		self.constraints += constraints
-		for c in constraints:
-			self.variables.update(c.variables)
-		return constraints
+    def _add_constraints(self, constraints, invalidate_cache=True):
+        self.constraints += constraints
+        for c in constraints:
+            self.variables.update(c.variables)
+        return constraints
 
 
-	def _simplify(self):
-		if len(self.constraints) == 0:
-			return
+    def _simplify(self):
+        if len(self.constraints) == 0:
+            return
 
-		self.constraints = [ simplify(And(*self.constraints)) ]
+        self.constraints = [ simplify(And(*self.constraints)) ]
 
-		# generate UUIDs for every constraint
-		for c in self.constraints:
-			if isinstance(c, Base): c.make_uuid()
+        # generate UUIDs for every constraint
+        for c in self.constraints:
+            if isinstance(c, Base): c.make_uuid()
 
-		self._simplified = True
-		return self.constraints
+        self._simplified = True
+        return self.constraints
 
-	def _solve(self, extra_constraints=()):
-		return SatResult(approximation=True)
+    def _solve(self, extra_constraints=()):
+        return SatResult(approximation=True)
 
-	def _satisfiable(self, extra_constraints=()):
-		return self.solve(extra_constraints=extra_constraints).sat
+    def _satisfiable(self, extra_constraints=()):
+        return self.solve(extra_constraints=extra_constraints).sat
 
-	def _eval(self, e, n, extra_constraints=()):
-		if len(extra_constraints) == 0:
-			for b in _eager_backends + [ self._solver_backend ]:
-				try: return b.eval(e, n, result=self.result)
-				except BackendError: pass
+    def _eval(self, e, n, extra_constraints=()):
+        if len(extra_constraints) == 0:
+            for b in _eager_backends + [ self._solver_backend ]:
+                try: return b.eval(e, n, result=self.result)
+                except BackendError: pass
 
-		raise ClaripyFrontendError("Light solver can't handle this eval().")
+        raise ClaripyFrontendError("Light solver can't handle this eval().")
 
-	def _max(self, e, extra_constraints=()):
-		if len(extra_constraints) == 0:
-			for b in _eager_backends + [ self._solver_backend ]:
-				try: return b.max(e, result=self.result)
-				except BackendError: pass
+    def _max(self, e, extra_constraints=()):
+        if len(extra_constraints) == 0:
+            for b in _eager_backends + [ self._solver_backend ]:
+                try: return b.max(e, result=self.result)
+                except BackendError: pass
 
-		raise ClaripyFrontendError("Light solver can't handle this max().")
+        raise ClaripyFrontendError("Light solver can't handle this max().")
 
-	def _min(self, e, extra_constraints=()):
-		extra_constraints = self._constraint_filter(extra_constraints)
+    def _min(self, e, extra_constraints=()):
+        extra_constraints = self._constraint_filter(extra_constraints)
 
-		if len(extra_constraints) == 0:
-			for b in _eager_backends + [ self._solver_backend ]:
-				try: return b.min(e, result=self.result)
-				except BackendError: pass
+        if len(extra_constraints) == 0:
+            for b in _eager_backends + [ self._solver_backend ]:
+                try: return b.min(e, result=self.result)
+                except BackendError: pass
 
-		two = self.eval(e, 2, extra_constraints=extra_constraints)
-		if len(two) == 0: raise UnsatError("unsat during min()")
-		elif len(two) == 1: return two[0]
+        two = self.eval(e, 2, extra_constraints=extra_constraints)
+        if len(two) == 0: raise UnsatError("unsat during min()")
+        elif len(two) == 1: return two[0]
 
-		raise ClaripyFrontendError("Light solver can't handle this min().")
+        raise ClaripyFrontendError("Light solver can't handle this min().")
 
-	def _solution(self, e, v, extra_constraints=()):
-		if len(extra_constraints) == 0:
-			for b in _eager_backends + [ self._solver_backend ]:
-				try: return b.solution(e, v, result=self.result)
-				except BackendError: pass
+    def _solution(self, e, v, extra_constraints=()):
+        if len(extra_constraints) == 0:
+            for b in _eager_backends + [ self._solver_backend ]:
+                try: return b.solution(e, v, result=self.result)
+                except BackendError: pass
 
-		raise ClaripyFrontendError("Light solver can't handle this solution().")
+        raise ClaripyFrontendError("Light solver can't handle this solution().")
 
-	#
-	# Serialization and such.
-	#
+    #
+    # Serialization and such.
+    #
 
-	def downsize(self):
-		Frontend.downsize(self)
+    def downsize(self):
+        Frontend.downsize(self)
 
-	#
-	# Merging and splitting
-	#
+    #
+    # Merging and splitting
+    #
 
-	def finalize(self):
-		self._finalized = True
+    def finalize(self):
+        self._finalized = True
 
-	def branch(self):
-		s = Frontend.branch(self)
-		s.constraints = list(self.constraints)
-		s.variables = set(self.variables)
-		self.finalize()
-		s.finalize()
-		return s
+    def branch(self):
+        s = Frontend.branch(self)
+        s.constraints = list(self.constraints)
+        s.variables = set(self.variables)
+        self.finalize()
+        s.finalize()
+        return s
 
-	def merge(self, others, merge_flag, merge_values):
-		merged = self.__class__(self._solver_backend)
-		merged._simplified = False
-		options = [ ]
+    def merge(self, others, merge_flag, merge_values):
+        merged = self.__class__(self._solver_backend)
+        merged._simplified = False
+        options = [ ]
 
-		for s, v in zip([self]+others, merge_values):
-			options.append(And(*([ merge_flag == v ] + s.constraints)))
-		merged.add([Or(*options)])
+        for s, v in zip([self]+others, merge_values):
+            options.append(And(*([ merge_flag == v ] + s.constraints)))
+        merged.add([Or(*options)])
 
-		return self._solver_backend is backend_z3, merged
+        return self._solver_backend is backend_z3, merged
 
-	def combine(self, others):
-		combined = self.__class__(self._solver_backend)
-		combined._simplified = False
+    def combine(self, others):
+        combined = self.__class__(self._solver_backend)
+        combined._simplified = False
 
-		combined.add(self.constraints) #pylint:disable=E1101
-		for o in others:
-			combined.add(o.constraints)
-		return combined
+        combined.add(self.constraints) #pylint:disable=E1101
+        for o in others:
+            combined.add(o.constraints)
+        return combined
 
-	def split(self):
-		results = [ ]
-		l.debug("Splitting!")
-		for variables,c_list in self.independent_constraints():
-			l.debug("... got %d constraints with %d variables", len(c_list), len(variables))
+    def split(self):
+        results = [ ]
+        l.debug("Splitting!")
+        for variables,c_list in self.independent_constraints():
+            l.debug("... got %d constraints with %d variables", len(c_list), len(variables))
 
-			s = self.__class__(self._solver_backend)
-			s._simplified = False
-			s.add(c_list)
-			results.append(s)
-		return results
+            s = self.__class__(self._solver_backend)
+            s._simplified = False
+            s.add(c_list)
+            results.append(s)
+        return results
 
 from ..result import SatResult
 from ..errors import UnsatError, BackendError, ClaripyFrontendError
