a8d6df0f30767fe3427a80bb8cd1655b99a17dd5
Yan <yans@yancomm.net>
2015-09-09 22:35:01 -0700

    ticked version number to 4.5.9.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='claripy',
-    version='4.5.9.9',
+    version='4.5.9.9b',
     packages=['claripy', 'claripy.backends', 'claripy.frontends', 'claripy.vsa', 'claripy.ast'],
     install_requires=[
         'ana',
