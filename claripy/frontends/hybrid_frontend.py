55750b4e8fc8d4dc40b0822979fab38cb6a5008d
Yan <yans@yancomm.net>
2015-11-11 23:01:34 -0800

    change HybridBackend to use a LightFrontend for approximations, instead of a standalone backend

--- claripy/frontends/hybrid_frontend.py
+++ claripy/frontends/hybrid_frontend.py
@@ -4,26 +4,27 @@ import logging
 l = logging.getLogger("claripy.frontends.full_frontend")
 
 from .full_frontend import FullFrontend
+from .light_frontend import LightFrontend
+from .replacement_frontend import ReplacementFrontend
 
 class HybridFrontend(FullFrontend):
-    def __init__(self, solver_backend, approximation_backend=None, **kwargs):
+    def __init__(self, solver_backend, approximation_frontend=None, **kwargs):
         FullFrontend.__init__(self, solver_backend, **kwargs)
-        self._approximation_backend = approximation_backend if approximation_backend is not None else _backends['BackendVSA']
+        self._approximation_frontend = approximation_frontend if approximation_frontend is not None else ReplacementFrontend(LightFrontend(_backends['BackendVSA']))
 
     #
     # Storable support
     #
 
     def _ana_getstate(self):
-        return (self._solver_backend.__class__.__name__, FullFrontend._ana_getstate(self))
+        return (self._approximation_frontend, FullFrontend._ana_getstate(self))
 
     def _ana_setstate(self, s):
-        approx_backend_name, full_state = s
-        self._approximation_backend = _backends[approx_backend_name]
+        self._approximation_frontend, full_state = s
         FullFrontend._ana_setstate(self, full_state)
 
     def _blank_copy(self):
-        return HybridFrontend(self._solver_backend, approximation_backend=self._approximation_backend, cache=self._cache, timeout=self.timeout)
+        return HybridFrontend(self._solver_backend, approximation_frontend=self._approximation_frontend, cache=self._cache, timeout=self.timeout)
 
     #
     # Hybrid solving
@@ -32,8 +33,8 @@ class HybridFrontend(FullFrontend):
     def _solve(self, extra_constraints=(), exact=None, cache=None):
         # if approximating, try the approximation backend
         if exact is False:
-            try: return self._approximation_backend.solve(extra_constraints=extra_constraints)
-            except BackendError: pass
+            try: return self._approximation_frontend.solve(extra_constraints=extra_constraints)
+            except ClaripyFrontendError: pass
 
         # if that fails, try the exact backend
         return super(HybridFrontend, self)._solve(extra_constraints=extra_constraints, exact=exact, cache=cache)
@@ -41,8 +42,8 @@ class HybridFrontend(FullFrontend):
     def _eval(self, e, n, extra_constraints=(), exact=None, cache=None):
         # if approximating, try the approximation backend
         if exact is False:
-            try: return self._approximation_backend.eval(e, n, extra_constraints=extra_constraints)
-            except BackendError: pass
+            try: return self._approximation_frontend.eval(e, n, extra_constraints=extra_constraints)
+            except ClaripyFrontendError: pass
 
         # if that fails, try the exact backend
         return super(HybridFrontend, self)._eval(e, n, extra_constraints=extra_constraints, exact=exact, cache=cache)
@@ -50,8 +51,8 @@ class HybridFrontend(FullFrontend):
     def _max(self, e, extra_constraints=(), exact=None, cache=None):
         # if approximating, try the approximation backend
         if exact is False:
-            try: return self._approximation_backend.max(e, extra_constraints=extra_constraints)
-            except BackendError: pass
+            try: return self._approximation_frontend.max(e, extra_constraints=extra_constraints)
+            except ClaripyFrontendError: pass
 
         # if that fails, try the exact backend
         return super(HybridFrontend, self)._max(e, extra_constraints=extra_constraints, exact=exact, cache=cache)
@@ -59,8 +60,8 @@ class HybridFrontend(FullFrontend):
     def _min(self, e, extra_constraints=(), exact=None, cache=None):
         # if approximating, try the approximation backend
         if exact is False:
-            try: return self._approximation_backend.min(e, extra_constraints=extra_constraints)
-            except BackendError: pass
+            try: return self._approximation_frontend.min(e, extra_constraints=extra_constraints)
+            except ClaripyFrontendError: pass
 
         # if that fails, try the exact backend
         return super(HybridFrontend, self)._min(e, extra_constraints=extra_constraints, exact=exact, cache=cache)
@@ -68,8 +69,8 @@ class HybridFrontend(FullFrontend):
     def _solution(self, e, v, extra_constraints=(), exact=None, cache=None):
         # if approximating, try the approximation backend
         if exact is False:
-            try: return self._approximation_backend.solution(e, v, extra_constraints=extra_constraints)
-            except BackendError: pass
+            try: return self._approximation_frontend.solution(e, v, extra_constraints=extra_constraints)
+            except ClaripyFrontendError: pass
 
         # if that fails, try the exact backend
         return super(HybridFrontend, self)._solution(e, v, extra_constraints=extra_constraints, exact=exact, cache=cache)
@@ -77,8 +78,8 @@ class HybridFrontend(FullFrontend):
     def _is_true(self, e, extra_constraints=(), exact=None, cache=None):
         # if approximating, try the approximation backend
         if exact is False:
-            try: return self._approximation_backend.is_true(e, extra_constraints=extra_constraints)
-            except BackendError: pass
+            try: return self._approximation_frontend.is_true(e, extra_constraints=extra_constraints)
+            except ClaripyFrontendError: pass
 
         # if that fails, try the exact backend
         return super(HybridFrontend, self)._is_true(e, extra_constraints=extra_constraints, exact=exact, cache=cache)
@@ -86,11 +87,11 @@ class HybridFrontend(FullFrontend):
     def _is_false(self, e, extra_constraints=(), exact=None, cache=None):
         # if approximating, try the approximation backend
         if exact is False:
-            try: return self._approximation_backend.is_false(e, extra_constraints=extra_constraints)
-            except BackendError: pass
+            try: return self._approximation_frontend.is_false(e, extra_constraints=extra_constraints)
+            except ClaripyFrontendError: pass
 
         # if that fails, try the exact backend
         return super(HybridFrontend, self)._is_false(e, extra_constraints=extra_constraints, exact=exact, cache=cache)
 
-from ..errors import BackendError
+from ..errors import ClaripyFrontendError
 from .. import _backends
