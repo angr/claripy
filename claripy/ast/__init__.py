fae0611c19f5ff2859437452be6f8f360a003abb
Yan <yans@yancomm.net>
2016-01-18 18:29:00 -0800

    also import the pre-created true and false BoolVs into claripy.ast

--- claripy/ast/__init__.py
+++ claripy/ast/__init__.py
@@ -5,16 +5,18 @@ FP = lambda *args, **kwargs: None
 Bool = lambda *args, **kwargs: None
 Int = lambda *args, **kwargs: None
 Base = lambda *args, **kwargs: None
+true = lambda *args, **kwargs: None
+false = lambda *args, **kwargs: None
 all_operations = None
 
 def _import():
-    global Bits, BV, VS, FP, Bool, Int, Base, all_operations
+    global Bits, BV, VS, FP, Bool, Int, Base, true, false, all_operations
 
     from .bits import Bits
     from .bv import BV
     from .vs import VS
     from .fp import FP
-    from .bool import Bool
+    from .bool import Bool, true, false
     from .int import Int
     from .base import Base
     from .. import ops as all_operations
