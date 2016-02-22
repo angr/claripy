2246de4d9c29183b8c6d9decd161abd5a6760b09
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-02-22 01:30:50 -0800

    Updating WI

--- claripy/vsa/wrapped_interval.py
+++ claripy/vsa/wrapped_interval.py
@@ -11,7 +11,7 @@ class WrappedInterval(object):
         Refer paper: Signedness-agnostic program analysis: Precise integer bounds for low-level code
     """
 
-    def __init__(self, lower_bound, upper_bound, no_of_bits=32):
+    def __init__(self, lower_bound, upper_bound, no_of_bits=32, is_bottom=False):
         """
         Construct a new wrapped interval with provided lower and upper bounds.
         :return:
@@ -23,6 +23,7 @@ class WrappedInterval(object):
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
         self.no_of_bits = no_of_bits
+        self.is_bottom = is_bottom
 
     @staticmethod
     def _max_upper_bound(no_of_bits):
@@ -52,6 +53,21 @@ class WrappedInterval(object):
         return (op1 - op2) & WrappedInterval._max_upper_bound(no_of_bits)
 
     @staticmethod
+    def _mod_add(op1, op2, no_of_bits):
+        """
+        Performs: Modular Addition.
+        Perform (op1 + op2) and return result with in no_of_bits
+        Refer Section 3 of the paper.
+        :param op1: operand 1
+        :param op2: operand 2
+        :param no_of_bits: Width of the result.
+        :return: result of modular addition.
+        Note: The return value will be always positive
+        """
+        assert no_of_bits > 0, "Number of bits should be more than zero"
+        return (op1 + op2) & WrappedInterval._max_upper_bound(no_of_bits)
+
+    @staticmethod
     def _less_than_a(num1, num2, a, no_of_bits):
         """
         Implements a less than equal operator as suggested in
@@ -78,6 +94,47 @@ class WrappedInterval(object):
         return (self.lower_bound == 0) and \
                (self.upper_bound == WrappedInterval._max_upper_bound(self.no_of_bits))
 
+    def is_bottom(self):
+        """
+        This function returns True if this WrappedInterval is Bottom else False
+        :return: True if the wrapped interval is true else false
+        """
+        return self.is_bottom
+
+    def get_size(self):
+        """
+        Gets the size of the interval represented by this range
+        Refer section 3.1
+        :return: Size or number of elements in this interval
+        """
+        # case 1
+        if self.is_bottom():
+            return 0
+        # case 2
+        if self.is_top():
+            return 1 << self.no_of_bits
+        # case 3
+        y_x = WrappedInterval._mod_sub(self.upper_bound, self.lower_bound, self.no_of_bits)
+        return WrappedInterval._mod_add(y_x, 1, self.no_of_bits)
+
+    def get_complement(self):
+        """
+        Return the complement of the interval
+        Refer section 3.1
+        :return:
+        """
+        # case 1
+        if self.is_bottom():
+            return WrappedInterval(0, (1 << self.no_of_bits)-1, no_of_bits=self.no_of_bits)
+        # case 2
+        if self.is_top():
+            return WrappedInterval(0, (1 << self.no_of_bits)-1, no_of_bits=self.no_of_bits, is_bottom=True)
+        # case 3
+        y_plus_1 = WrappedInterval._mod_add(self.upper_bound, 1, self.no_of_bits)
+        x_minus_1 = WrappedInterval._mod_sub(self.lower_bound, 1, self.no_of_bits)
+        return WrappedInterval(y_plus_1, x_minus_1, self.no_of_bits)
+
+    # Section 3.1 Ordering Wrapped Intervals
     def is_in_interval(self, target_number):
         """
         Check if target_number is in the range represented by this object.
@@ -85,10 +142,35 @@ class WrappedInterval(object):
         :param target_number: number to be checked.
         :return: True if target_number is in this range else False
         """
+        # case 0
+        # if the number if more than max number representable by this range
+        if target_number > WrappedInterval._max_upper_bound(self.no_of_bits):
+            return False
         # case 1
         if self.is_top():
             return True
         # case 2
-        # Ignore Bottom case
+        if self.is_bottom():
+            return False
         # case 3
         return WrappedInterval._less_than_a(target_number, self.upper_bound, self.lower_bound, self.no_of_bits)
+
+    def is_interval_included(self, to_check_interval):
+        """
+        Check if to_check_interval is in interval represented by this object.
+        Refer section 3.1
+        :param to_check_interval: Interval to check
+        :return: True if to_check_interval is in this range else False
+        """
+        assert to_check_interval is not None, "Interval to check should be not None"
+        # Case 1
+        if self.is_top() or to_check_interval.is_bottom():
+            return True
+        # Case 2
+        if to_check_interval.is_top() or self.is_bottom():
+            return False
+        # Case 3
+        return (self.is_in_interval(to_check_interval.lower_bound)) and \
+               (self.is_in_interval(to_check_interval.upper_bound)) and \
+               ((not to_check_interval.is_in_interval(self.lower_bound)) or
+                (not to_check_interval.is_in_interval(self.upper_bound)))
