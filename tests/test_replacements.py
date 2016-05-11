6f5dd795d8efed3207f19aba1473e2693f39a3f0
Yan <yans@yancomm.net>
2016-05-10 22:45:31 -0700

    add a testcase to make sure that contradictions are handled properly

--- tests/test_replacements.py
+++ tests/test_replacements.py
@@ -14,5 +14,17 @@ def test_replacement_solver():
     sr2.add(x + 8 < 2000)
     assert sr2.max(x) == sr2.min(x) == sr.max(x)
 
+def test_contradiction():
+    sr = claripy.ReplacementFrontend(claripy.FullFrontend(claripy.backends.z3), replace_constraints=True)
+    x = claripy.BVS('x', 32)
+
+    sr.add(x == 10)
+    assert sr.satisfiable()
+    assert sr.eval(x, 10) == (10,)
+
+    sr.add(x == 100)
+    assert not sr.satisfiable()
+
 if __name__ == '__main__':
     test_replacement_solver()
+    test_contradiction()
