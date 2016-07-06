478906aa950ff8e749b1d9b358c014f23005b976
Yan <yans@yancomm.net>
2016-07-06 00:39:27 -0700

    properly handle primitive boolean constraints

---
+++ claripy/frontend_mixins/constraint_fixer_mixin.py
@@ -0,0 +1,11 @@
+class ConstraintFixerMixin(object):
+    def add(self, constraints, **kwargs):
+        constraints = [ constraints ] if not isinstance(constraints, (list, tuple, set)) else constraints
+
+        if len(constraints) == 0:
+            return [ ]
+
+        constraints = [ BoolV(c) if type(c) is bool else c for c in constraints ]
+        return super(ConstraintFixerMixin, self).add(constraints, **kwargs)
+
+from .. import BoolV
