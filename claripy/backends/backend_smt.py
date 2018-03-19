74010c049eb6819a99c5604e1ccbbbd2a2fcb0c8
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2018-03-19 12:38:49 -0700

    Add input normalization during equality check
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- claripy/backends/backend_smt.py
+++ claripy/backends/backend_smt.py
@@ -12,6 +12,13 @@ from . import BackendError, Backend
 
 
 def _expr_to_smtlib(e):
+    """
+    Dump the symbol in its smt-format depending on its type
+
+    :param e: symbol to dump
+
+    :return string: smt-lib representation of the symbol
+    """
     if e.is_symbol():
         return "(declare-const %s %s)" % (e.symbol_name(), e.symbol_type())
     else:
@@ -19,14 +26,37 @@ def _expr_to_smtlib(e):
 
 
 def _exprs_to_smtlib(*exprs):
+    """
+    Dump all the variables and all the constraints in an smt-lib format
+
+    :param exprs : List of variable declaration and constraints that have to be
+             dumped in an smt-lib format
+
+    :return string: smt-lib representation of the list of expressions 
+    """
     return '\n'.join(_expr_to_smtlib(e) for e in exprs) + '\n'
+
+
+def _normalize_arguments(expr_left, expr_rigth):
+    """
+    Since we decided to emulate integer with bitvector, this method transform their
+    concrete value (if any) in the corresponding integer
+    """
+    if expr_left.is_str_op() and expr_rigth.is_bv_constant():
+        return expr_left, Int(expr_rigth.bv_signed_value())
+    elif expr_left.is_bv_constant() and expr_rigth.is_str_op():
+        return expr_rigth, Int(expr_left.bv_signed_value())
+    return expr_left, expr_rigth
+
 
 
 class BackendSMT(Backend):
     def __init__(self):
         Backend.__init__(self)
 
-        self._assertions_stack = []
+        # The assertions stack is inside tehe solver so we need the resference
+        #  to it in order to retrieve variable declarations
+        self.solver = None
 
         self._op_expr['StringV'] = self.StringV
         self._op_expr['StringS'] = self.StringS
@@ -45,16 +75,40 @@ class BackendSMT(Backend):
         # self._op_raw['__xor__'] = self._op_xor
         # self._op_raw['__and__'] = self._op_and
 
-        # self._cache_objects = False
+    def register_solver(self, solver):
+        """
+        Since the backend is shared among all the solver we need to register
+        the correct solver for this backend so the backend can retieve the
+        correct assertion_stack.
+
+        This implementation is still wrong because if we register a new solver,
+        All the other solvers wioll lose their assertion stacks
+
+        :param solver : the appropriate solver for this backend
+        """
+        self.solver = solver
 
     def _smtlib_exprs(self, extra_constraints=()):
-        all_exprs = tuple(self._assertions_stack) + tuple(extra_constraints)
+        """
+        Return the smt-lib representation of the current context and constraints
+
+        :param extra-constraints: list of extra constraints that we want to evaluate only
+                                 in the scope of this call
+
+        :return string: smt-lib representation of the list of expressions and variable declarations
+        """
+        all_exprs = tuple(self.solver.get_assertions()) + tuple(extra_constraints)
         return _exprs_to_smtlib(*all_exprs)
 
     def _get_satisfiability_smt_script(self, extra_constraints=()):
         '''
         Returns a SMT script that declare all the symbols and constraint and checks
         their satisfiability (check-sat)
+
+        :param extra-constraints: list of extra constraints that we want to evaluate only
+                                 in the scope of this call
+                                
+        :return string: smt-lib representation of the script that checks the satisfiability
         '''
         smt_script = '(set-logic ALL)\n'
         smt_script += self._smtlib_exprs(extra_constraints)
@@ -65,6 +119,11 @@ class BackendSMT(Backend):
         '''
         Returns a SMT script that declare all the symbols and constraint and checks
         their satisfiability (check-sat)
+
+        :param extra-constraints: list of extra constraints that we want to evaluate only
+                                 in the scope of this call
+
+        :return string: smt-lib representation of the script that checks the satisfiability
         '''
         smt_script = '(set-logic ALL)\n'
         smt_script += '(set-option :produce-models true)\n'
@@ -75,18 +134,16 @@ class BackendSMT(Backend):
 
 
     def StringV(self, ast):
-        # TODO: check correct format
-        # self._op_expr['StringV'] = self.StringV
-        #       if format not correct throw exception BackError()
         content, _ = ast.args
         return String(content)
 
     def StringS(self, ast):
-        # TODO: check correct format
-        #       if format not correct throw exception BackError()
         name, _ = ast.args
-        assertion = Symbol(name, STRING) 
-        self._assertions_stack.append(assertion)
+        # We need to keep track of new declarations
+        # because when we dump the constraints we need to dump the
+        # declaration as well
+        assertion = Symbol(name, STRING)
+        self.solver.push(assertion)
         return assertion
 
     def BoolV(self, ast):
@@ -111,21 +168,24 @@ class BackendSMT(Backend):
         return StrReplace(initial_str, pattern_to_replace, replacement_pattern)
 
     def _op_eq(self, *args):
-        expr_left, expr_rigth = args
         # We emulate the integer through a bitvector but
         # since a constraint with the form (assert (= (str.len Symb_str) bit_vect))
         # is not valid we need to tranform the concrete vqalue of the bitvector in an integer
         #
         # TODO: implement logic for integer
-        if expr_left.is_str_op() and expr_rigth.is_bv_constant():
-            return Equals(expr_left, Int(expr_rigth.bv_signed_value()))
-        elif expr_left.is_bv_constant() and expr_rigth.is_str_op():
-            return Equals(expr_rigth, Int(expr_left.bv_signed_value())) 
-        return Equals(expr_left, expr_rigth)
+        left_expr, right_expr = args
+        norm_left_expr, norm_right_expr = _normalize_arguments(left_expr, right_expr)
+        return Equals(norm_left_expr, norm_right_expr)
 
     def _op_ne(self, *args):
-        expr_left, expr_rigth = args
-        return NotEquals(expr_left, expr_rigth)
+        # We emulate the integer through a bitvector but
+        # since a constraint with the form (assert (= (str.len Symb_str) bit_vect))
+        # is not valid we need to tranform the concrete vqalue of the bitvector in an integer
+        #
+        # TODO: implement logic for integer
+        left_expr, right_expr = args
+        norm_left_expr, norm_right_expr = _normalize_arguments(left_expr, right_expr)
+        return NotEquals(norm_left_expr, norm_right_expr)
 
     # @staticmethod
     # def _op_sub(*args):
