aca33e46d8f82fbfece328b3e6596b4de51011d1
Lukas-Dresel <Lukas-Dresel@users.noreply.github.com>
2018-03-19 21:47:15 -0700

    make strings behave like bit-addressable objects

--- claripy/backends/backend_smt.py
+++ claripy/backends/backend_smt.py
@@ -5,7 +5,7 @@ from pysmt.shortcuts import Symbol, String, StrConcat, Equals, NotEquals, \
                             Bool, BV, Or, LT, LE, GT, GE, \
                             StrContains
 
-from pysmt.typing import STRING
+from pysmt.typing import STRING, BVType
 
 
 l = logging.getLogger("claripy.backends.backend_smt")
@@ -72,6 +72,10 @@ class BackendSMT(Backend):
         self._op_expr['BoolV'] = self.BoolV
         self._op_expr['BVV'] = self.BVV
 
+        # ------------------- BITVECTOR OPERATIONS -------------------
+        # self._op_raw['Extract'] = self._op_raw_extract
+        # self._op_raw['Concat'] = self._op_raw_concat
+
         # ------------------- GENERAL PURPOSE OPERATIONS ------------------- 
         self._op_raw['__eq__'] = self._op_raw_eq
         self._op_raw['__ne__'] = self._op_raw_ne
@@ -86,7 +90,7 @@ class BackendSMT(Backend):
         self._op_raw['Substr'] = self._op_raw_str_substr
         self._op_raw['StrLen'] = self._op_raw_str_strlen
         self._op_raw['StrReplace'] = self._op_raw_str_replace
-        self._op_raw["StrContains"] = self._op_raw_str_contains
+        self._op_raw['StrContains'] = self._op_raw_str_contains
 
 
     def _smtlib_exprs(self, constraints=()):
@@ -155,6 +159,20 @@ class BackendSMT(Backend):
     def BVV(self, ast):
         val, size = ast.args
         return BV(val, size)
+
+    def BVS(self, ast):
+        return Symbol(ast.args[0], BVType(ast.length))
+
+    # ------------------- BITVECTOR OPERATIONS -------------------
+    '''
+    def _op_raw_extract(self, high, low, val):
+        import ipdb; ipdb.set_trace()
+        return BVExtract(val, start=low, end=high)
+
+    def _op_raw_concat(self, left, right):
+        import ipdb; ipdb.set_trace()
+        return BVConcat(left, right)
+    '''
 
     # ------------------- GENERAL PURPOSE OPERATIONS ------------------- 
 
