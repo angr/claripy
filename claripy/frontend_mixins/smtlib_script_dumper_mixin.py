bbefa0dbce0149c225e961b877d4132a13fa0ab4
Lukas-Dresel <Lukas-Dresel@users.noreply.github.com>
2018-03-30 14:45:54 -0700

    made solver handle cases of eval for non-constrained variables correctly

--- claripy/frontend_mixins/smtlib_script_dumper_mixin.py
+++ claripy/frontend_mixins/smtlib_script_dumper_mixin.py
@@ -8,15 +8,18 @@ from ..frontends.constrained_frontend import ConstrainedFrontend
 l = logging.getLogger(__name__)
 
 class SMTLibScriptDumperMixin(object):
-    def get_smtlib_script_satisfiability(self, extra_constraints=()):
+    def get_smtlib_script_satisfiability(self, extra_constraints=(), extra_variables=()):
         """
         Return an smt-lib script that check the satisfiability of the current constraints
 
         :return string: smt-lib script
         """
         try:
-            return self._solver_backend._get_satisfiability_smt_script(
-                constraints=self._solver_backend.convert_list(tuple(self.constraints) + extra_constraints))
+            e_csts = self._solver_backend.convert_list(extra_constraints + tuple(self.constraints))
+            e_variables = self._solver_backend.convert_list(extra_variables)
+
+            variables, csts = self._solver_backend._get_all_vars_and_constraints(e_c=e_csts, e_v=e_variables)
+            return self._solver_backend._get_satisfiability_smt_script(csts, variables)
         except BackendError as e:
             raise_from(ClaripyFrontendError("Backend error during smtlib script generation"), e)
 
