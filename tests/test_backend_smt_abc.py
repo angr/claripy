62dd72ed826d695472eb3010c2e3f4ae319bae5f
Lukas-Dresel <Lukas-Dresel@users.noreply.github.com>
2018-05-15 14:39:48 -0700

    made tests nose compatible with added skipping if the backends are not available

--- tests/test_backend_smt_abc.py
+++ tests/test_backend_smt_abc.py
@@ -1,11 +1,15 @@
 import unittest
 import claripy
-from claripy.backends.backend_smtlib_solvers.abc_popen import SolverBackendABC
+import nose
 from test_backend_smt_solver import SmtLibSolverTest
 
 
 class SmtLibSolverTest_ABC(SmtLibSolverTest):
     def get_solver(self):
+        if 'smtlib_abc' not in claripy.backends._backends_by_name:
+            raise nose.SkipTest()
+
+        from claripy.backends.backend_smtlib_solvers.abc_popen import SolverBackendABC
         backend = SolverBackendABC(daggify=True)
         solver = claripy.SolverStrings(backend=backend)
         return solver
