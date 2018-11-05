d2f37c5eb8774d3a637a8cb14a8c57634d95b789
Audrey Dutcher <audrey@rhelmot.io>
2018-11-05 12:05:54 -0800

    Add dependency on cachetools

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         'ana',
         'z3-solver==4.5.1.0.post2',
         'future',
+        'cachetools<3'
     ],
     description='An abstraction layer for constraint solvers',
     url='https://github.com/angr/claripy',
