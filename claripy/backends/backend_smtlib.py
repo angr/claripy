60b919a3f4000015bdc1959f98d4739926c9d247
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2018-12-16 19:33:03 -0800

    Add And and Not operations
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- claripy/backends/backend_smtlib.py
+++ claripy/backends/backend_smtlib.py
@@ -4,7 +4,7 @@ from pysmt.shortcuts import Symbol, String, StrConcat, NotEquals, \
     StrSubstr, Int, StrLength, StrReplace, \
     Bool, Or, LT, LE, GT, GE, \
     StrContains, StrPrefixOf, StrSuffixOf, StrIndexOf, \
-    StrToInt, Ite, EqualsOrIff, Minus, Plus, BVToNatural, IntToStr
+    StrToInt, Ite, EqualsOrIff, Minus, Plus, IntToStr, Not, And
 
 from pysmt.typing import STRING, INT, BOOL
 
@@ -79,6 +79,8 @@ class BackendSMTLibBase(Backend):
         self._op_raw['__ge__'] = self._op_raw_ge
         self._op_raw['Or'] = self._op_raw_or
         self._op_raw['If'] = self._op_raw_if
+        self._op_raw['Not'] = self._op_raw_not
+        self._op_raw['And'] = self._op_raw_and
 
         # ------------------- STRINGS OPERATIONS ------------------- 
         self._op_raw['StrConcat'] = self._op_raw_str_concat
@@ -194,9 +196,6 @@ class BackendSMTLibBase(Backend):
 
     # ------------------- GENERAL PURPOSE OPERATIONS -------------------
 
-    def _op_raw_if(self, *args):
-        return Ite(*args)
-
     def _op_raw_eq(self, *args):
         # We emulate the integer through a bitvector but
         # since a constraint with the form (assert (= (str.len Symb_str) bit_vect))
@@ -251,7 +250,13 @@ class BackendSMTLibBase(Backend):
     def _op_raw_if(self, *args):
         return Ite(*args)
 
-    # ------------------- STRINGS OPERATIONS ------------------- 
+    def _op_raw_not(self, *args):
+        return Not(*args)
+
+    def _op_raw_and(self, *args):
+        return And(*args)
+
+    # ------------------- STRINGS OPERATIONS -------------------
     
     def _op_raw_str_concat(self, *args):
         return StrConcat(*args)
