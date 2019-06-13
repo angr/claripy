56dab1d737eb4d759abb4e7c826ca9306e2b8092
Saullo Carvalho Castelo Branco <saullocarvalho@gmail.com>
2019-06-13 16:06:13 -0700

    Modify ast lite repr test to consider operation precedence

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -14,10 +14,9 @@ def test_lite_repr():
     nose.tools.assert_regex(((a + one) * (b + two)).shallow_repr(),
                             r'\<BV8 \(a_\d+_8 \+ 1\) \* \(b_\d+_8 \+ 2\)\>')
     nose.tools.assert_regex((a * one + b * two).shallow_repr(),
-                            r'\<BV8 \(a_\d+_8 \* 1\) \+ \(b_\d+_8 \* 2\)\>')
-    #r'\<BV8 a_\d+_8 \* 1 \+ b_\d+_8 \* 2\>')
+                            r'\<BV8 a_\d+_8 \* 1 \+ b_\d+_8 \* 2\>')
     nose.tools.assert_regex(((one + a)*(two + b)+(two + a)*(one + b)).shallow_repr(),
-                            r'\<BV8 \(\(1 \+ a_\d+_8\) \* \(2 \+ b_\d+_8\)\) \+ \(\(2 \+ a_\d+_8\) \* \(1 \+ b_\d+_8\)\)\>')
+                            r'\<BV8 \(1 \+ a_\d+_8\) \* \(2 \+ b_\d+_8\) \+ \(2 \+ a_\d+_8\) \* \(1 \+ b_\d+_8\)\>')
 
 
 
