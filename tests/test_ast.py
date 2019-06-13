417e398519d53cd33d0fa9036c46b44fd4cb7677
Saullo Carvalho Castelo Branco <saullocarvalho@gmail.com>
2019-06-13 09:05:27 -0700

    Add simple test to ast lite repr

---
+++ tests/test_ast.py
@@ -0,0 +1,22 @@
+import nose.tools
+
+import claripy
+
+
+def test_lite_repr():
+    one = claripy.BVV(1, 8)
+    two = claripy.BVV(2, 8)
+    a = claripy.BVS('a', 8)
+    b = claripy.BVS('b', 8)
+
+    nose.tools.assert_regex((a + one * b + two).shallow_repr(),
+                            r'\<BV8 __add__\(a_\d+_8, \(1 \* b_\d+_8\), 2\)\>')
+    nose.tools.assert_regex(( (a + one) * (b + two) ).shallow_repr(),
+                            r'\<BV8 \(a_\d+_8 \+ 1\) \* \(b_\d+_8 \+ 2\)\>')
+    nose.tools.assert_regex((a * one + b * two).shallow_repr(),
+                            r'\<BV8 \(a_\d+_8 \* 1\) \+ \(b_\d+_8 \* 2\)\>')
+                            #r'\<BV8 a_\d+_8 \* 1 \+ b_\d+_8 \* 2\>')
+
+
+if __name__ == '__main__':
+    test_lite_repr()
