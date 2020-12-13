ca29a75303016ae6964482c9bfbb2677787bd9c4
zwimer <zwimer@gmail.com>
2020-12-13 13:17:59 -0700

    clang-format codebase

--- native/src/utils/set_join.hpp
+++ native/src/utils/set_join.hpp
@@ -11,36 +11,36 @@
 /** A namespace used for the utils directory */
 namespace Utils {
 
-	/** A namespaced used to designate certain items in utils as private
-	 *  These functions should not be called outside of the utils directory
-	 *  This is useful for helper functions templated functions call
-	 */
-	namespace Private {
-		/** A helper function used to merge a set of set<T>'s into ret
-		 *  A specialization of set_join_helper that handles the single argument case
-		 */
-		template <typename T>
-			void set_join_helper(std::set<T> & ret, const std::set<T> & a) {
-				ret.insert(a.begin(), a.end());
-			}
+    /** A namespace used to designate certain items in utils as private
+     *  These functions should not be called outside of the utils directory
+     *  This is useful for helper functions templated functions call
+     */
+    namespace Private {
+        /** A helper function used to merge a set of set<T>'s into ret
+         *  A specialization of set_join_helper that handles the single argument case
+         */
+        template <typename T>
+        void set_join_helper( std::set<T> &ret, const std::set<T> &a ) {
+            ret.insert( a.begin(), a.end() );
+        }
 
-		/** A helper function used to merge a set of set<T>'s into ret
-		 *  Merge the set a into ret then recurse
-		 */
-		template <typename T, typename... Args>
-			void set_join_helper(std::set<T> & ret, const std::set<T> & a, const Args... args) {
-				ret.insert(a.begin(), a.end());
-				set_join_helper(ret, args...);
-			}
-	}
+        /** A helper function used to merge a set of set<T>'s into ret
+         *  Merge the set a into ret then recurse
+         */
+        template <typename T, typename... Args>
+        void set_join_helper( std::set<T> &ret, const std::set<T> &a,
+                              const Args... args ) {
+            ret.insert( a.begin(), a.end() );
+            set_join_helper( ret, args... );
+        }
+    } // namespace Private
 
-	/** Joins a set of set<T>'s into one */
-	template <typename T, typename... Args>
-		std::set<T> set_join(const Args... args) {
-			auto ret = std::set<T>();
-			Private::set_join_helper<T>(ret, args...);
-			return ret;
-		}
+    /** Joins a set of set<T>'s into one */
+    template <typename T, typename... Args> std::set<T> set_join( const Args... args ) {
+        auto ret = std::set<T>();
+        Private::set_join_helper<T>( ret, args... );
+        return ret;
+    }
 
-}
+} // namespace Utils
 #endif
