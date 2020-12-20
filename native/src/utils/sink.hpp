5b4a13dbb95db351a7ca730594583c83ae66e2c1
zwimer <zwimer@gmail.com>
2020-12-19 22:49:58 -0700

    Typo

--- native/src/utils/sink.hpp
+++ native/src/utils/sink.hpp
@@ -11,11 +11,11 @@
 /** A namespace used for the utils directory */
 namespace Utils {
 
-    /** A struct that does nothing with it's  arguments
+    /** A struct that does nothing with it's arguments
      *  This is useful if you have some parameter that is used only when DEBUG is defined but
-     * during a relase build is not; in this case we could have an unused parameter warning; this
-     * struct exiats to mitigate that. Normal variables can just be voided out via (void) x;, but
-     * this is not true for argument packs; Utils::Sink can handle these. */
+     *  during a relase build is not; in this case we could have an unused parameter warning; this
+     *  struct exists to mitigate that. Normal variables can just be voided out via (void) x;, but
+     *  this is not true for argument packs; Utils::Sink can handle these. */
     struct Sink {
         /** Constructor */
         template <typename... Args> Sink(const Args &...) {}
