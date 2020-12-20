399b98ee3d09be29a784cbf8691b2eecaf5898af
zwimer <zwimer@gmail.com>
2020-12-20 14:44:51 -0700

    Improve compilation flags and update test case dummy to remove boost dependency

---
+++ native/tests/native/ast/dummy.cpp
@@ -0,0 +1,6 @@
+/** A place holder test
+ *  @todo make this a real test
+ */
+bool dummy() {
+    return true;
+}
---
+++ native/tests/native/constants.hpp
@@ -0,0 +1,12 @@
+#ifndef __TESTS_NATIVE_CONSTANTS_HPP__
+#define __TESTS_NATIVE_CONSTANTS_HPP__
+
+/** A namespace used for the test/native directory */
+namespace Tests::Native {
+
+    /** The test function type */
+    using Test = bool();
+
+} // namespace Tests::Native
+
+#endif
