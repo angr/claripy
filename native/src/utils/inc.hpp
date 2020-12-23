d3f6b5a7cc004faeeb0ae9d849593b8be9b5b090
zwimer <zwimer@gmail.com>
2020-12-23 04:24:06 -0700

    Make inc threadsafe

--- native/src/utils/inc.hpp
+++ native/src/utils/inc.hpp
@@ -7,15 +7,19 @@
 
 #include "../constants.hpp"
 
+#include <mutex>
+
 
 /** A namespace used for the utils directory */
 namespace Utils {
 
-    /** Increment a static number and return the result
+    /** Thread-safe-ly increment a static number and return the result
      *  The template Args are used as a map key to allow this function to be reused as needed
      */
     template <typename... Args> Constants::Int inc() {
+        static std::mutex m;
         static Constants::Int ret = 0;
+        std::unique_lock<decltype(m)> lock(m);
         return ++ret;
     }
 
