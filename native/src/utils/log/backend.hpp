ad360de577d28c3592dc53fedaaa9c48256a3efd
zwimer <zwimer@gmail.com>
2020-12-24 16:48:48 -0700

    Create abstract base file

--- native/src/utils/log/backend.hpp
+++
@@ -1,8 +0,0 @@
-/**
- * @file
- * @brief This file defines the logging backend class
- */
-#ifndef __UTILS_LOG_BACKEND_HPP__
-#define __UTILS_LOG_BACKEND_HPP__
-
-#endif
---
+++ native/src/utils/log/backend/abstract_base.hpp
@@ -0,0 +1,8 @@
+/**
+ * @file
+ * @brief This file defines the base logging backend class
+ */
+#ifndef __UTILS_LOG_BACKEND_ABSTRACTBASE_HPP__
+#define __UTILS_LOG_BACKEND_ABSTRACTBASE_HPP__
+
+#endif
