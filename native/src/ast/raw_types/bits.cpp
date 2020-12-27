5000aba72f0638e024d56f5fa9cda6409832e697
zwimer <zwimer@gmail.com>
2020-12-26 18:33:03 -0700

    Cleanup bits.cpp

--- native/src/ast/raw_types/bits.cpp
+++ native/src/ast/raw_types/bits.cpp
@@ -13,29 +13,28 @@ AST_RAWTYPES_DEFINE_AST_SUBBITS_ID_FUNCTIONS(Bits)
 
 
 // For brevity
-using CBits = AST::RawTypes::Bits;
+using namespace AST::RawTypes;
 
 
-CBits::~Bits() {}
+Bits::~Bits() {}
 
 /** @todo */
-CBits::Bits(const Hash h, const Ops::Operation o, const Constants::Int l)
-    : Base(h, o), length(l) {}
+Bits::Bits(const Hash h, const Ops::Operation o, const Constants::Int l) : Base(h, o), length(l) {}
 
 /** @todo change this */
-AST::Hash CBits::hash(const Ops::Operation o, const Constants::Int l) {
-    return Hash(CBits::static_class_id) * (1 + Hash(o)) + l;
+AST::Hash Bits::hash(const Ops::Operation o, const Constants::Int l) {
+    return Hash(Bits::static_class_id) * (1 + Hash(o)) + l;
 }
 
 // A special definition of type_name
-std::string CBits::type_name() const {
+std::string Bits::type_name() const {
     auto s = std::set<BackendID>();
     std::stringstream ret;
     ret << this->fundamental_type_name() << this->length;
     return ret.str();
 }
 
-void CBits::check_replaceability(const AST::Bits &old, const AST::Bits &new_) {
+void Bits::check_replaceability(const ::AST::Bits &old, const ::AST::Bits &new_) {
     if (old->length != new_->length) {
         throw Errors::AST::Base("Replacements must have matching sizes");
     }
