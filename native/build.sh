cf25421a3feda86928fa6e5cf7b49bde1dad5888
zwimer <zwimer@gmail.com>
2020-12-28 23:57:36 -0700

    Update make target names and improve build.sh

--- native/build.sh
+++ native/build.sh
@@ -8,9 +8,8 @@ DOCKER_TARGET="${DOCKER_TARGET:-test}"
 # Optional env arguments passed as --build-args to docker
 FORMAT="${FORMAT:-False}"
 CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Debug}"
-MAKE_TARGETS="${MAKE_TARGETS:-}"
-TEST="${TEST:-False}"
-VERBOSE="${VERBOSE:-False}"
+MAKE_TARGET="${MAKE_TARGET:-claricpp}"
+VERBOSE_TEST="${VERBOSE_TEST:-False}"
 
 
 # Get the version
@@ -19,11 +18,15 @@ VERSION="$(cat ../VERSION)"
 
 # Build claricpp:<VERSION>
 docker build --target "${DOCKER_TARGET}" -t claricpp:"${VERSION}" \
+	`# Config ` \
 	--build-arg VERSION="${VERSION}" \
-	--build-arg FORMAT="False" \
-	--build-arg CMAKE_BUILD_TYPE="Debug" \
-	--build-arg MAKE_TARGETS="" \
-	--build-arg VERBOSE="False" \
+	--build-arg FORMAT="${FORMAT}" \
+	`# CMake ` \
+	--build-arg CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+	`# Make ` \
+	--build-arg MAKE_TARGET="${MAKE_TARGET}" \
+	`# Test ` \
+	--build-arg VERBOSE_TEST="${VERBOSE_TEST}" \
 	.
 
 # Tag the build as latest
