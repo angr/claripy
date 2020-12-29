9951136c3261660d92a18ae351de3f5525fefe10
zwimer <zwimer@gmail.com>
2020-12-28 23:19:14 -0700

    Update build.sh and dockerignore based on new multi-stage build

--- native/build.sh
+++ native/build.sh
@@ -1,20 +1,21 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
+set -o pipefail
 
-# Optional env arguments
+
+# Optional env arguments for build.sh
+DOCKER_TARGET="${DOCKER_TARGET:-test}"
+
+# Optional env arguments passed as --build-args to docker
 FORMAT="${FORMAT:-False}"
 CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Debug}"
 MAKE_TARGETS="${MAKE_TARGETS:-}"
 TEST="${TEST:-False}"
 VERBOSE="${VERBOSE:-False}"
-DOCKER_TARGET="${DOCKER_TARGET:-test}"
 
-
-# Build the base
-# No build context required
-docker build -t claricpp:base - < base.docker
 
 # Get the version
 VERSION="$(cat ../VERSION)"
+
 
 # Build claricpp:<VERSION>
 docker build --target "${DOCKER_TARGET}" -t claricpp:"${VERSION}" \
