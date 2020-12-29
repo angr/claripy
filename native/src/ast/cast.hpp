d77d1a3fa0abf4ed8dfadfd5713be6e3bba7e4ae
zwimer <zwimer@gmail.com>
2020-12-29 16:38:57 -0700

    Add unsafe_down_cast to ast cast methods

--- native/src/ast/cast.hpp
+++ native/src/ast/cast.hpp
@@ -66,6 +66,26 @@ namespace AST {
         return ret;
     }
 
+    /** This function is used to statically up-cast between AST types
+     *  Normal dynamic casting will not work on because shared pointers are not subclasses of each
+     *  other like their template arguments are.
+     *  Since we are down-casting, this is not guaranteed to be typesafe
+     *  This function should only be used if f is guaranteed, by some other method, to be a To
+     *  When running in debug mode, this is an alias to down_cast_throw_on_fail
+     */
+    template <typename To, typename From> To unsafe_down_cast(const From &f) noexcept {
+#if DEBUG
+        return down_cast_throw_on_fail(f);
+#else
+        using RawTo = Private::Raw<To>;
+        using RawFrom = Private::Raw<From>;
+        static_assert(std::is_base_of<RawTypes::Base, RawFrom>::value,
+                      "From must derive from Base");
+        static_assert(std::is_base_of<RawFrom, RawTo>::value, "To must derive from From");
+        return std::static_pointer_cast<RawTo>(f);
+#endif
+    }
+
 } // namespace AST
 
 #endif
