04b7560b3545bd77cecf3487afdca03bd13bcc10
zwimer <zwimer@gmail.com>
2020-12-29 20:50:21 -0700

    Expose public interface.hpp and simplification.hpp

---
+++ native/src/interface/op.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * @brief This file exposes all relevant public members of Interface::Op
+ */
+#ifndef __INTERFACE_OP_HPP__
+#define __INTERFACE_OP_HPP__
+
+#include "op/if.hpp"
+#include "op/op.hpp"
+
+#endif
---
+++ native/src/simplification.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * @brief This file exposes all relevant public members of Simplification
+ */
+#ifndef __SIMPLIFICATION_HPP__
+#define __SIMPLIFICATION_HPP__
+
+#include "simplification/constants.hpp"
+#include "simplification/simplifiers.hpp"
+#include "simplification/simplify.hpp"
+
+#endif
