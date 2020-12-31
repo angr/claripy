6992137223bf4f0e9bcfe414ff7d8b1a8bd33df8
zwimer <zwimer@gmail.com>
2020-12-31 15:23:24 -0700

    rename errors dir error

---
+++ native/src/utils/error/claricpp.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file
+ * @brief This file contains the base claricpp exception
+ * These exceptions have python analogs and must be caught and sent to python
+ * via a different method.
+ * @todo Document method when known
+ */
+#ifndef __UTILS_ERRORS_CLARICPP_HPP__
+#define __UTILS_ERRORS_CLARICPP_HPP__
+
+#include "../to_str.hpp"
+
+#include <exception>
+#include <string>
+
+
+namespace Utils::Error {
+
+    class Claricpp : public std::exception {
+      public:
+        /** Public constructor
+         *  This constructor consumes its arguments via const reference
+         */
+        template <typename... Args> Claricpp(const Args &...args) : msg(Utils::to_str(args...)) {}
+
+        /** Message getter */
+        const char *what() const throw();
+
+      protected:
+        /** Delete default constructor */
+        Claricpp() = delete;
+
+      private:
+        /** The message */
+        const std::string msg;
+
+        /** Allow all error factories friend access */
+        template <typename T, typename S> friend T factory(const S msg);
+    };
+
+} // namespace Utils::Error
+
+#endif
