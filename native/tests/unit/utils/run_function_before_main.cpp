90ba2e2f66473e7725e28d92de5386db92ee7ed3
zwimer <zwimer@gmail.com>
2021-01-03 15:15:00 -0700

    Add run before main test cases

---
+++ native/tests/unit/utils/run_function_before_main.cpp
@@ -0,0 +1,27 @@
+/** @file */
+
+#include "testlib.hpp"
+#include "utils.hpp"
+
+
+/** A function to to be run before main */
+int f(int a = 0) {
+    static int ret = 0;
+    if (a != 0) {
+        return (ret = a);
+    }
+    else {
+        return ret;
+    }
+}
+
+
+// Run f(4) before main
+UTILS_RUN_FUNCTION_BEFORE_MAIN(f, 4);
+
+
+/** Ensure f(4) was run before main */
+int run_function_before_main() {
+    UNITTEST_ASSERT(f() == 4)
+    return 0;
+}
---
+++ native/tests/unit/utils/run_statement_before_main.cpp
@@ -0,0 +1,18 @@
+/** @file */
+
+#include "testlib.hpp"
+#include "utils.hpp"
+
+
+/** A global variable */
+int i = 0;
+
+// Run i = 4 before main
+UTILS_RUN_STATEMENT_BEFORE_MAIN(i = 4);
+
+
+/** Ensure f(4) was run before main */
+int run_statement_before_main() {
+    UNITTEST_ASSERT(i == 4)
+    return 0;
+}
