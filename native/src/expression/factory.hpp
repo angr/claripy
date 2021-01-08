193c900028e71679586400767821554e8b3d44e8
zwimer <zwimer@gmail.com>
2021-01-08 14:45:57 -0700

    Refactored factory

--- native/src/expression/factory.hpp
+++ native/src/expression/factory.hpp
@@ -15,14 +15,12 @@
 namespace Expression {
 
     /** A factory used to construct subclasses of Expression::Raw::Type::Base. Arguments are
-     * consumed. This function takes in move references for everything; it has no const promises,
-     *  it may consume anything that is passed to it. This factory handles hashing and
-     *  returns an Expression::Base (a shared pointer to the constructed object)
+     *  consumed. This function takes in move references for everything; it has no const
+     *  promises, it may consume anything that is passed to it. This factory handles hashing
+     *  and returns an Expression::Base (a shared pointer to the constructed object)
      *  @todo update eager_backends functionality
      */
-    template <typename T, typename... Args>
-    T factory(std::set<BackendID> &&eager_backends, Args &&...args) {
-        (void) eager_backends;
+    template <typename T, typename... Args> T factory(Args &&...args) {
 
         // Deduce the Expression::Raw::Type type the shared pointer type T contains
         using RawT = Private::Raw<T>;
@@ -34,7 +32,7 @@ namespace Expression {
 
         // Check to see if the object to be constructed exists in the hash cache
         // We run hash vis run_cr_function to ensure args are passed by const reference
-        const Hash h = Utils::run_cr_function(RawT::hash, args...);
+        const Hash h = Utils::run_cr_function(hash<RawT>, args...);
         auto base_ptr =
             Private::factory_cache.find_or_emplace<RawT>(h, std::forward<Args>(args)...);
         return down_cast_throw_on_fail<T>(base_ptr);
