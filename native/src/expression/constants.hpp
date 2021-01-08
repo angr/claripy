b0338c615381fae63858b44eb4820e255ad50237
zwimer <zwimer@gmail.com>
2021-01-08 14:46:08 -0700

    Created hash file

---
+++ native/src/expression/constants.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @brief This file defines constants used within the Expression namespace
+ */
+#ifndef __EXPRESSION_CONSTANTS_HPP__
+#define __EXPRESSION_CONSTANTS_HPP__
+
+#include "../constants.hpp"
+
+
+namespace Expression {
+
+    /** Define a type to store hashes */
+    using Hash = uint_fast64_t;
+
+} // namespace Expression
+
+
+#endif
