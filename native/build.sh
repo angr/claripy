36ef10f79a62ae21ae6cc9dcd1e197f6732c90dc
zwimer <zwimer@gmail.com>
2021-01-13 12:28:17 -0700

    Remove dead variable from build.sh

--- native/build.sh
+++ native/build.sh
@@ -8,7 +8,6 @@ DOCKER_TARGET="${DOCKER_TARGET:-test}"
 # Optional env arguments passed as --build-args to docker
 FORMAT="${FORMAT:-False}"
 CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Debug}"
-MAKE_TARGET="${MAKE_TARGET:-claricpp}"
 VERBOSE_TEST="${VERBOSE_TEST:-False}"
 
 
@@ -23,8 +22,6 @@ docker build --target "${DOCKER_TARGET}" -t claricpp:"${VERSION}" \
 	--build-arg FORMAT="${FORMAT}" \
 	`# CMake ` \
 	--build-arg CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
-	`# Make ` \
-	--build-arg MAKE_TARGET="${MAKE_TARGET}" \
 	`# Test ` \
 	--build-arg VERBOSE_TEST="${VERBOSE_TEST}" \
 	.
