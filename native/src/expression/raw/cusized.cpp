f9532b59465a5ffa3f1dd9935d8946f405e8b474
zwimer <zwimer@gmail.com>
2021-01-15 17:07:53 -0700

    Create sized class

---
+++ native/src/expression/raw/cusized.cpp
@@ -0,0 +1,10 @@
+#include "cusized.hpp"
+
+
+// For brevity
+using namespace Expression::Raw;
+
+
+CUSized::CUSized(const SizeT s) : size(s) {}
+
+CUSized::~CUSized() {}
---
+++ native/src/expression/raw/cusized.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * @brief This file defines a class that has a const size
+ */
+#ifndef __EXPRESSION_RAW_CUSIZED_HPP__
+#define __EXPRESSION_RAW_CUSIZED_HPP__
+
+#include "../../macros.hpp"
+#include "../../constants.hpp"
+
+
+namespace Expression::Raw {
+
+	/** A class with a const size */
+	struct CUSized {
+		DELETE_DEFAULTS(CUSized)
+
+			/** The size type */
+			using SizeT = Constants::UInt;
+
+		/** Constructor */
+		CUSized(const SizeT size);
+
+		/** Pure virtual destructor */
+		virtual ~CUSized()=0;
+
+		/** The size */
+		const SizeT size;
+	};
+
+}
+
+#endif
