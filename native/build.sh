c6773af64293dcafd0787801804763641b6b994e
zwimer <zwimer@gmail.com>
2021-01-19 19:11:41 -0700

    Allow building with any number of cores and overriding of docker buildkit

--- native/build.sh
+++ native/build.sh
@@ -9,14 +9,17 @@ DOCKER_TARGET="${DOCKER_TARGET:-test}"
 CXX_COMPILER="${CXX_COMPILER:-g++}"
 FORMAT="${FORMAT:-False}"
 CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Debug}"
+NUM_CORES="${NUM_CORES:-"$(nproc)"}"
 VERBOSE_TEST="${VERBOSE_TEST:-False}"
 
 
 # Get the version
 VERSION="$(cat ../VERSION)"
 
-# Use docker build kit (only builds dependent stages among other benefits)
-export DOCKER_BUILDKIT=1
+# Use docker build kit by default (only builds dependent stages among other benefits)
+if [[ -z "${DOCKER_BUILDKIT:-}" ]]; then
+	export DOCKER_BUILDKIT=1
+fi
 
 # Build claricpp:<VERSION>
 docker build --target "${DOCKER_TARGET}" -t claricpp:"${VERSION}" \
@@ -27,6 +30,8 @@ docker build --target "${DOCKER_TARGET}" -t claricpp:"${VERSION}" \
 	--build-arg FORMAT="${FORMAT}" \
 	`# CMake ` \
 	--build-arg CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+	`# Build ` \
+	--build-arg NUM_CORES="${NUM_CORES}" \
 	`# Test ` \
 	--build-arg VERBOSE_TEST="${VERBOSE_TEST}" \
 	.
