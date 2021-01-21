34f896de3201435c0a6ba79514d2a01ee4c05922
zwimer <zwimer@gmail.com>
2021-01-20 19:58:08 -0700

    Allow building with make verbose

--- native/build.sh
+++ native/build.sh
@@ -9,6 +9,7 @@ DOCKER_TARGET="${DOCKER_TARGET:-test}"
 CXX_COMPILER="${CXX_COMPILER:-g++}"
 FORMAT="${FORMAT:-False}"
 CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Debug}"
+CMAKE_VERBOSE_MAKEFILE="${CMAKE_VERBOSE_MAKEFILE:-OFF}"
 NUM_CORES="${NUM_CORES:-"$(nproc)"}"
 VERBOSE_TEST="${VERBOSE_TEST:-False}"
 
@@ -30,6 +31,7 @@ docker build --target "${DOCKER_TARGET}" -t claricpp:"${VERSION}" \
 	--build-arg FORMAT="${FORMAT}" \
 	`# CMake ` \
 	--build-arg CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+	--build-arg CMAKE_VERBOSE_MAKEFILE="${CMAKE_VERBOSE_MAKEFILE}" \
 	`# Build ` \
 	--build-arg NUM_CORES="${NUM_CORES}" \
 	`# Test ` \
