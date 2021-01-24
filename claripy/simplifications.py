de2a8dc260d6ca82dacc2a615939367b57ef4b09
Fish <fish@cs.ucsb.edu>
2021-01-24 01:21:01 -0700

    Clarify the docstring.

--- claripy/simplifications.py
+++ claripy/simplifications.py
@@ -912,14 +912,16 @@ class SimplificationManager:
         """
         This simplifier handles the following cases:
 
-            Extract(hi, 0, Concat(0, A)) == b, and
-            Extract(hi, 0, Concat(0, A)) != b, and
-            Extract(hi, 0, ZeroExt(n, A)) == b, and
-            Extract(hi, 0, ZeroExt(n, A)) != b
+            Extract(hi, 0, Concat(0, A)) op b, and
+            Extract(hi, 0, ZeroExt(n, A)) op b
 
         Extract can be eliminated if the high bits of Concat(0, A) or ZeroExt(n, A) are all zeros.
         """
-        if op not in (operator.__eq__, operator.__ne__) or b.op != "BVV" or a.op != "Extract" or a.args[1] != 0:
+
+        # TODO: This method is only used when op is __eq__ or __ne__. We need more experiment to see if it is necessary
+        # TODO: to apply this optimization to other comparison operators.
+
+        if a.op != "Extract" or a.args[1] != 0:
             return None
 
         highbits_are_zeros = False
