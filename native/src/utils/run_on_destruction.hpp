e8c0bec222e3315f49fef20bbc7fb7cc175e0a76
zwimer <zwimer@gmail.com>
2021-01-28 13:40:44 -0700

    ROD now takes in callable rather than using legacy bind invocation

--- native/src/utils/run_on_destruction.hpp
+++ native/src/utils/run_on_destruction.hpp
@@ -13,15 +13,18 @@
 
 namespace Utils {
 
-    /** Runs f(args...) when this is destructed unless disabled */
-    template <typename F, typename... Args> class RunOnDestruction {
+    /** Runs f() when this is destructed unless disabled
+     *  f() must have the type signature: void()
+     *  Note: We use the template to permit passing callable objects as well
+     *  As such, F can be the return of a lambda, or a callable object, for example
+     */
+    template <typename F> class [[nodiscard]] RunOnDestruction {
       public:
         /** Constructor
          *  Consumes args via move semantics
          *  enabled defaults to true
          */
-        explicit RunOnDestruction(const F &func, Args &&...args)
-            : enabled(true), f(std::bind(func, std::forward<Args>(args)...)) {}
+        explicit RunOnDestruction(const F &func) : enabled(true), f(func) {}
 
         /** Destructor */
         ~RunOnDestruction() {
