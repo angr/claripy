ea4b8e94b3adb39ed1f8c8fed4d0c63cbb7c4cde
zwimer <zwimer@gmail.com>
2021-02-01 18:22:11 -0700

    Hash top level include

---
+++ native/src/hash.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief This file includes all relevant public members of hash
+ */
+#ifndef __HASH_HPP__
+#define __HASH_HPP__
+
+#include "hash/hash.hpp"
+#include "hash/hashed.hpp"
+#include "hash/singular.hpp"
+#include "hash/type.hpp"
+
+#endif
