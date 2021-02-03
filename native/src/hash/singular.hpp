69c647cda069c2c738237b75634a033476c66d37
zwimer <zwimer@gmail.com>
2021-02-02 19:05:57 -0700

    Use constexpr function aliasing in hash fnv1a alias

--- native/src/hash/singular.hpp
+++ native/src/hash/singular.hpp
@@ -38,7 +38,8 @@ namespace Hash {
     }
 
     /** The FNV1a hash function to be invoked for for size sizeof(Hash) */
-    template <typename Type> const auto &fnv1a { Utils::FNV1a<Type>::template hash<Hash> };
+    template <typename Type>
+    const constexpr auto fnv1a { Utils::FNV1a<Type>::template hash<Hash> };
 
     // Specialization
 
