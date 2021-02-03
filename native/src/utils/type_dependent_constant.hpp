b71d93b112e510e9be986c7f4fbba1d4aa97bf91
zwimer <zwimer@gmail.com>
2021-02-03 01:32:59 -0700

    Cleanup

--- native/src/utils/type_dependent_constant.hpp
+++ native/src/utils/type_dependent_constant.hpp
@@ -7,29 +7,24 @@
 #define __UTILS_TYPEDEPENDENTCONSTANT_HPP__
 
 
-namespace Utils {
+namespace Utils::TD {
 
-    namespace TD {
+    /** A type-dependent constant type
+     *  Because templates are not evaluated until use, this can safely be placed
+     *  within a constexpr conditional static_assert without instantly trigging an assertion
+     *  failure. Instead, the static_assert will only fail if the line of code is reached
+     *  as until that point constant<bool, false, T> is an unevaluated expression, for example.
+     */
+    template <typename T, T value, typename...> constexpr T constant { value };
 
-        /** A type-dependent constant type
-         *  Because templates are not evaluated until use, this can safely be placed
-         *  within a constexpr conditional static_assert without instantly trigging an assertion
-         *  failure. Instead, the static_assert will only fail if the line of code is reached
-         *  as until that point constant<bool, false, T> is an unevaluated expression, for example.
-         */
-        template <typename T, T value, typename...> constexpr T constant { value };
+    // Abbreviations
 
-        // Abbreviations
+    /** A type dependent boolean */
+    template <bool Value, typename... _> constexpr bool boolean { constant<bool, Value, _...> };
 
-        /** A type dependent boolean */
-        template <bool value, typename... _>
-        constexpr bool boolean { constant<bool, value, _...> };
+    /** A type dependent false boolean */
+    template <typename... _> constexpr bool false_ { boolean<false, _...> };
 
-        /** A type dependent false boolean */
-        template <typename... _> constexpr bool false_ { boolean<false, _...> };
-
-    } // namespace TD
-
-} // namespace Utils
+} // namespace Utils::TD
 
 #endif
