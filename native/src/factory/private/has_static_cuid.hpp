a47d45c62fdd401aa3f59387782a94544f7eb578
zwimer <zwimer@gmail.com>
2021-02-03 01:45:19 -0700

    Comments

--- native/src/factory/private/has_static_cuid.hpp
+++ native/src/factory/private/has_static_cuid.hpp
@@ -16,8 +16,12 @@ namespace Factory::Private {
     template <typename T, typename = int> struct HasStaticCUID : std::false_type {};
 
     /** Used to determine if T has a static Constants::UInt called static_cuid
+     *  True case
      *  The comma operator returns the second item, so "(A, B)" returns B
-     *  If static_cuid exists, then this specalization is prioritized over the general definition
+     *  If static_cuid does not exist, T::static_cuid fails is not resolvable so this
+     * specialization is rejected (via SFINAE) in favor if the previous, more general, definition.
+     *  If static_cuid exists, then (T::static_cuid,  0) returns 0, and we have specialized
+     *  with int = 0. Note that specializations are prioritized over the general definition
      */
     template <typename T>
     struct HasStaticCUID<T, decltype((void) T::static_cuid, 0)> : std::true_type {};
