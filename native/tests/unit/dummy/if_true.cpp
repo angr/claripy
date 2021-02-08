c2f95dba3890a60a7ad4128796944e572dfe17cc
zwimer <zwimer@gmail.com>
2021-02-08 13:16:40 -0700

    Add dummy tests

---
+++ native/tests/unit/dummy/if_true.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * \ingroup unittest
+ */
+#include "dummy.hpp"
+#include "testlib.hpp"
+
+
+/** Verify that the if_true class works */
+void if_true() {
+    const auto b = UnitTest::TestLib::Factories::t_literal<Expression::Bool>();
+
+    Dummy::echo(true, true);
+    UNITTEST_ASSERT(b->is_true())
+    UNITTEST_ASSERT(!b->is_false())
+
+    Dummy::echo(true, false);
+    UNITTEST_ASSERT(!b->is_true())
+    UNITTEST_ASSERT(b->is_true())
+}
+
+// Define the test
+UNITTEST_DEFINE_MAIN_TEST(if_true)
