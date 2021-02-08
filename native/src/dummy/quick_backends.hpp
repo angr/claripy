d8843093c4013eca35c8062e198e926069e68739
zwimer <zwimer@gmail.com>
2021-02-08 13:16:06 -0700

    Add dummy backend

--- native/src/dummy/quick_backends.hpp
+++ native/src/dummy/quick_backends.hpp
@@ -5,21 +5,34 @@
 #ifndef __DUMMY_QUICKBACKENDS_HPP__
 #define __DUMMY_QUICKBACKENDS_HPP__
 
+#include "../constants.hpp"
+
 #include <array>
 
+// Forward declarations
+namespace Expression {
+    class Bool;
+}
 
 namespace Dummy {
 
-    bool echo(const bool set = false, const bool value = true) {
+    // Echo the last value of value
+    inline bool echo(const bool set = false, const bool value = true) {
         static bool b = false;
         if (set) {
             b = value;
         }
         return b;
     }
-    bool echo_wrap() { return echo(); }
 
-    constexpr std::array<bool(), 1> quick_backends { echo_wrap };
+    // A dummy backend
+    struct Backend {
+        static bool is_true(Constants::CTSC<Expression::Bool>) { return echo(); }
+        static bool is_false(Constants::CTSC<Expression::Bool>) { return !echo(); }
+    };
+
+    // backends._quick_backends
+    constexpr std::array<Backend, 1> quick_backends { Backend {} };
 
 } // namespace Dummy
 
