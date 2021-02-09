d680719d574afd0b4921c388d289284d835d08bc
zwimer <zwimer@gmail.com>
2021-02-09 16:04:03 -0700

    Create::add test added

---
+++ native/tests/unit/create/add.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * \ingroup unittest
+ */
+#include "create.hpp"
+#include "testlib.hpp"
+
+
+/** Verify that the add function compiles and can be run without issue */
+void add() {
+
+    // Create input
+    std::vector<Factory::Ptr<Expression::Base>> input;
+    for (int i = 0; i < 4; ++i) { // NOLINT
+        input.push_back(UnitTest::TestLib::Factories::t_literal<Expression::BV>());
+    }
+
+    (void) Create::add<Expression::BV>(std::move(input), Expression::Base::AnnotationVec {});
+}
+
+// Define the test
+UNITTEST_DEFINE_MAIN_TEST(add)
