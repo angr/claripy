b06babb6d1ae706c8aee1fdca90be1c3ed9defd1
zwimer <zwimer@gmail.com>
2021-02-09 16:23:44 -0700

    Added all flat ops

--- native/src/op/trivial.hpp
+++ native/src/op/trivial.hpp
@@ -12,7 +12,8 @@
 // Forward declarations
 namespace Expression {
     class Bits;
-}
+    class BV;
+} // namespace Expression
 
 namespace Op {
 
@@ -22,6 +23,18 @@ namespace Op {
 
     /** The op class: Add */
     OP_FLAT_TRIVIAL_SUBCLASS(Add)
+
+    /** The op class: And */
+    OP_FLAT_TRIVIAL_SUBCLASS(And)
+
+    /** The op class: Or */
+    OP_FLAT_TRIVIAL_SUBCLASS(Or)
+
+    /** The op class: Xor */
+    OP_FLAT_TRIVIAL_SUBCLASS(Xor, Expression::BV)
+
+    /** The op class: Mul */
+    OP_FLAT_TRIVIAL_SUBCLASS(Mul, Expression::Bits)
 
     /**********************************************************/
     /*                   Binary Subclasses                    */
