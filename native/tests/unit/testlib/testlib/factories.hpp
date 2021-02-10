bbd2ba24e0db872e299575772e40fbe02a9cc23d
zwimer <zwimer@gmail.com>
2021-02-10 16:37:10 -0700

    Update testlib to use baseptr

--- native/tests/unit/testlib/testlib/factories.hpp
+++ native/tests/unit/testlib/testlib/factories.hpp
@@ -9,6 +9,7 @@
 #include "annotation.hpp"
 #include "expression.hpp"
 #include "factory.hpp"
+#include "op.hpp"
 #include "utils.hpp"
 
 
@@ -18,7 +19,7 @@ namespace UnitTest::TestLib::Factories {
     inline auto symbol(std::string name = std::string { "hi" }) { Op::factory<Op::Symbol>(name); }
 
     /** Create a simple Literal */
-    inline Factory::Ptr<Op::Literal> literal(const Constants::Int i = 0) {
+    inline Op::BasePtr literal(const Constants::Int i = 0) {
         const constexpr Constants::UInt size { sizeof(Constants::Int) };
         char buf[size]; // NOLINT
         std::memcpy(buf, &i, size);
@@ -27,18 +28,19 @@ namespace UnitTest::TestLib::Factories {
     }
 
     /** Make it easier to create expressions */
-    template <typename T = Expression::Int> Factory::Ptr<T> t_literal(const Constants::Int i = 0) {
+    template <typename T = Expression::Int>
+    Expression::BasePtr t_literal(const Constants::Int i = 0) {
         static_assert(std::is_base_of_v<Expression::Base, T>,
                       "T must derive from Expression::Base");
         auto ans { std::vector<Factory::Ptr<Annotation::Base>> {} };
         auto op { literal(i) };
         auto baseop { Utils::up_cast<Op::Base>(op) };
         if constexpr (std::is_base_of_v<Expression::Bits, T>) {
-            return Expression::factory<T>(false, std::move(baseop), std::move(ans),
+            return Expression::factory<T>(std::move(ans), false, std::move(baseop),
                                           Constants::UInt { sizeof(i) });
         }
         else {
-            return Expression::factory<T>(false, std::move(baseop), std::move(ans));
+            return Expression::factory<T>(std::move(ans), false, std::move(baseop));
         }
     }
 
