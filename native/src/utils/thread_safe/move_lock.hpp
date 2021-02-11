1c17934d7f9a1806c52fd2c854fd3ade2e044d73
zwimer <zwimer@gmail.com>
2021-02-10 17:09:45 -0700

    movelock created

---
+++ native/src/utils/thread_safe/move_lock.hpp
@@ -0,0 +1,70 @@
+/**
+ * @file
+ * \ingroup utils
+ * @brief This file defines a move-able lock
+ */
+#ifndef __UTILS_THREADSAFE_MOVELOCK_HPP__
+#define __UTILS_THREADSAFE_MOVELOCK_HPP__
+
+#include "../../macros.hpp"
+
+#include <shared_mutex>
+
+
+namespace Utils::ThreadSafe {
+
+    /** A move-able scoped lock
+     *  If Mutex is not a shareable mutex, Shared must be false
+     *  Compilation requires that if Shared then the class
+     *  Mutex must implement lock_shared and unlock_shared
+     */
+    template <typename Mutex, bool Shared = false> class [[nodiscard]] MoveLock final {
+      public:
+        /** Constructor
+         *  Lock on construction
+         *  Shared lock if Shared, else exclusive lock
+         */
+        MoveLock(std::shared_mutex *const m) : mutex { m } {
+            if constexpr (Shared) {
+                mutex->lock_shared();
+            }
+            else {
+                mutex->lock();
+            }
+        }
+
+        /** Move constructor
+         *  Disables the auto-locking on destruction of old
+         */
+        MoveLock(MoveLock &&old) : mutex { old.mutex } { old.mutex = nullptr; }
+
+        /** Destructor
+         *  On destruction, unlock if the mutex pointer is valid
+         */
+        ~MoveLock() {
+            if (mutex) {
+                if constexpr (Shared) {
+                    mutex->unlock_shared();
+                }
+                else {
+                    mutex->unlock();
+                }
+            }
+        }
+
+      private:
+        /** The mutex to lock */
+        Mutex *mutex;
+
+        /** Delete copy constructor */
+        MoveLock(const MoveLock &) = delete;
+        /** Delete default constructor */
+        MoveLock() = delete;
+
+        // Disable assignment
+        SET_IMPLICITS_OPERATORS(MoveLock, delete);
+    };
+
+} // namespace Utils::ThreadSafe
+
+#endif
