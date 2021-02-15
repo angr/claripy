3cc7e3b52d74d0a40182daf8498b1b2a6e45fce6
zwimer <zwimer@gmail.com>
2021-02-15 15:39:53 -0700

    Vastly improve abs create test

--- native/tests/unit/create/abs.cpp
+++ native/tests/unit/create/abs.cpp
@@ -13,7 +13,22 @@ void abs() {
     const auto a { UnitTest::TestLib::Factories::t_literal<Expression::BV>() };
 
     // Test
-    (void) Create::abs<Expression::BV>(Create::EAnVec {}, a);
+    const auto exp { Create::abs<Expression::BV>(Create::EAnVec {}, a) };
+
+    // Pointer checks
+    UNITTEST_ASSERT(a.use_count() == 2)
+    UNITTEST_ASSERT(exp->op.use_count() == 1)
+
+    // Type check
+    const auto exp_bv { Utils::dynamic_down_cast_throw_on_fail<Expression::BV>(exp) };
+    const auto a_bv { Utils::dynamic_down_cast_throw_on_fail<Expression::BV>(a) };
+    const auto unary { Utils::dynamic_down_cast_throw_on_fail<Op::Abs>(exp->op) };
+
+    // Contains check
+    UNITTEST_ASSERT(unary->child == a)
+
+    // Size test
+    UNITTEST_ASSERT(a_bv->size == exp_bv->size)
 }
 
 // Define the test
