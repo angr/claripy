17d78c6817b948791af85ec70d04dc03b67af997
zwimer <zwimer@gmail.com>
2021-02-16 16:16:28 -0700

    Update test binary to allow different in out types

--- native/tests/unit/create/binary.hpp
+++ native/tests/unit/create/binary.hpp
@@ -15,13 +15,20 @@ using SM = Create::Private::SizeMode;
 
 
 /** Test a binary op */
-template <typename T, typename OpT, SM Mode, auto CreateF> inline void binary() {
-    static_assert(Utils::is_ancestor<Expression::Base, T>, "binary requires T be an Expression");
+template <typename Out, typename In, typename OpT, SM Mode, auto CreateF> inline void binary() {
+    static_assert(Utils::is_ancestor<Expression::Base, Out>,
+                  "binary requires Out be an Expression");
+    static_assert(Utils::is_ancestor<Expression::Base, In>, "binary requires In be an Expression");
     static_assert(Op::is_binary<OpT>, "binary requires a binary OpT");
+    if constexpr (Utils::is_ancestor<Expression::Bits, Out>) {
+        const constexpr bool sized_in { Utils::is_ancestor<Expression::Bits, In> };
+        static_assert(Utils::TD::boolean<sized_in, In>,
+                      "binary does not suppot sized output types without sized input types");
+    }
 
     // Create distinct inputs
-    const auto a { UnitTest::TestLib::Factories::t_literal<T>(0) };
-    const auto b { UnitTest::TestLib::Factories::t_literal<T>(1) };
+    const auto a { UnitTest::TestLib::Factories::t_literal<In>(0) };
+    const auto b { UnitTest::TestLib::Factories::t_literal<In>(1) };
 
     // Test
     const auto exp { CreateF(Create::EAnVec {}, a, b) };
@@ -33,16 +40,17 @@ template <typename T, typename OpT, SM Mode, auto CreateF> inline void binary() 
 
     // Type check
     const auto binary { Utils::dynamic_down_cast_throw_on_fail<OpT>(exp->op) };
-    const auto exp_down { Utils::dynamic_down_cast_throw_on_fail<T>(exp) };
-    const auto a_down { Utils::dynamic_down_cast_throw_on_fail<T>(a) };
-    const auto b_down { Utils::dynamic_down_cast_throw_on_fail<T>(b) };
+    const auto exp_down { Utils::dynamic_down_cast_throw_on_fail<Out>(exp) };
+    const auto a_down { Utils::dynamic_down_cast_throw_on_fail<In>(a) };
+    const auto b_down { Utils::dynamic_down_cast_throw_on_fail<In>(b) };
 
     // Contains check
     UNITTEST_ASSERT(binary->left == a)
     UNITTEST_ASSERT(binary->right == b)
 
     // Size test
-    if constexpr (Utils::is_ancestor<Expression::Bits, T>) {
+    if constexpr (Utils::is_ancestor<Expression::Bits, Out>) {
+        // Because of previous static asserts we know In must also be sized
         Constants::UInt size { a_down->size };
         if constexpr (Mode == SM::Add) {
             size += b_down->size;
@@ -52,6 +60,11 @@ template <typename T, typename OpT, SM Mode, auto CreateF> inline void binary() 
         }
         UNITTEST_ASSERT(exp_down->size == size)
     }
+}
+
+/** A specialization where In = Out */
+template <typename InOut, typename OpT, SM Mode, auto CreateF> inline void binary() {
+    binary<InOut, InOut, OpT, Mode, CreateF>();
 }
 
 #endif
