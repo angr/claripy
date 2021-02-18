f95b52f74fd38dc5132853e5eeaa51df430da27b
zwimer <zwimer@gmail.com>
2021-02-18 15:50:01 -0700

    New fp and string ops added

--- native/src/op/fp/trivial.hpp
+++ native/src/op/fp/trivial.hpp
@@ -7,29 +7,59 @@
 
 #include "mode_binary.hpp"
 
+#include "../binary.hpp"
 #include "../ternary.hpp"
+#include "../unary.hpp"
 
 
 namespace Op::FP {
 
     /**********************************************************/
+    /*                    Unary Subclasses                    */
+    /**********************************************************/
+
+    /** The unary fp op class: FP::ToIEEEBV */
+    OP_UNARY_TRIVIAL_SUBCLASS(ToIEEEBV);
+
+    /** The unary fp op class: FP::IsInf */
+    OP_UNARY_TRIVIAL_SUBCLASS(IsInf);
+
+    /** The unary fp op class: FP::IsNaN */
+    OP_UNARY_TRIVIAL_SUBCLASS(IsNaN);
+
+    /**********************************************************/
     /*               FP Mode Binary Subclasses                */
     /**********************************************************/
 
-    /** The op class: FP::Add */
+    /** The op class: FP::NE
+     *  Input sizes must be the same
+     */
+    OP_BINARY_TRIVIAL_SUBCLASS(NE, true);
+
+    /**********************************************************/
+    /*               FP Mode Binary Subclasses                */
+    /**********************************************************/
+
+    /** The op class: FP::Add
+     *  Input sizes may not differ
+     */
     OP_FP_MODEBINARY_TRIVIAL_SUBCLASS(Add);
 
-    /** The op class: FP::Sub */
+    /** The op class: FP::Sub
+     *  Input sizes may not differ
+     */
     OP_FP_MODEBINARY_TRIVIAL_SUBCLASS(Sub);
 
-    /** The op class: FP::Div */
+    /** The op class: FP::Div
+     *  Input sizes may not differ
+     */
     OP_FP_MODEBINARY_TRIVIAL_SUBCLASS(Div);
 
     /**********************************************************/
     /*                   Ternary Subclasses                   */
     /**********************************************************/
 
-    /** The ternary op class: FP
+    /** The ternary op class: FP::FP
      *  Input sizes may differ
      */
     OP_TERNARY_TRIVIAL_SUBCLASS(FP, false);
--- native/src/op/string/trivial.hpp
+++ native/src/op/string/trivial.hpp
@@ -16,24 +16,30 @@ namespace Op::String {
     /*                    Unary Subclasses                    */
     /**********************************************************/
 
-    /** The unary string op class: IsDigit */
+    /** The unary string op class: String::IsDigit */
     OP_UNARY_TRIVIAL_SUBCLASS(IsDigit);
+
+    /** The unary string op class: String::FromInt */
+    OP_UNARY_TRIVIAL_SUBCLASS(FromInt);
+
+    /** The unary string op class: String::Unit */
+    OP_UNARY_TRIVIAL_SUBCLASS(Unit);
 
     /**********************************************************/
     /*                   Binary Subclasses                    */
     /**********************************************************/
 
-    /** The string binary op class: Contains
+    /** The string binary op class: String::Contains
      *  Input sizes may differ
      */
     OP_BINARY_TRIVIAL_SUBCLASS(Contains, false);
 
-    /** The string binary op class: PrefixOf
+    /** The string binary op class: String::PrefixOf
      *  Input sizes may differ
      */
     OP_BINARY_TRIVIAL_SUBCLASS(PrefixOf, false);
 
-    /** The string binary op class: SuffixOf
+    /** The string binary op class: String::SuffixOf
      *  Input sizes may differ
      */
     OP_BINARY_TRIVIAL_SUBCLASS(SuffixOf, false);
@@ -42,7 +48,7 @@ namespace Op::String {
     /*                   Ternary Subclasses                   */
     /**********************************************************/
 
-    /** The ternary string op class: Replace
+    /** The ternary string op class: String::Replace
      *  Input sizes may differ
      */
     OP_TERNARY_TRIVIAL_SUBCLASS(Replace, false);
