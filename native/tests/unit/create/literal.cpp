55ce462c3151ffa455b8e52210bc6c7ba017b9b9
zwimer <zwimer@gmail.com>
2021-02-23 15:35:49 -0700

    Symbol and literal tests greatly improved

--- native/tests/unit/create/literal.cpp
+++ native/tests/unit/create/literal.cpp
@@ -3,20 +3,58 @@
  * \ingroup unittest
  */
 #include "create.hpp"
+#include "dcast.hpp"
 #include "testlib.hpp"
 
 
-/** Verify that the literal function compiles and can be run without issue */
-void literal() {
+/** Test the literal create function with type T */
+template <typename T> void literal_t() {
+
+    // For brevity
+    namespace F = UnitTest::TestLib::Factories;
+    namespace Ex = Expression;
 
     // Create inputs
-    std::string data1 { "This is data!" };
-    std::string data2 { data1 };
-    const auto bl { 100 + BitLength::char_bit * data1.size() };
+    std::string data { "This is data!" };
+    const std::string data_copy { data };
+    const auto size { BitLength::char_bit * data.size() };
 
     // Test
-    (void) Create::literal<Expression::Bool>(Create::EAnVec {}, std::move(data1));
-    (void) Create::literal<Expression::BV>(Create::EAnVec {}, std::move(data2), bl);
+    Expression::BasePtr lit;
+    if constexpr (Utils::is_ancestor<Ex::Bits, T>) {
+        lit = Create::literal<T>(Create::EAnVec {}, std::move(data), size);
+    }
+    else {
+        lit = Create::literal<T>(Create::EAnVec {}, std::move(data));
+    }
+
+    // Pointer checks
+    UNITTEST_ASSERT(lit.use_count() == 1)
+    UNITTEST_ASSERT(lit->op.use_count() == 1)
+
+    // Symbolic check
+    UNITTEST_ASSERT(not lit->symbolic)
+
+    // Type check
+    const auto op_down { dcast<Op::Literal>(lit->op) };
+    const auto exp_down { dcast<T>(lit) };
+
+    // Contains check
+    UNITTEST_ASSERT(op_down->value == data_copy)
+
+    // Size check
+    if constexpr (Utils::is_ancestor<Ex::Bits, T>) {
+        UNITTEST_ASSERT(exp_down->bit_length == size)
+    }
+}
+
+/** Test the literal create function */
+void literal() {
+    literal_t<Expression::Bool>();
+    literal_t<Expression::String>();
+    literal_t<Expression::BV>();
+    literal_t<Expression::VS>();
+    literal_t<Expression::FP>();
 }
 
 // Define the test
--- native/tests/unit/create/symbol.cpp
+++ native/tests/unit/create/symbol.cpp
@@ -3,17 +3,59 @@
  * \ingroup unittest
  */
 #include "create.hpp"
+#include "dcast.hpp"
 #include "testlib.hpp"
 
 
-/** Verify that the symbol function compiles and can be run without issue */
+/** Test the symbol create function with type T */
+template <typename T> void symbol_t() {
+    static int n_runs = 0;
+
+    // For brevity
+    namespace F = UnitTest::TestLib::Factories;
+    namespace Ex = Expression;
+
+    // Create name
+    std::string name { std::to_string(++n_runs) };
+    const std::string name_copy { name };
+    const Constants::UInt size { 0x10 };
+
+    // Test
+    Expression::BasePtr sym;
+    if constexpr (Utils::is_ancestor<Ex::Bits, T>) {
+        sym = Create::symbol<T>(Create::EAnVec {}, std::move(name), size);
+    }
+    else {
+        sym = Create::symbol<T>(Create::EAnVec {}, std::move(name));
+    }
+
+    // Pointer checks
+    UNITTEST_ASSERT(sym.use_count() == 1)
+    UNITTEST_ASSERT(sym->op.use_count() == 1)
+
+    // Symbolic check
+    UNITTEST_ASSERT(sym->symbolic)
+
+    // Type check
+    const auto op_down { dcast<Op::Symbol>(sym->op) };
+    const auto exp_down { dcast<T>(sym) };
+
+    // Contains check
+    UNITTEST_ASSERT(op_down->name == name_copy)
+
+    // Size check
+    if constexpr (Utils::is_ancestor<Ex::Bits, T>) {
+        UNITTEST_ASSERT(exp_down->bit_length == size)
+    }
+}
+
+/** Test the symbol create function */
 void symbol() {
-
-    // Non-Bits subclass test
-    (void) Create::symbol<Expression::Bool>(Create::EAnVec {}, "mysym");
-
-    // Bits subclass test
-    (void) Create::symbol<Expression::BV>(Create::EAnVec {}, "mysym2", 10);
+    symbol_t<Expression::Bool>();
+    symbol_t<Expression::String>();
+    symbol_t<Expression::BV>();
+    symbol_t<Expression::VS>();
+    symbol_t<Expression::FP>();
 }
 
 // Define the test
