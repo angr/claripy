df585a9b8e4596779c140ac3f12e7efcf2cec202
zwimer <zwimer@gmail.com>
2021-04-15 16:48:44 -0700

    is_strong_enum

---
+++ native/src/utils/is_strong_enum.hpp
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * \ingroup utils
+ * @brief An implementation of C++23's std::is_scoped_enum
+ */
+#ifndef __UTILS_ISSTRONGENUM_HPP__
+#define __UTILS_ISSTRONGENUM_HPP__
+
+#include "macros.hpp"
+
+#include <type_traits>
+
+namespace Utils {
+
+    namespace Private {
+
+        /** Determine if T is a strong enum: default false case
+         *  We implicitly leverage SFINAE here
+         */
+        template <typename T, bool = std::is_enum_v<T>>
+        struct IsStrongEnum final : std::false_type {};
+
+        /** Determine if T is a strong enum: possible true case
+         *  We implicitly leverage SFINAE here
+         */
+        template <typename T>
+        struct IsStrongEnum<T, true> final :
+            std::bool_constant<!std::is_convertible_v<T, std::underlying_type_t<T>>> {};
+
+    } // namespace Private
+
+    /** True if T is a strong enum */
+    template <typename T> UTILS_ICCBOOL is_strong_enum { Private::IsStrongEnum<T>::value };
+} // namespace Utils
+
+#endif
--- native/src/utils/private/ostream_helper_conversions.hpp
+++ native/src/utils/private/ostream_helper_conversions.hpp
@@ -9,6 +9,7 @@
 #define __UTILS_PRIVATE_OSTREAMHELPERCONVERSIONS_HPP__
 
 #include "../../macros.hpp"
+#include "../is_strong_enum.hpp"
 #include "../sfinae_test.hpp"
 
 #include <ostream>
@@ -16,19 +17,6 @@
 
 
 namespace Utils::Private {
-
-    /** Determine if T is a strong enum: default false case
-     *  We implicitly leverage SFINAE here
-     */
-    template <typename T, bool = std::is_enum_v<T>> struct IsStrongEnum final : std::false_type {};
-
-    /** Determine if T is a strong enum: possible true case
-     *  We implicitly leverage SFINAE here
-     */
-    template <typename T>
-    struct IsStrongEnum<T, true> final :
-        std::bool_constant<!std::is_convertible_v<T, std::underlying_type_t<T>>> {};
-
 
     /** A struct which determines if T has a << stream op defined */
     UTILS_SFINAETEST(has_stream_op, // Invoke this
@@ -40,20 +28,20 @@ namespace Utils::Private {
 
     /** True if and only if T is a strong enum with no << operator defined */
     template <typename T>
-    UTILS_ICCBOOL needs_ostream_conversion { IsStrongEnum<T>::value && !has_stream_op<T> };
+    UTILS_ICCBOOL needs_ostream_conversion { is_strong_enum<T> && !has_stream_op<T> };
 
 
     /** If T is a strong enum with no stream operator defined, static cast to its underlying type
      */
     template <typename T, std::enable_if_t<needs_ostream_conversion<T>, int> = 0>
-    [[gnu::always_inline]] constexpr std::underlying_type_t<T>
+    [[gnu::always_inline]] constexpr inline std::underlying_type_t<T>
     ostream_helper_conversions(const T &v) noexcept {
         return static_cast<std::underlying_type_t<T>>(v);
     }
 
     /** This specalization is a no-op */
     template <typename T, std::enable_if_t<!needs_ostream_conversion<T>, int> = 0>
-    [[gnu::always_inline]] constexpr T &ostream_helper_conversions(T &t) noexcept {
+    [[gnu::always_inline]] constexpr inline T &ostream_helper_conversions(T &t) noexcept {
         return t;
     }
 
