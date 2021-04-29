c2545d0ac318195d3f0d6a2b08047e8e952cc77b
zwimer <zwimer@gmail.com>
2021-04-29 12:44:57 -0700

    To underlying

---
+++ native/src/utils/to_underlying.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * \ingroup utils
+ * @brief This file defines to_underlying
+ * We use the word affirm since C libraries like to define assert as a macro
+ */
+#ifndef __UTILS_TOUNDERLYING_HPP__
+#define __UTILS_TOUNDERLYING_HPP__
+
+#include "is_strong_enum.hpp"
+
+
+namespace Utils {
+
+    /** Convert e to its underlying type */
+    template <typename E> constexpr std::underlying_type_t<E> to_underlying(const E e) {
+        static_assert(std::is_enum_v<E>, "Requires E be an enum");
+        static_assert(is_strong_enum<E>, "Strong enums are preferred to weak enums");
+        return static_cast<std::underlying_type_t<E>>(e);
+    }
+
+} // namespace Utils
+
+#endif
