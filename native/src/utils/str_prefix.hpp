8a86844d6f7b647e29d9a3a42d5d95e00d66c139
zwimer <zwimer@gmail.com>
2021-05-06 20:51:14 -0700

    str_prefix

---
+++ native/src/utils/str_prefix.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * \ingroup utils
+ * @brief This file defines the str_prefix function
+ */
+#ifndef __UTILS_STRPREFIX_HPP__
+#define __UTILS_STRPREFIX_HPP__
+
+#include "strlen.hpp"
+
+
+namespace Utils {
+
+	/** Return true if a starts with b */
+	constexpr bool str_prefix(Constants::CCS a, Constants::CCS b) {
+		if (Utils::strlen(a) < Utils::strlen(b)) {
+			return false;
+		}
+		for(; *b; ++a, ++b) {
+			if (*a != *b) { return false; }
+		}
+		return true;
+	}
+}
+
+#endif
