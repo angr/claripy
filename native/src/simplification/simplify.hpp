e20156c00b7f82dfe9ff242fbcd88fd79fea7d52
zwimer <zwimer@gmail.com>
2021-05-20 17:27:47 -0700

    Simplify invoked simplficaction cache

--- native/src/simplification/simplify.hpp
+++ native/src/simplification/simplify.hpp
@@ -11,21 +11,30 @@
 
 namespace Simplification {
 
+    namespace Private {
+        /** Simplify old and return the result */
+        inline Expression::BasePtr simplify(const Factory::Ptr<Expression::Base> &old) {
+            if (const auto itr { op_map.find(old->op->cuid) }; itr != op_map.end()) {
+                return itr->second(old);
+            }
+            else {
+                Utils::Log::verbose(
+                    "Simplify found no suitable claricpp simplifier for the given expression");
+                return old;
+            }
+        }
+    } // namespace Private
+
     /** Simplify old and return the result */
     inline Expression::BasePtr simplify(const Factory::Ptr<Expression::Base> &old) {
-        if (const auto lookup { Private::cache.find(old->hash) }; lookup) {
+        if (auto lookup { Private::cache.find(old->hash) }; lookup) {
+            Utils::Log::verbose("Simplification cache hit");
             return lookup;
         }
-        else if (const auto itr { op_map.find(old->op->cuid) }; itr != op_map.end()) {
-            return itr->second(old);
-        }
-        else {
-            Utils::Log::verbose(
-                "Simplify found no suitable claricpp simplifier for the given expression");
-            return old;
-        }
+        auto ret { Private::simplify(old) };
+        cache(old->hash, ret);
+        return ret;
     }
-
 } // namespace Simplification
 
 #endif
