e780f34283191dda414e5955fbafda9c233d4a7f
zwimer <zwimer@gmail.com>
2021-06-02 13:29:27 -0700

    Clean up hash and increase speed

--- native/src/hash/hash.hpp
+++ native/src/hash/hash.hpp
@@ -21,19 +21,29 @@ namespace Hash {
      */
     template <typename... Args> constexpr Hash hash(const Args &...args) {
         constexpr Constants::UInt size { sizeof...(Args) };
-        Constants::UInt hashes[size]; // NOLINT
+        static_assert(size > 0, "hash needs arguments");
+
+        // Single item case
+        if constexpr (size == 1) {
+            return singular(args...);
+        }
+
+        // Variables
+        Hash hashes[size]; // NOLINT
         Constants::UInt i { -1ULL };
+
         // Basically: hashes[i] = singular(args[i]) for each arg
         (static_cast<void>(hashes[++i] = singular(args)), ...);
+
 #ifdef DEBUG
         // Verify no memory corruption
         Utils::affirm<Utils::Error::Unexpected::Unknown>(size == i + 1, WHOAMI_WITH_SOURCE
                                                          "Incorrect value of i within Hash::hash");
 #endif
-        // Return hash
-        using Hasher = Utils::FNV1a<Hash>; // The type being hashed is Hash
+
+        // Return a hash of the array of hashes
         // Since Hash is a fundamental numeric type, we specify the size as Hash
-        return Hasher::hash<Hash>(hashes, size);
+        return Utils::FNV1a<Hash>::hash<Hash>(hashes, size);
     }
 
 } // namespace Hash
--- native/src/hash/singular.hpp
+++ native/src/hash/singular.hpp
@@ -21,16 +21,9 @@
 namespace Hash {
 
     /** Converts its input into a Hash
-     *  Note: these do not have to be unique to all hashes, however
-     *  this function called on various inputs should avoid collsions.
-     *  This function exists to convert its input into Hash that will
-     *  later be hashed by a more secure hash function along with other arguments
-     *  For example, one use case of this function could be:
-     *  array = { singular(expression1), singular(name_string) }; return hash(array);
-     *  Since, for numeric types among others, this may be a no-op or very quick, we inline
-     *  specializations Note: Otherwise full specializations are put in a cpp file and linked later
+     *  Since this may be a no-op or very quick we inline full specializations
      *  Every type requries a specialization or is not supported!
-     *  Note: we do want to avoid inter-type collisions if possible, so we often add a line hash
+     *  Note: we want to avoid inter-type hash collisions so we often xor with a file line hash
      */
     template <typename T> constexpr Hash singular(const T &) noexcept {
         static_assert(Utils::TD::false_<T>,
@@ -39,14 +32,87 @@ namespace Hash {
     }
 
     /** The FNV1a hash function to be invoked for for size sizeof(Hash) */
-    template <typename Type>
-    const constexpr auto fnv1a { Utils::FNV1a<Type>::template hash<Hash> };
+    template <typename Type> constexpr auto fnv1a { Utils::FNV1a<Type>::template hash<Hash> };
 
-    // Specializations
+    /**************************************************/
+    /*           Primitive Specializations            */
+    /**************************************************/
 
-    /** A specialization for shared pointers to pre-hashed types
-     *  Not constexpr
+    /** A specialization for T = char */
+    template <> constexpr Hash singular(const char &c) noexcept {
+        return UTILS_FILE_LINE_HASH ^ static_cast<Hash>(c);
+    }
+
+    /** A specialization for T = float
+     *  \todo: For C++20 look into std::bit_cast to make this constexpr
      */
+    template <> inline Hash singular(const float &f) noexcept {
+        static_assert(sizeof(float) == sizeof(uint32_t),
+                      "Numeric type must have the same size as float");
+        uint32_t tmp; // NOLINT
+        UTILS_TYPE_PUN_ONTO(uint32_t, &tmp, &f, false);
+        return UTILS_FILE_LINE_HASH ^ tmp;
+    }
+
+    /** A specialization for T = double
+     *  \todo: For C++20 look into std::bit_cast to make this constexpr
+     */
+    template <> inline Hash singular(const double &d) noexcept {
+        Hash tmp; // NOLINT
+        UTILS_TYPE_PUN_ONTO(Hash, &tmp, &d, false);
+        return UTILS_FILE_LINE_HASH ^ tmp;
+    }
+
+    /** A specialization for T = bool */
+    template <> constexpr Hash singular(const bool &b) noexcept {
+        return UTILS_FILE_LINE_HASH ^ (b ? 1ULL : 0ULL);
+    }
+
+    /** A specialization for T = Mode::FP */
+    template <> constexpr Hash singular(const Mode::FP &m) noexcept {
+        using U = std::underlying_type_t<Mode::FP>;
+        static_assert(sizeof(m) <= sizeof(Hash), "singular(Mode::FP) must be modified");
+        static_assert(std::is_fundamental_v<U> && std::is_fundamental_v<Hash>,
+                      "singular(Mode::FP) must be modified");
+        return UTILS_FILE_LINE_HASH ^ static_cast<Hash>(Utils::to_underlying(m));
+    }
+
+    /** A specialization for T = Constants::CCSC */
+    template <> constexpr Hash singular(Constants::CCSC &s) noexcept {
+        return UTILS_FILE_LINE_HASH ^ fnv1a<char>(s, Utils::strlen(s));
+    }
+
+    /** A specialization for T = Constants::Int */
+    template <> constexpr Hash singular(const Constants::Int &i) noexcept {
+        static_assert(sizeof(Constants::Int) == sizeof(Hash),
+                      "singular(Constants::Int) must be modified");
+        static_assert(std::is_fundamental_v<Hash>, "singular(Constants::Int) must be modified");
+        // Unsafe for numerical reasons if Hash is unsigned. But we only
+        // care about uniqueness, so this is fine if the above hold
+        return UTILS_FILE_LINE_HASH ^ static_cast<Hash>(i);
+    }
+
+    /** A specialization for T = char[] */
+    template <std::size_t N> constexpr Hash singular(const char (&s)[N]) noexcept { // NOLINT
+        return UTILS_FILE_LINE_HASH ^ fnv1a<char>(s, N);
+    }
+
+    /** A specialization for T = Constants::UInt */
+    template <> constexpr Hash singular(const Constants::UInt &i) noexcept {
+        // Compiler will warn if this is unsafe or invalid
+        return UTILS_FILE_LINE_HASH ^ i;
+    }
+
+    /**************************************************/
+    /*                Specializations                 */
+    /**************************************************/
+
+    /** A specialization for T = std::string */
+    template <> inline Hash singular(const std::string &s) noexcept {
+        return UTILS_FILE_LINE_HASH ^ fnv1a<char>(s.c_str(), s.size());
+    }
+
+    /** A specialization for shared pointers to pre-hashed types */
     template <> inline Hash singular(const std::shared_ptr<const Hashed> &h) noexcept {
         if (h == nullptr) {
             return UTILS_FILE_LINE_HASH;
@@ -55,9 +121,7 @@ namespace Hash {
         return h->hash;
     }
 
-    /** A specialization for shared pointers of strict subclasses of Hashed types
-     *  Not constexpr
-     */
+    /** A specialization for shared pointers of strict subclasses of Hashed types */
     template <typename Internal,
               // Require to prevent infinite recursion
               std::enable_if_t<!Utils::is_wrap_same<Hashed, Internal, std::remove_cv_t>, int> = 0,
@@ -68,79 +132,7 @@ namespace Hash {
         return singular(Utils::up_cast<Hashed>(h));
     }
 
-    /** A specialization for T = char */
-    template <> constexpr inline Hash singular(const char &c) noexcept {
-        return UTILS_FILE_LINE_HASH + static_cast<Hash>(c);
-    }
-
-    /** A specialization for T = float */
-    template <> inline Hash singular(const float &f) noexcept {
-        static_assert(sizeof(float) == sizeof(uint32_t),
-                      "Numeric type must have the same size as float");
-        uint32_t tmp;
-        UTILS_TYPE_PUN_ONTO(uint32_t, &tmp, &f, false);
-        return UTILS_FILE_LINE_HASH + tmp;
-    }
-
-    /** A specialization for T = double */
-    template <> inline Hash singular(const double &d) noexcept {
-        Hash tmp;
-        UTILS_TYPE_PUN_ONTO(Hash, &tmp, &d, false);
-        return UTILS_FILE_LINE_HASH + tmp;
-    }
-
-    /** A specialization for T = bool */
-    template <> constexpr inline Hash singular(const bool &b) noexcept {
-        return UTILS_FILE_LINE_HASH + (b ? 1ULL : 0ULL);
-    }
-
-    /** A specialization for T = Mode::FP */
-    template <> constexpr inline Hash singular(const Mode::FP &m) noexcept {
-        using U = std::underlying_type_t<Mode::FP>;
-        static_assert(sizeof(m) <= sizeof(Hash), "singular(Mode::FP) must be modified");
-        static_assert(std::is_fundamental_v<U> && std::is_fundamental_v<Hash>,
-                      "singular(Mode::FP) must be modified");
-        return UTILS_FILE_LINE_HASH + static_cast<Hash>(Utils::to_underlying(m));
-    }
-
-    /** A specialization for T = Constants::CCSC */
-    template <> constexpr inline Hash singular(Constants::CCSC &s) noexcept {
-        return UTILS_FILE_LINE_HASH + fnv1a<char>(s, Utils::strlen(s));
-    }
-
-    /** A specialization for T = char[]
-     *  Not constexpr
-     */
-    template <std::size_t N> constexpr Hash singular(const char (&s)[N]) noexcept { // NOLINT
-        return UTILS_FILE_LINE_HASH + fnv1a<char>(s, N);
-    }
-
-    /** A specialization for T = std::string
-     *  Not constexpr
-     */
-    template <> inline Hash singular(const std::string &s) noexcept {
-        return UTILS_FILE_LINE_HASH + fnv1a<char>(s.c_str(), s.size());
-    }
-
-    /** A specialization for T = Constants::Int */
-    template <> constexpr inline Hash singular(const Constants::Int &i) noexcept {
-        static_assert(sizeof(Constants::Int) == sizeof(Hash),
-                      "singular(Constants::Int) must be modified");
-        static_assert(std::is_fundamental_v<Hash>, "singular(Constants::Int) must be modified");
-        // Unsafe for numerical reasons if Hash is unsigned. But we only
-        // care about uniqueness, so this is fine if the above hold
-        return UTILS_FILE_LINE_HASH + static_cast<Hash>(i);
-    }
-
-    /** A specialization for T = Constants::UInt */
-    template <> constexpr inline Hash singular(const Constants::UInt &i) noexcept {
-        // Compiler will warn if this is unsafe or invalid
-        return UTILS_FILE_LINE_HASH + i;
-    }
-
-    /** A specialization for T = std::vector<Internal>
-     *  Not constexpr
-     */
+    /** A specialization for T = std::vector<Internal> */
     template <typename Internal> inline Hash singular(const std::vector<Internal> &v) noexcept {
         Constants::UInt hashes[v.size()]; // NOLINT
         Constants::UInt i = -1ULL;
