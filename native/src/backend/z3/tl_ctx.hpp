20209e4e016dea68f17b628f1223e3e87dce4072
zwimer <zwimer@gmail.com>
2021-06-14 18:22:08 -0700

    Expose raw z3 context

--- native/src/backend/z3/tl_ctx.hpp
+++ native/src/backend/z3/tl_ctx.hpp
@@ -10,8 +10,15 @@
 
 namespace Backend::Z3::Private {
 
-    /** A thread local context all Z3 exprs should use */
-    inline thread_local z3::context tl_ctx;
+    /** A thread local context all Z3 exprs should use
+     *  This might be inline-able, but the tests cases might violate the ODR rules
+     */
+    thread_local z3::context tl_ctx;
+
+    /** A z3 thread local raw context that is the same as the z3::context
+     *  This might be inline-able, but the tests cases might violate the ODR rules
+     */
+    thread_local Z3_context tl_raw_ctx { tl_ctx };
 
 } // namespace Backend::Z3::Private
 
