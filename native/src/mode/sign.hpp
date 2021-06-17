4ecf17161f8d55101343abc946826b816cac8e08
zwimer <zwimer@gmail.com>
2021-06-17 13:57:36 -0700

    Signed modes updates

--- native/src/mode/sign.hpp
+++ native/src/mode/sign.hpp
@@ -5,13 +5,24 @@
 #ifndef R_MODE_SIGN_HPP_
 #define R_MODE_SIGN_HPP_
 
-namespace Mode {
+#include "../utils.hpp"
 
-    /** An Sign enum with an underlying signed value
+
+namespace Mode::Sign {
+
+    /** An Sign enum with an underlying signed value where non-signed-ness is disallowed
      *  The underlying values of the enum values are 1 and -1
      */
-    enum class Sign : char { Minus = -1, Plus = 1 };
+    enum class FP : char { Minus = -1, Plus = 1 };
 
-} // namespace Mode
+    /** An Sign enum with an underlying signed value where non-signed-ness is allowed
+     *  The underlying values of the enum values are 1, -1, and 0
+     */
+    enum class Real : char { Minus = -1, None = 0, Plus = 1 };
+
+    /** Convert an FP to a Real */
+    constexpr Real to_real(const FP f) { return static_cast<Real>(Utils::to_underlying(f)); }
+
+} // namespace Mode::Sign
 
 #endif
