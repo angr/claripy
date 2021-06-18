d63b079e86f91e2788f238bf513da59ca6da562c
zwimer <zwimer@gmail.com>
2021-06-18 13:13:43 -0700

    Symbol translocation data type added

--- native/src/backend/z3/constants.hpp
+++ native/src/backend/z3/constants.hpp
@@ -5,10 +5,21 @@
 #ifndef R_BACKEND_Z3_CONSTANTS_HPP_
 #define R_BACKEND_Z3_CONSTANTS_HPP_
 
+#include "../../expression.hpp"
+
+#include <map>
+#include <string>
+
+
 namespace Backend::Z3 {
 
     /** The z3 unsigned type */
     using z3u = unsigned;
+
+    /** A map used for translocating annotations between symbols
+     *  It assists in translocations between pre-conversion and post-abstraction expressions
+     */
+    using SymAnTransData = std::map<std::string, Expression::Base::SPAV>;
 
 } // namespace Backend::Z3
 
