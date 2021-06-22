39942d79aebd051d36295f7cc359c5b0405edc95
zwimer <zwimer@gmail.com>
2021-06-22 14:55:52 -0700

    Cleanup

--- native/tests/unit/backend/z3/abstract.cpp
+++ native/tests/unit/backend/z3/abstract.cpp
@@ -12,20 +12,13 @@
 void abstract() {
     auto z3 { Backend::Z3::Z3 {} };
 
-#if 0
-
-	auto f = Create::literal(3425.f);
-	auto c = z3.convert(f);
-	(void) z3.abstract(c);
-
-#else
     // Create a z3 expression
-    const auto ite { make_ite("Hello") };
+    /* const auto ite { make_ite("Hello") }; */
+    const auto ite { Create::literal(std::string { "Hello" }) };
     const auto conv { z3.convert(ite.get()) };
 
     // Verify abstraction runs
     (void) z3.abstract(conv);
-#endif
 }
 
 // Define the test
