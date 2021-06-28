9aeaada56deabecd33d149be6966197bb605ea12
zwimer <zwimer@gmail.com>
2021-06-28 15:59:59 -0700

    refactor trivial tests

--- native/tests/unit/backend/z3/trivial.cpp
+++ native/tests/unit/backend/z3/trivial.cpp
@@ -25,55 +25,37 @@ std::vector<Func> functions {};
 
 /** Test trivial ops in claricpp */
 void trivial() {
-    for (const auto &func : functions) {
-        func();
-    }
+
+    Backend::Z3::Z3 z3bk;
+
+    const auto fp_x { Create::symbol<Ex::FP>("fp_x", 64_ui) };
+    const auto bv_x { Create::symbol<Ex::BV>("bv_x", 64_ui) };
+    const auto bool_x { Create::symbol("bool_x") };
+
+    // Verify the round trip changes nothing
+    const auto test_id = [&z3bk](const Expression::BasePtr &&x) {
+        return z3bk.abstract(z3bk.convert(x)) == x;
+    };
+
+    // Unary
+
+    UNITTEST_ASSERT(test_id(Create::abs(fp_x)));
+    UNITTEST_ASSERT(test_id(Create::not_(bool_x)));
+    UNITTEST_ASSERT(test_id(Create::invert(bv_x)));
+
+    // Neg
+    UNITTEST_ASSERT(test_id(Create::neg<Ex::FP>(fp_x)));
+    UNITTEST_ASSERT(test_id(Create::neg<Ex::BV>(bv_x)));
+
+    // Reverse
+    const auto also_x { Create::reverse(Create::reverse(bv_x)) };
+    UNITTEST_ASSERT(z3bk.simplify(also_x) == bv_x);
+
+    // UInt Binary
+
+    /* UNITTEST_ASSERT(test_id(Create::sign_ext(bv_x, 1))); */
+    /* UNITTEST_ASSERT(test_id(Create::zero_ext(bv_x, 1))); */
 }
 
 // Define the test
 UNITTEST_DEFINE_MAIN_TEST(trivial)
-
-
-/**********************************************************/
-/*                  General Expressions                   */
-/**********************************************************/
-
-
-Backend::Z3::Z3 z3bk;
-
-const auto fp_x { Create::symbol<Ex::FP>("fp_x", 64_ui) };
-const auto bv_x { Create::symbol<Ex::BV>("bv_x", 64_ui) };
-const auto bool_x { Create::symbol("bool_x") };
-
-
-/**********************************************************/
-/*                   Trivial Functions                    */
-/**********************************************************/
-
-
-bool test_id(const Expression::BasePtr &&x) {
-    return z3bk.abstract(z3bk.convert(x)) == x;
-}
-
-ADD_TEST(abs) {
-    UNITTEST_ASSERT(test_id(Create::abs(fp_x)));
-}
-
-ADD_TEST(neg) {
-    UNITTEST_ASSERT(test_id(Create::neg<Ex::FP>(fp_x)));
-    UNITTEST_ASSERT(test_id(Create::neg<Ex::BV>(bv_x)));
-}
-
-
-ADD_TEST(not_) {
-    UNITTEST_ASSERT(test_id(Create::not_(bool_x)));
-}
-
-ADD_TEST(invert) {
-    UNITTEST_ASSERT(test_id(Create::invert(bv_x)));
-}
-
-ADD_TEST(reverse) {
-    const auto also_x { Create::reverse(Create::reverse(bv_x)) };
-    UNITTEST_ASSERT(z3bk.simplify(also_x) == bv_x);
-}
