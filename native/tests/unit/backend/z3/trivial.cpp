14d2e222c32fbbc1e363568c0f6e6b1eb6d985d3
zwimer <zwimer@gmail.com>
2021-06-28 13:49:11 -0700

    New tests

---
+++ native/tests/unit/backend/z3/trivial.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file
+ * \ingroup unittest
+ */
+#include "backend.hpp"
+#include "testlib.hpp"
+
+// For brevity
+namespace Ex = Expression;
+
+
+/** The type of test functions */
+using Func = void (*)();
+
+/** List of functions to test */
+std::vector<Func> functions {};
+
+
+/** A macro to define a new test function */
+#define ADD_TEST(FNAME)                                                                           \
+    void FNAME();                                                                                 \
+    UTILS_RUN_STATEMENT_BEFORE_MAIN(functions.push_back(&FNAME));                                 \
+    void FNAME()
+
+
+/** Test trivial ops in claricpp */
+void trivial() {
+    for (const auto &func : functions) {
+        func();
+    }
+}
+
+// Define the test
+UNITTEST_DEFINE_MAIN_TEST(trivial)
+
+
+/**********************************************************/
+/*                  General Expressions                   */
+/**********************************************************/
+
+
+/** Z3 backend */
+Backend::Z3::Z3 z3bk;
+
+/** A symbolic bv variable */
+const auto x { Create::symbol<Ex::BV>("x", 64_ui) };
+
+
+/**********************************************************/
+/*                   Trivial Functions                    */
+/**********************************************************/
+
+
+ADD_TEST(abs) {
+    const auto abs { Create::abs<Ex::BV>(x) };
+    Utils::Log::warning(__FILE__ " ", __LINE__);
+    const auto simp { z3bk.simplify(abs) };
+    UNITTEST_ASSERT(abs == simp);
+}
