780a9cd3f29f2a801da4964c00ec4797115cd626
zwimer <zwimer@gmail.com>
2021-06-29 16:22:42 -0700

    Improve clarity

--- native/src/utils/fnv1a.hpp
+++ native/src/utils/fnv1a.hpp
@@ -20,7 +20,7 @@
 namespace Utils {
 
     /** FNV-1a class of hashes */
-    template <typename Type> struct FNV1a final : public Unconstructable {
+    template <typename TypeToHash> struct FNV1a final : public Unconstructable {
 
         /** 32-bit type */
         using u32T = uint32_t;
@@ -28,25 +28,25 @@ namespace Utils {
         using u64T = uint64_t;
 
         /** const Input type */
-        using CInput = Constants::CTSC<Type>;
+        using CInput = Constants::CTSC<TypeToHash>;
 
       private:
         /** FNV-1a hash body */
-        template <typename Size, Size Prime, Size Offset>
-        static constexpr Size internal_hash(CInput s, const Size len,
-                                            const Size pre_hash = Offset) noexcept {
+        template <typename HashSize, HashSize Prime, HashSize Offset>
+        static constexpr HashSize internal_hash(CInput s, const HashSize len,
+                                                const HashSize pre_hash = Offset) noexcept {
             if (len == 0) { // It is unsafe to dereference s here
                 return pre_hash;
             }
             else {
-                return internal_hash<Size, Prime, Offset>(
+                return internal_hash<HashSize, Prime, Offset>(
                     // Passes an invalid pointer of len == 1 (this is ok because we do not use it
                     // when len = 0
                     &s[1],
                     // len >= 1, so this is safe
                     len - 1,
                     // s[0] is safe since len != 0
-                    Prime * (pre_hash ^ static_cast<Size>(s[0])));
+                    Prime * (pre_hash ^ static_cast<HashSize>(s[0])));
             }
         }
 
@@ -65,24 +65,24 @@ namespace Utils {
             return internal_hash<u64T, prime, offset>(s, len);
         }
 
-        /** Any Size version
+        /** Any HashSize version
          *  Default: Contants::UInt
          */
-        template <typename Size = Constants::UInt>
+        template <typename HashSize = Constants::UInt>
         static constexpr Constants::UInt hash(CInput s, const Constants::UInt len) noexcept {
-            static_assert(sizeof(Size) >= sizeof(Type),
-                          "FNV1a::hash given a size too small for the given Type");
-            if constexpr (is_same_ignore_cv<Size, u64T>) {
+            static_assert(sizeof(HashSize) >= sizeof(TypeToHash),
+                          "FNV1a::hash given a size too small for the given TypeToHash");
+            if constexpr (is_same_ignore_cv<HashSize, u64T>) {
                 return u64(s, len);
             }
-            else if constexpr (Utils::is_same_ignore_cv<Size, u32T>) {
+            else if constexpr (Utils::is_same_ignore_cv<HashSize, u32T>) {
                 return u32(s, len);
             }
             else {
                 // Static assert false
                 // Use TD::false_ to ensure we only fail if this branch is taken
-                static_assert(TD::false_<Type>,
-                              "Hash::FNV1a::hash not implemented for choice of Size");
+                static_assert(TD::false_<TypeToHash>,
+                              "Hash::FNV1a::hash not implemented for choice of HashSize");
             }
         }
     };
