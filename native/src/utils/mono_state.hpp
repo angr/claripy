1560337fcafe9baecdcdd9b0d16805d48db331ba
zwimer <zwimer@gmail.com>
2021-06-29 13:36:59 -0700

    Utils monostate

---
+++ native/src/utils/mono_state.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * \ingroup utils
+ * @brief This file defines a type called MonoState
+ * This type is meant to signal that some condition only has one state
+ */
+#ifndef R_UTILS_MONOSTATE_HPP_
+#define R_UTILS_MONOSTATE_HPP_
+
+namespace Utils {
+
+    /** A type meant to signal that some condition has only one state */
+    class MonoState {};
+
+} // namespace Utils
+
+#endif
