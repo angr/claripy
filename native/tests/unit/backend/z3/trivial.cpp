a1ac9f77a264cb31b910b37b8cbb014c366f7558
zwimer <zwimer@gmail.com>
2021-06-29 17:10:47 -0700

    More trivial test

--- native/tests/unit/backend/z3/trivial.cpp
+++ native/tests/unit/backend/z3/trivial.cpp
@@ -25,13 +25,14 @@ std::vector<Func> functions {};
 
 /** Test trivial ops in claricpp */
 void trivial() {
-    Utils::Log::Level::set(Utils::Log::Level::Level::Critical);
 
     Backend::Z3::Z3 z3bk;
 
     const auto string_x { Create::symbol<Ex::String>("string_x", 64_ui) };
     const auto fp_x { Create::symbol<Ex::FP>("fp_x", Mode::FP::dbl.width()) };
+    const auto fp_y { Create::symbol<Ex::FP>("fp_y", Mode::FP::dbl.width()) };
     const auto bv_x { Create::symbol<Ex::BV>("bv_x", 64_ui) };
+    const auto bv_y { Create::symbol<Ex::BV>("bv_y", 64_ui) };
     const auto bool_x { Create::symbol("bool_x") };
 
     // Verify the round trip changes nothing
@@ -42,42 +43,68 @@ void trivial() {
 
     // Unary
 
+    Utils::Log::debug("Testing abs...");
     UNITTEST_ASSERT(test_id(Create::abs(fp_x)));
+
+    Utils::Log::debug("Testing not...");
     UNITTEST_ASSERT(test_id(Create::not_(bool_x)));
+
+    Utils::Log::debug("Testing invert...");
     UNITTEST_ASSERT(test_id(Create::invert(bv_x)));
 
     // Neg
+    Utils::Log::debug("Testing neg...");
     UNITTEST_ASSERT(test_id(Create::neg<Ex::FP>(fp_x)));
     UNITTEST_ASSERT(test_id(Create::neg<Ex::BV>(bv_x)));
 
     // Reverse
+    Utils::Log::debug("Testing reverse...");
     const auto also_x { Create::reverse(Create::reverse(bv_x)) };
     UNITTEST_ASSERT(z3bk.simplify(also_x) == bv_x);
 
 
     // UInt Binary
 
+    Utils::Log::debug("Testing signext...");
     UNITTEST_ASSERT(test_id(Create::sign_ext(bv_x, 1)));
+
+    Utils::Log::debug("Testing zeroext...");
     UNITTEST_ASSERT(test_id(Create::zero_ext(bv_x, 1)));
 
 
     // Binary
 
     // eq
+    Utils::Log::debug("Testing eq...");
     UNITTEST_ASSERT(test_id(Create::eq<Ex::FP>(fp_x, fp_x)));
     UNITTEST_ASSERT(test_id(Create::eq<Ex::Bool>(bool_x, bool_x)));
     /* UNITTEST_ASSERT(test_id(Create::eq<Ex::String>(string_x, string_x))); */
 
     // neq
+    Utils::Log::debug("Testing neq...");
     UNITTEST_ASSERT(test_id(Create::neq<Ex::FP>(fp_x, fp_x)));
     UNITTEST_ASSERT(test_id(Create::neq<Ex::Bool>(bool_x, bool_x)));
     /* UNITTEST_ASSERT(test_id(Create::neq<Ex::String>(string_x, string_x))); */
 
-    // compare
-    /* static_assert(Utils::BitMask::has(Mask, Mode::Compare::Signed), */
-    /*               "FP comparisons must be signed"); */
-    /* return Private::binary<Ex::Bool, In, Op::Compare<Mask>, Private::SizeMode::NA, Ex::FP, */
-    /*                    Ex::BV>(left, right, std::move(sp)); */
+    // fp compare
+    using C = Mode::Compare;
+    Utils::Log::debug("Testing compare...");
+    UNITTEST_ASSERT(test_id(Create::compare<Ex::FP, C::Signed | C::Less | C::Eq>(fp_x, fp_y)));
+    UNITTEST_ASSERT(test_id(Create::compare<Ex::FP, C::Signed | C::Less | C::Neq>(fp_x, fp_y)));
+    UNITTEST_ASSERT(test_id(Create::compare<Ex::FP, C::Signed | C::Greater | C::Eq>(fp_x, fp_y)));
+    UNITTEST_ASSERT(test_id(Create::compare<Ex::FP, C::Signed | C::Greater | C::Neq>(fp_x, fp_y)));
+
+    // bv compare
+    UNITTEST_ASSERT(test_id(Create::compare<Ex::BV, C::Signed | C::Less | C::Eq>(bv_x, bv_y)));
+    UNITTEST_ASSERT(test_id(Create::compare<Ex::BV, C::Signed | C::Less | C::Neq>(bv_x, bv_y)));
+    UNITTEST_ASSERT(test_id(Create::compare<Ex::BV, C::Signed | C::Greater | C::Eq>(bv_x, bv_y)));
+    UNITTEST_ASSERT(test_id(Create::compare<Ex::BV, C::Signed | C::Greater | C::Neq>(bv_x, bv_y)));
+    UNITTEST_ASSERT(test_id(Create::compare<Ex::BV, C::Unsigned | C::Less | C::Eq>(bv_x, bv_y)));
+    UNITTEST_ASSERT(test_id(Create::compare<Ex::BV, C::Unsigned | C::Less | C::Neq>(bv_x, bv_y)));
+    UNITTEST_ASSERT(
+        test_id(Create::compare<Ex::BV, C::Unsigned | C::Greater | C::Eq>(bv_x, bv_y)));
+    UNITTEST_ASSERT(
+        test_id(Create::compare<Ex::BV, C::Unsigned | C::Greater | C::Neq>(bv_x, bv_y)));
 }
 
 // Define the test
