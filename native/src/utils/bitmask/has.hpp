474b47f6b0a2820f9febf784a5aa6c12302b9b63
zwimer <zwimer@gmail.com>
2021-06-29 17:05:36 -0700

    Bitmask bug fix

--- native/src/utils/bitmask/has.hpp
+++ native/src/utils/bitmask/has.hpp
@@ -16,6 +16,12 @@ namespace Utils::BitMask {
 
     /** Return true if e has all flags in flags set */
     template <typename Enum> constexpr bool has(const Enum e, const Enum flags) {
+        const auto tmp { to_underlying(flags) };
+        return (to_underlying(e) & tmp) == tmp;
+    }
+
+    /** Return true if e has all flags in flags set */
+    template <typename Enum> constexpr bool has_any(const Enum e, const Enum flags) {
         return (to_underlying(e) & to_underlying(flags)) != 0;
     }
 
