9fad595684b9b5aa2bad2809df4652e352880e39
zwimer <zwimer@gmail.com>
2021-07-02 12:47:08 -0700

    Fix abstract test

--- native/tests/unit/backend/z3/abstract.cpp
+++ native/tests/unit/backend/z3/abstract.cpp
@@ -5,33 +5,13 @@
 #include "backend.hpp"
 #include "testlib.hpp"
 
-#include "../../make_ite.hpp"
-
 
 /** Try to abstract a claricpp expression from z3 */
 void abstract() {
-
     z3::context ctx;
-    auto b { ctx.string_val("Hello") };
-    auto decl { b.decl() };
-    auto decl_kind { decl.decl_kind() };
-    Utils::Log::critical(b);
-    Utils::Log::critical("Kind: ", b.kind());
-    Utils::Log::critical("Has to be: ", Z3_APP_AST, " or ", Z3_NUMERAL_AST);
-    Utils::Log::critical(decl);
-    Utils::Log::critical(decl_kind);
-
-#if 1
     auto z3 { Backend::Z3::Z3 {} };
-
-    // Create a z3 expression
-    /* const auto ite { make_ite("Hello") }; */
-    const auto ite { Create::literal(std::string { "Hello" }) };
-    const auto conv { z3.convert(ite.get()) };
-
-    // Verify abstraction runs
-    (void) z3.abstract(conv);
-#endif
+    const auto abs { z3.abstract(ctx.bool_val(true)) };
+    UNITTEST_ASSERT(abs == Create::literal(true));
 }
 
 // Define the test
