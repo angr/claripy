5bfba75e8e03986c5feeaf33df1b02dc98764601
zwimer <zwimer@gmail.com>
2021-07-16 17:26:21 -0700

    Create support for new literal options and clean up of literal functions

--- native/src/create/literal.hpp
+++ native/src/create/literal.hpp
@@ -10,38 +10,61 @@
 
 namespace Create {
 
-    /** Create a Bool Expression with a Literal op */
-    inline EBasePtr literal(const bool data, SPAV &&sp = nullptr) {
-        return Private::literal<Expression::Bool, bool>(bool { data }, std::move(sp));
+/** A local macro used for consistency */
+#define TRIVIAL_TYPE(EXP, INPUT)                                                                  \
+    inline EBasePtr literal(const INPUT data, SPAV &&sp = nullptr) {                              \
+        return Private::literal<Expression::EXP, INPUT>(INPUT { data }, std::move(sp));           \
     }
+
+/** A local macro used for consistency */
+#define TRIVIAL_MOVE_TYPE(EXP, INPUT)                                                             \
+    inline EBasePtr literal(INPUT &&data, SPAV &&sp = nullptr) {                                  \
+        return Private::literal<Expression::EXP, INPUT>(std::move(data), std::move(sp));          \
+    }
+
+    /** Create a Bool Expression with a Literal op */
+    TRIVIAL_TYPE(Bool, bool);
 
     /** Create a String Expression with a Literal op */
-    inline EBasePtr literal(std::string &&data, SPAV &&sp = nullptr) {
-        return Private::literal<Expression::String, std::string>(std::move(data), std::move(sp));
-    }
-
-    /** Create a BV Expression with a Literal op */
-    inline EBasePtr literal(std::vector<std::byte> &&data, SPAV &&sp = nullptr) {
-        return Private::literal<Expression::BV, std::vector<std::byte>>(std::move(data),
-                                                                        std::move(sp));
-    }
+    TRIVIAL_MOVE_TYPE(String, std::string);
 
     /** Create a FP Expression with a Literal op containing a double precision float */
-    inline EBasePtr literal(const double data, SPAV &&sp = nullptr) {
-        return Private::literal<Expression::FP, double>(double { data }, std::move(sp));
-    }
+    TRIVIAL_TYPE(FP, double);
 
     /** Create a FP Expression with a Literal op containing a single precision float */
-    inline EBasePtr literal(const float data, SPAV &&sp = nullptr) {
-        return Private::literal<Expression::FP, float>(float { data }, std::move(sp));
-    }
+    TRIVIAL_TYPE(FP, float);
 
     /** Create a FP Expression with a Literal op containing a single precision float
      *  data may not be nullptr
      */
-    inline EBasePtr literal(PyObj::VSPtr &&data, SPAV &&sp = nullptr) {
-        return Private::literal<Expression::VS, PyObj::VSPtr>(std::move(data), std::move(sp));
-    }
+    TRIVIAL_MOVE_TYPE(VS, PyObj::VSPtr);
+
+    // BV creation methods
+
+    /** Create a BV Expression with a Literal op containing an 8-bit signed int */
+    TRIVIAL_TYPE(BV, int8_t);
+    /** Create a BV Expression with a Literal op containing an 16-bit signed int */
+    TRIVIAL_TYPE(BV, int16_t);
+    /** Create a BV Expression with a Literal op containing an 32-bit signed int */
+    TRIVIAL_TYPE(BV, int32_t);
+    /** Create a BV Expression with a Literal op containing an 64-bit signed int */
+    TRIVIAL_TYPE(BV, int64_t);
+
+    /** Create a BV Expression with a Literal op containing an 8-bit unsigned int */
+    TRIVIAL_TYPE(BV, uint8_t);
+    /** Create a BV Expression with a Literal op containing an 16-bit unsigned int */
+    TRIVIAL_TYPE(BV, uint16_t);
+    /** Create a BV Expression with a Literal op containing an 32-bit unsigned int */
+    TRIVIAL_TYPE(BV, uint32_t);
+    /** Create a BV Expression with a Literal op containing an 64-bit unsigned int */
+    TRIVIAL_TYPE(BV, uint64_t);
+
+    /** Create a BV Expression with a Literal op containing an arbitrary length int */
+    TRIVIAL_MOVE_TYPE(VS, BigInt);
+
+// Cleanup
+#undef TRIVIAL_TYPE
+#undef TRIVIAL_MOVE_TYPE
 
 } // namespace Create
 
