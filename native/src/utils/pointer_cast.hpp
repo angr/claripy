28901aab963524eef39da6d1d58b1a9516b4e1c7
zwimer <zwimer@gmail.com>
2021-08-09 15:00:59 -0700

    Pointer cast refactor

--- native/src/utils/pointer_cast.hpp
+++ native/src/utils/pointer_cast.hpp
@@ -8,9 +8,11 @@
 #define R_UTILS_POINTERCAST_HPP_
 
 #include "affirm.hpp"
+#include "affirm_not_null_debug.hpp"
 #include "error.hpp"
 #include "full.hpp"
 #include "is_ancestor.hpp"
+#include "is_in.hpp"
 #include "private/pointer_cast.hpp"
 
 #include "../constants.hpp"
@@ -19,99 +21,119 @@
 #include <type_traits>
 
 
-namespace Utils {
+namespace Utils::Cast {
 
-    /** An up cast */
-    template <typename Out, typename In>
-    constexpr auto up_cast(const std::shared_ptr<In> &in) noexcept {
-        static_assert(is_ancestor<Out, In>, "up_cast passed invalid <In, Out> type pair");
-        return Private::static_pointer_cast<Out>(in); // Does its own checks as well
-    }
+    namespace Dynamic {
 
-    /** A dynamic down cast */
-    template <typename Out, typename In>
-    constexpr auto dynamic_down_cast(const std::shared_ptr<In> &in) noexcept {
-        static_assert(is_ancestor<In, Out>,
-                      "dynamic_down_cast passed invalid <In, Out> type pair");
-        return Private::dynamic_pointer_cast<Out>(in); // Does its own checks as well
-    }
+        /** A dynamic down cast */
+        template <typename Out, typename In>
+        constexpr auto down(const std::shared_ptr<In> &in) noexcept {
+            static_assert(is_ancestor<In, Out>,
+                          "dynamic_down_cast passed invalid <In, Out> type pair");
+            return Utils::Private::dynamic_pointer_cast<Out>(in); // Does its own checks as well
+        }
 
-    /** A dynamic side cast
-     *  Warning: No static checks used
-     */
-    template <typename Out, typename In>
-    constexpr auto dynamic_side_cast(const std::shared_ptr<In> &in) noexcept {
-        return Private::dynamic_pointer_cast<Out>(in); // Does its own checks as well
-    }
+        /** A dynamic side cast
+         *  Warning: Use up / down cast functions if applicable
+         */
+        template <typename Out, typename In>
+        constexpr auto side(const std::shared_ptr<In> &in) noexcept {
+            return Utils::Private::dynamic_pointer_cast<Out>(in); // Does its own checks as well
+        }
 
-    /** Dynamic down-cast that throws on failure */
-    template <typename Out, typename In, typename Err = Error::Unexpected::BadCast>
-    constexpr auto dynamic_down_cast_throw_on_fail(const std::shared_ptr<In> &in) noexcept {
-        const auto ret = dynamic_down_cast<Out>(in);
-        affirm<Err>(full(ret), WHOAMI_WITH_SOURCE "Dynamic down-cast failed");
-        return ret;
-    }
+        /** Return true if in is an Out; in may not be nullptr
+         *  Warning: We return false if the pointer points to nullptr
+         *  Note: this does not do any static assertion verification
+         */
+        template <typename Out, typename In> constexpr bool test(const std::shared_ptr<In> &in) {
+            UTILS_AFFIRM_NOT_NULL_DEBUG(in);
+            using Ptr = Constants::CTSC<TransferConst<Out, In>>;
+            return dynamic_cast<Ptr>(in.get()) != nullptr;
+        }
 
-    /** A dynamic side cast that throws on failure */
-    template <typename Out, typename In, typename Err = Error::Unexpected::BadCast>
-    constexpr auto dynamic_side_cast_throw_on_fail(const std::shared_ptr<In> &in) noexcept {
-        const auto ret = dynamic_side_cast<Out>(in);
-        affirm<Err>(full(ret), WHOAMI_WITH_SOURCE "Dynamic side-cast failed");
-        return ret;
-    }
+        /** Dynamic down-cast that throws on failure; in may not be nullptr */
+        template <typename Out, typename In, typename Err = Error::Unexpected::BadCast>
+        constexpr auto down_throw_on_fail(const std::shared_ptr<In> &in) noexcept {
+            UTILS_AFFIRM_NOT_NULL_DEBUG(in);
+            const auto ret { down<Out>(in) };
+            affirm<Err>(full(ret), WHOAMI_WITH_SOURCE "Dynamic down-cast failed");
+            return ret;
+        }
 
-    // Functions that throw
+        /** A dynamic side cast that throws on failure; in may not be nullptr */
+        template <typename Out, typename In, typename Err = Error::Unexpected::BadCast>
+        constexpr auto side_throw_on_fail(const std::shared_ptr<In> &in) noexcept {
+            UTILS_AFFIRM_NOT_NULL_DEBUG(in);
+            const auto ret { side<Out>(in) };
+            affirm<Err>(full(ret), WHOAMI_WITH_SOURCE "Dynamic pointer cast failed");
+            return ret;
+        }
 
-    /** An static down cast
-     *  Is dynamic and throws on failure during debugging
-     */
-    template <typename Out, typename In>
-    constexpr auto static_down_cast(const std::shared_ptr<In> &in)
+        /** Return true if the dynamic cast desired will pass; in may not be nullptr
+         *  Note: this does not do any static assertion verification itself
+         *  Note: This requires the type of exception to be thrown to be passed
+         */
+        template <typename To, typename Err, typename In, typename... Args>
+        constexpr void test_throw_on_fail(const std::shared_ptr<In> &in, const Args &...args) {
+            UTILS_AFFIRM_NOT_NULL_DEBUG(in);
+            affirm<Err>(test<To>(in), args...);
+        }
+    } // namespace Dynamic
+
+    namespace Static {
+
+        /** An up cast */
+        template <typename Out, typename In>
+        constexpr auto up(const std::shared_ptr<In> &in) noexcept {
+            static_assert(is_ancestor<Out, In>, "up_cast passed invalid <In, Out> type pair");
+            return Utils::Private::static_pointer_cast<Out>(in); // Does its own checks as well
+        }
+
+        /** An static down cast
+         *  Is dynamic and throws on failure during debugging
+         */
+        template <typename Out, typename In>
+        constexpr auto down(const std::shared_ptr<In> &in)
 #ifdef DEBUG
-    {
-        return dynamic_down_cast<Out>(in);
-    }
+        {
+            return Utils::Cast::Dynamic::down<Out>(in);
+        }
 #else
-        noexcept {
-        static_assert(is_ancestor<In, Out>, "static_down_cast passed invalid <In, Out> type pair");
-        return Private::static_pointer_cast<Out>(in); // Does its own checks as well
-    }
+            noexcept {
+            static_assert(is_ancestor<In, Out>,
+                          "static_down_cast passed invalid <In, Out> type pair");
+            return Utils::Private::static_side_cast<Out>(in); // Does its own checks as well
+        }
 #endif
 
-    /** An static side cast
-     *  Is dynamic and throws on failure during debugging
-     */
-    template <typename Out, typename In>
-    constexpr auto static_side_cast(const std::shared_ptr<In> &in)
+        /** An static side cast
+         *  Is dynamic and throws on failure during debugging
+         */
+        template <typename Out, typename In>
+        constexpr auto side(const std::shared_ptr<In> &in)
 #ifdef DEBUG
-    {
-        return dynamic_side_cast<Out>(in);
-    }
+        {
+            return Utils::Cast::Dynamic::side<Out>(in);
+        }
 #else
-        noexcept {
-        return Private::static_pointer_cast<Out>(in);
-    }
+            noexcept {
+            return Utils::Private::static_side_cast<Out>(in);
+        }
 #endif
 
-    /** Return true if in is an Out
-     *  Warning: We return false if the pointer points to nullptr
-     *  Note: this does not do any static assertion verification itself
-     */
-    template <typename Out, typename In>
-    constexpr bool dynamic_test(const std::shared_ptr<In> &in) {
-        return dynamic_cast<Constants::CTSC<Out>>(in.get()) != nullptr;
-    }
+        /** Cast from void; in may not be nullptr
+         *  In may be a const qualifed void type
+         *  Note: this does not do any verification
+         */
+        template <typename Out, typename In>
+        constexpr auto from_void(const std::shared_ptr<In> &in) {
+            UTILS_AFFIRM_NOT_NULL_DEBUG(in);
+            static_assert(Utils::is_in_ignore_const<In, void>, "Will only cast from void type");
+            static_assert(!Utils::is_same_ignore_cv<Out, void>, "Cannot cast to void");
+            return Utils::Private::static_pointer_cast<Out>(in);
+        }
+    } // namespace Static
 
-    /** Return true if the dynamic cast desired will pass
-     *  Warning: We throw if the pointer points to nullptr
-     *  Note: this does not do any static assertion verification itself
-     *  Note: This requires the type of exception to be thrown to be passed
-     */
-    template <typename To, typename Err, typename In, typename... Args>
-    constexpr void dynamic_test_throw_on_fail(const std::shared_ptr<In> &in, const Args &...args) {
-        affirm<Err>(dynamic_test<To>(in), args...);
-    }
-
-} // namespace Utils
+} // namespace Utils::Cast
 
 #endif
