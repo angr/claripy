0e620ace7fb6a2e74a6da460a4307da1a8f7dba1
zwimer <zwimer@gmail.com>
2021-08-10 16:09:28 -0700

    Solution tests

--- native/tests/unit/backend/z3/solution.cpp
+++ native/tests/unit/backend/z3/solution.cpp
@@ -7,7 +7,37 @@
 
 
 /** Test is_true and is_false */
-void solution() {}
+void solution() {
+    namespace Ex = Expression;
+    using B = Ex::Bool;
+
+    auto z3 { Backend::Z3::Z3 {} };
+    auto solver { z3.new_tls_solver() };
+
+    // Leaves
+    const auto x { Create::symbol("x") };
+    const auto t { Create::literal(true) };
+    const auto f { Create::literal(false) };
+
+    // Statements
+    const auto maybe_true { Create::and_<B>({ x, t }) };
+    const auto maybe_false { Create::or_<B>({ x, f }) };
+
+    // Create a solver
+    auto is_sol = [&x, &z3, &solver](const Ex::BasePtr &start, const Ex::BasePtr &x_s) {
+        solver->push();
+        solver->add(z3.convert(start));
+        const bool ret = z3.solution(x, x_s, solver);
+        solver->pop();
+        return ret;
+    };
+
+    // Test sat
+    UNITTEST_ASSERT(is_sol(maybe_true, t));
+    UNITTEST_ASSERT(!is_sol(maybe_true, f));
+    UNITTEST_ASSERT(is_sol(maybe_false, t));
+    UNITTEST_ASSERT(!is_sol(maybe_false, f));
+}
 
 // Define the test
 UNITTEST_DEFINE_MAIN_TEST(solution)
