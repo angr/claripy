fa35fe161c24dccf8545e7fdb3674af3ffec8ba0
zwimer <zwimer@gmail.com>
2021-08-10 16:09:20 -0700

    Satisfiable tests

--- native/tests/unit/backend/z3/satisfiable.cpp
+++ native/tests/unit/backend/z3/satisfiable.cpp
@@ -8,9 +8,11 @@
 
 /** Test is_true and is_false */
 void satisfiable() {
-#if 0
+    namespace Ex = Expression;
+    using B = Ex::Bool;
+
     auto z3 { Backend::Z3::Z3 {} };
-    using B = Expression::Bool;
+    auto solver { z3.new_tls_solver() };
 
     // Leaves
     const auto x { Create::symbol("x") };
@@ -24,25 +26,34 @@ void satisfiable() {
     const auto maybe_false { Create::or_<B>({ x, f }) };
 
     // Create a solver
-    z3
+    auto is_sat = [&x, &z3, &solver](const Ex::BasePtr &e, const Ex::BasePtr ec = nullptr) {
+        solver->push();
+        bool ret; // NOLINT
+        if (ec != nullptr) {
+            std::set<Expression::BasePtr> ecs;
+            ecs.emplace(Create::eq<B>(x, ec));
+            solver->add(z3.convert(e));
+            ret = z3.satisfiable(solver, ecs);
+        }
+        else {
+            solver->add(z3.convert(e));
+            ret = z3.satisfiable(solver);
+        }
+        solver->pop();
+        return ret;
+    };
 
     // Test sat
-    UNITTEST_ASSERT(z3.satisfiable(true_));
-    UNITTEST_ASSERT(!z3.satisfiable(false_));
-    UNITTEST_ASSERT(z3.satisfiable(maybe_true));
-    UNITTEST_ASSERT(z3.satisfiable(maybe_false));
+    UNITTEST_ASSERT(is_sat(true_));
+    UNITTEST_ASSERT(!is_sat(false_));
+    UNITTEST_ASSERT(is_sat(maybe_true));
+    UNITTEST_ASSERT(is_sat(maybe_false));
 
     // Test sat with extra constraints
-    auto ec = lambda[](const Expression::BasePtr& w) {
-        std::set<Expression::BasePtr> ret;
-        ret.emplace(Create::eq<B>(x, w));
-        return ret;
-    }
-    UNITTEST_ASSERT(!z3.satisfiable(true_, ec(f)));
-    UNITTEST_ASSERT(!z3.satisfiable(false_, ec(f)));
-    UNITTEST_ASSERT(!z3.satisfiable(maybe_true, ec(f)));
-    UNITTEST_ASSERT(z3.satisfiable(maybe_false, ec(t)));
-#endif
+    UNITTEST_ASSERT(is_sat(true_, f));
+    UNITTEST_ASSERT(!is_sat(false_, f));
+    UNITTEST_ASSERT(!is_sat(maybe_true, f));
+    UNITTEST_ASSERT(is_sat(maybe_false, t));
 }
 
 // Define the test
