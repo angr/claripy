fac0a4fab67498d8df76642394e22313a1f9edc2
zwimer <zwimer@gmail.com>
2021-08-23 13:36:56 -0700

    Add functionality to fp width, improve clarity

--- native/src/mode/fp/width.hpp
+++ native/src/mode/fp/width.hpp
@@ -17,17 +17,21 @@ namespace Mode::FP {
     struct Width final {
         /** The width of the exponent */
         const uint32_t exp;
-        /** The width of the mantissa */
+        /** The width of the mantissa, including the implicit 1 bit */
         const uint32_t mantissa;
+        /** The width of the mantissa, excluding the implicit 1 bit */
+        constexpr uint32_t mantissa_raw() const noexcept { return mantissa - 1; }
         /** The full width of the fp */
-        constexpr Constants::UInt width() const noexcept { return exp + mantissa; }
+        constexpr Constants::UInt width() const noexcept { return 1 + no_sign_width(); }
+        /** The full width of the fp */
+        constexpr Constants::UInt no_sign_width() const noexcept { return exp + mantissa - 1; }
     };
 
     /** A single wide fp width */
-    static inline const constexpr Width flt { 8, 24 };
+    static inline const constexpr Width flt { 8, 23 + 1 };
 
     /** A double wide fp width */
-    static inline const constexpr Width dbl { 11, 53 };
+    static inline const constexpr Width dbl { 11, 52 + 1 };
 
     /** Equality operator */
     constexpr bool operator==(const Width &a, const Width &b) noexcept {
