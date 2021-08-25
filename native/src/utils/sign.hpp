3b8c806106912f17e68bbde3343c7185e4ce81c6
zwimer <zwimer@gmail.com>
2021-08-24 22:54:44 -0700

    Utils sign permit nops

--- native/src/utils/sign.hpp
+++ native/src/utils/sign.hpp
@@ -10,16 +10,16 @@
 namespace Utils {
 
     /** Sign x */
-    template <typename T> constexpr inline auto sign(T x) {
+    template <typename T, bool AllowNop = false> constexpr inline auto sign(T x) {
+        static_assert(AllowNop || std::is_unsigned_v<T>, "T must be unsigned");
         static_assert(std::is_integral_v<T>, "T must be a primitive");
-        static_assert(std::is_unsigned_v<T>, "T must be unsigned");
         return static_cast<std::make_signed_t<T>>(x);
     }
 
     /** Unsign x */
-    template <typename T> constexpr inline auto unsign(T x) {
+    template <typename T, bool AllowNop = false> constexpr inline auto unsign(T x) {
+        static_assert(AllowNop || std::is_signed_v<T>, "T must be unsigned");
         static_assert(std::is_integral_v<T>, "T must be a primitive");
-        static_assert(std::is_signed_v<T>, "T must be unsigned");
         return static_cast<std::make_unsigned_t<T>>(x);
     }
 
