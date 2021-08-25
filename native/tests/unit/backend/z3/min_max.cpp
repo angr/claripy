335337f14cdd067e2a510658697ec37b61dd5819
zwimer <zwimer@gmail.com>
2021-08-24 17:12:46 -0700

    z3 min test case improved

--- native/tests/unit/backend/z3/min_max.cpp
+++ native/tests/unit/backend/z3/min_max.cpp
@@ -5,28 +5,49 @@
 #include "backend.hpp"
 #include "testlib.hpp"
 
-/** Test the backend min and max functions */
+/** Test the backend min and max functions
+ * @todo: max
+ */
 void min_max() {
     z3::context ctx;
     auto z3 { Backend::Z3::Z3 {} };
 
-    // (x-5)^2 - 5 = 0
-    const auto x { Create::symbol<Expression::BV>("x", 64) };
-    const auto five { Create::literal(5_ui) };
-    const auto diff { Create::sub(x, five) };
-    const auto quadratic { Create::sub(Create::mul({ diff, diff }), five) };
-    const auto equation { Create::eq<Expression::BV>(quadratic, Create::literal(0_ui)) };
+    // For brevity
+    namespace C = Create;
+    using M = Mode::Compare;
+    namespace E = Expression;
 
-    // Construct the solver
-    const auto solver { z3.new_tls_solver() };
-    solver->add(z3.convert(equation));
+    // Prep
+    const auto x { C::symbol<E::BV>("x", 64) };
+    const auto mfive { C::literal(-5_ui) };
 
-    // Find min (should be -5 at x = 5).
-    auto s { *solver.get() };
-    const auto min { z3.min<true>(z3.convert(x), s) };
+    // Test funciton
+    const auto get_min { [&x, &z3](Expression::BasePtr e) {
+        const auto solver { z3.new_tls_solver() };
+        solver->add(z3.convert(e));
+        auto s { *solver.get() };
+        return z3.min<true>(z3.convert(x), s);
+    } };
 
-    Utils::Log::critical(min);
-    UNITTEST_ASSERT(min == -5);
+    // Test x > -5
+    auto min { get_min(C::compare<E::BV, M::Greater | M::Neq | M::Signed>(x, mfive)) };
+    UNITTEST_ASSERT(min == -5 + 1);
+
+    // Test x < -5
+    const auto xleq5 { C::compare<E::BV, M::Less | M::Neq | M::Signed>(x, mfive) };
+    const auto int_min { std::numeric_limits<int64_t>::min() };
+    UNITTEST_ASSERT(get_min(xleq5) == int_min);
+
+    // Test x < -5 && x != uint_min
+    const auto neq { [&x](const int64_t y) {
+        return C::neq<E::BV>(x, C::literal(Utils::unsign(y)));
+    } };
+    const auto plus1 { C::and_<E::Bool>({ xleq5, neq(int_min) }) };
+    UNITTEST_ASSERT(get_min(plus1) == int_min + 1);
+
+    // Test x < -5 && x != uint_min && x != uint_min+1
+    const auto plus2 { C::and_<E::Bool>({ xleq5, neq(int_min), neq(int_min + 1) }) };
+    UNITTEST_ASSERT(get_min(plus2) == int_min + 2);
 }
 
 // Define the test
