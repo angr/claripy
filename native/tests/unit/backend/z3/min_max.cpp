fb60202c55b4164d80d9775447a73700a5fc3cad
zwimer <zwimer@gmail.com>
2021-08-26 01:18:25 -0700

    Cleanup min/max test case; tests pass.

--- native/tests/unit/backend/z3/min_max.cpp
+++ native/tests/unit/backend/z3/min_max.cpp
@@ -26,7 +26,6 @@ static T get_ext(Backend::Z3::Z3 &z3, const Expression::BasePtr &x,
     return static_cast<T>((ec.size() == 0) ? f(conv, solver) : f_ec(conv, solver, ec));
 }
 
-
 /** Tests min and maxed for the chosen type */
 template <typename T, bool Signed = std::is_signed_v<T>>
 static void min_max_test(Backend::Z3::Z3 &z3) {
@@ -41,13 +40,27 @@ static void min_max_test(Backend::Z3::Z3 &z3) {
 
     // Prep
     const auto &unsign { Utils::unsign<T, true> };
-    const constexpr M neq_mask { M::Neq | (Signed ? M::Signed : M::Unsigned) };
     const auto int_max { std::numeric_limits<T>::max() };
     const auto int_min { std::numeric_limits<T>::min() };
+    const constexpr M neq_mask { M::Neq | (Signed ? M::Signed : M::Unsigned) };
+
+    // x and expr generators
+    const auto x { C::symbol<E::BV>("x", C_CHAR_BIT * sizeof(T)) };
+    const auto num { [](const T v) { return C::literal(unsign(T { v })); } };
+    const auto neq { [&x](const T y) { return C::neq<E::BV>(x, C::literal(unsign(y))); } };
 
     // Exprs
-    const auto x { C::symbol<E::BV>("x", C_CHAR_BIT * sizeof(T)) };
-    const auto five { C::literal(unsign(T { 5 })) };
+    const auto xleq10 { C::compare<E::BV, M::Less | neq_mask>(x, num(10)) };
+    const auto xleq5 { C::compare<E::BV, M::Less | neq_mask>(x, num(5)) };
+    const auto xgeq5 { C::compare<E::BV, M::Greater | neq_mask>(x, num(5)) };
+
+    const auto plus1 { C::and_<E::Bool>({ xleq5, neq(int_min) }) };
+    const auto plus2 { C::and_<E::Bool>({ xleq5, neq(int_min), neq(int_min + 1) }) };
+    const auto minus1 { C::and_<E::Bool>({ xgeq5, neq(int_max) }) };
+    const auto minus2 { C::and_<E::Bool>({ xgeq5, neq(int_max), neq(int_max - 1) }) };
+
+    const auto max { C::eq<E::BV>(x, Create::literal(unsign(int_max))) };
+    const auto min { C::eq<E::BV>(x, Create::literal(unsign(int_min))) };
 
     // Test functions
     const auto get_min { [&z3, &x](auto e) {
@@ -61,24 +74,21 @@ static void min_max_test(Backend::Z3::Z3 &z3) {
 
     // Test x > 5
     Utils::Log::debug("\t\t- Standard tests...");
-    const auto xgeq5 { C::compare<E::BV, M::Greater | neq_mask>(x, five) };
     UNITTEST_ASSERT(get_min(xgeq5) == 6);
     UNITTEST_ASSERT(get_max(xgeq5) == int_max);
 
     // Test x < 5
-    const auto xleq5 { C::compare<E::BV, M::Less | neq_mask>(x, five) };
     UNITTEST_ASSERT(get_min(xleq5) == int_min);
     UNITTEST_ASSERT(get_max(xleq5) == 4);
 
     // Test extra constraints
 
-    // Test x > 5; ec: x < 10
+    // Test x < 10; ec: x > 5
     Utils::Log::debug("\t\t- Extra constraints tests...");
-    const auto xleq10 { C::compare<E::BV, M::Less | neq_mask>(
-        x, Create::literal(unsigned(T { 10 }))) };
-    T res { get_ext<Signed, T, true>(z3, x, xgeq5, EC { xleq10 }) };
-    ;
+    T res { get_ext<Signed, T, true>(z3, x, xleq10, EC { xgeq5 }) };
     UNITTEST_ASSERT(res == 6);
+
+    // Test x > 5; ec: x < 10
     res = get_ext<Signed, T, false>(z3, x, xgeq5, EC { xleq10 });
     UNITTEST_ASSERT(res == 9);
 
@@ -86,23 +96,18 @@ static void min_max_test(Backend::Z3::Z3 &z3) {
 
     // Test x < 5 && x != int_min
     Utils::Log::debug("\t\t- Testing final step...");
-    const auto neq { [&x](const T y) { return C::neq<E::BV>(x, C::literal(unsign(y))); } };
-    const auto plus1 { C::and_<E::Bool>({ xleq5, neq(int_min) }) };
     UNITTEST_ASSERT(get_min(plus1) == int_min + 1);
     UNITTEST_ASSERT(get_max(plus1) == 4);
 
     // Test x < 5 && x != int_min && x != int_min+1
-    const auto plus2 { C::and_<E::Bool>({ xleq5, neq(int_min), neq(int_min + 1) }) };
     UNITTEST_ASSERT(get_min(plus2) == int_min + 2);
     UNITTEST_ASSERT(get_max(plus2) == 4);
 
     // Test x > 5 && x != int_max
-    const auto minus1 { C::and_<E::Bool>({ xgeq5, neq(int_max) }) };
     UNITTEST_ASSERT(get_max(minus1) == int_max - 1);
     UNITTEST_ASSERT(get_min(minus1) == 6);
 
     // Test x > 5 && x != int_max && x != int_max-1
-    const auto minus2 { C::and_<E::Bool>({ xgeq5, neq(int_max), neq(int_max - 1) }) };
     UNITTEST_ASSERT(get_max(minus2) == int_max - 2);
     UNITTEST_ASSERT(get_min(minus2) == 6);
 
@@ -110,12 +115,10 @@ static void min_max_test(Backend::Z3::Z3 &z3) {
 
     // Test x == int_max
     Utils::Log::debug("\t\t- Testing extrema...");
-    const auto max { C::eq<E::BV>(x, Create::literal(unsign(int_max))) };
     UNITTEST_ASSERT(get_min(max) == int_max);
     UNITTEST_ASSERT(get_max(max) == int_max);
 
     // Test x == int_min
-    const auto min { C::eq<E::BV>(x, Create::literal(unsign(int_min))) };
     UNITTEST_ASSERT(get_min(min) == int_min);
     UNITTEST_ASSERT(get_max(min) == int_min);
 }
