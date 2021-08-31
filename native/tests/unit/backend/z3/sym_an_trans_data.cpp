49c8cfcfa44fe4dd6b7cf1d9c9715179c0893cd2
zwimer <zwimer@gmail.com>
2021-08-31 16:56:07 -0700

    SymAnTransData test

---
+++ native/tests/unit/backend/z3/sym_an_trans_data.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file
+ * \ingroup unittest
+ */
+#include "backend.hpp"
+#include "testlib.hpp"
+
+
+/** A built-in annotation */
+class TestAnnotation final : public Annotation::Base {
+    FACTORY_ENABLE_CONSTRUCTION_FROM_BASE(Base, 0)
+  public:
+    /** Constructor */
+    explicit inline TestAnnotation(const Hash::Hash &h, std::string &&s) noexcept
+        : Base { h, static_cuid }, str { std::move(s) } {}
+
+    /** Returns whether this annotation can be eliminated in a simplification. */
+    inline bool eliminatable() const override final { return false; }
+
+    /** Returns whether this annotation can be relocated in a simplification. */
+    inline bool relocatable() const override final { return false; }
+
+    /** Some data */
+    const std::string str;
+};
+
+/** Test how the z3 backend handles sym_an_trans_data */
+void sym_an_trans_data() {
+    auto z3 { Backend::Z3::Z3 {} };
+    namespace A = Annotation;
+
+    // Create an annotation
+    const auto an { A::factory<TestAnnotation>(std::string { "Hello" }) };
+
+    // Create an BV symbol
+    const auto an_vec { std::make_shared<A::Vec>(A::Vec::RawVec { an }) };
+    const auto sym { Create::symbol<Expression::BV>(std::string { "x" }, 32, an_vec) };
+
+    // Round trip it through z3
+    const auto conv { z3.convert(sym.get()) };
+    const auto abs { z3.abstract(conv) };
+
+    // Tests
+    UNITTEST_ASSERT(sym == abs);
+    UNITTEST_ASSERT(sym->annotations == abs->annotations);
+    const auto bptr { abs->annotations->vec[0] };
+    UNITTEST_ASSERT(sym->annotations->vec[0] == bptr);
+    UNITTEST_ASSERT(static_cast<const TestAnnotation *>(bptr.get())->str == "Hello");
+
+    // Verify data can be cleared
+    z3.clear_persistent_data();
+    const auto abs2 { z3.abstract(conv) };
+    UNITTEST_ASSERT(sym != abs2);
+    UNITTEST_ASSERT(abs != abs2);
+    UNITTEST_ASSERT(abs2->annotations == nullptr);
+}
+
+// Define the test
+UNITTEST_DEFINE_MAIN_TEST(sym_an_trans_data)
