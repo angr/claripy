4576347b6f14803e014eb833c6542f72c6fa048f
zwimer <zwimer@gmail.com>
2021-09-01 15:44:15 -0700

    to_hex imrovements / bug fix

--- native/src/utils/to_hex.hpp
+++ native/src/utils/to_hex.hpp
@@ -13,32 +13,49 @@
 
 namespace Utils {
 
-    /** A noexcept method of converting a T into a hex string
-     *  If string allocation throws an exception, just crash
+    /** Returns the maximum length string that to_hex_buf can generate give n type T
+     * '0x' + 2 characters per byte, each representing a nibble
      */
-    template <typename T> inline std::string to_hex(const T val) noexcept {
+    template <typename T> static const constexpr std::size_t to_hex_max_len { 2 + 2 * sizeof(T) };
+
+    /** A noexcept method of converting a T into a hex string
+     *  Stores the resulting string in buf
+     *  Warning: This method assumes buf is large enough to hold the string!
+     *  Returns the number of bytes stored in buf
+     */
+    template <typename T> inline std::size_t to_hex(const T val, char *const buf) noexcept {
         static_assert(std::is_integral_v<T>, "This will not work on non-integral types");
         static_assert(CHAR_BIT % 2 == 0, "CHAR_BIT is not even");
-        // Prep
         static const constexpr char syms[] = "0123456789abcdef"; // NOLINT
-        const std::size_t max { 2 + 2 * sizeof(T) };
-        std::string ret(max, 0);
-        // Convert the value nibble by nibble
-        ret[0] = '0';
-        ret[1] = 'x';
+        // Prep
+        buf[0] = '0';
+        buf[1] = 'x';
+        if (val == 0) {
+            buf[2] = '0';
+            return 3;
+        }
         std::size_t str_i { 2 };
         const constexpr std::size_t delta { CHAR_BIT / 2 };
         for (std::size_t shft { CHAR_BIT * sizeof(T) - delta }; (shft + delta) != 0;
              shft -= delta) {
             const char next { syms[(val >> shft) & 0xf] };
             if (next != '0' || str_i > 2) {
-                ret[str_i] = next;
+                buf[str_i] = next;
                 str_i += 1;
             }
         }
-        ret.resize(str_i);
+        return str_i;
+    }
+
+    /** A noexcept method of converting a T into a hex string
+     *  If string allocation throws an exception, just crash
+     */
+    template <typename T> inline std::string to_hex(const T val) noexcept {
+        std::string ret(to_hex_max_len<T>, NULL); // () not {}
+        ret.resize(to_hex(val, ret.data()));
         return ret;
     }
+
 } // namespace Utils
 
 #endif
---
+++ native/tests/unit/utils/to_hex.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file
+ * \ingroup unittest
+ */
+#include "testlib.hpp"
+
+#include <limits>
+
+
+/** Returns true if to_hex worked for the chosen value */
+template <typename T> static bool test(const T val) {
+    std::ostringstream os;
+    os << std::hex << "0x" << val;
+    const auto sol { os.str() };
+    const auto ml { Utils::to_hex_max_len<T> };
+    UNITTEST_ASSERT(sol.size() <= ml);
+    const auto got { Utils::to_hex(val) };
+    UNITTEST_ASSERT(got.size() <= ml);
+    return got == sol;
+}
+
+/** Verify to_hex works */
+void to_hex() {
+
+    // Test for a few values around boundires and extras
+    for (int i { -300 }; i < 300; ++i) {
+        UNITTEST_ASSERT_MSG(test(i), "Value: ", i);
+    }
+
+    // Test for a few values around boundires and extras
+    for (unsigned i { static_cast<unsigned>(-300) }; i < 300; ++i) {
+        UNITTEST_ASSERT_MSG(test(i), "Value: ", i);
+    }
+
+    // Test for a few values around boundires and extras
+    const auto max { std::numeric_limits<int>::max() };
+    const auto min { std::numeric_limits<int>::min() };
+    for (int i { max - 10 }; i != min + 10; ++i) {
+        UNITTEST_ASSERT_MSG(test(i), "Value: ", i);
+    }
+}
+
+// Define the test
+UNITTEST_DEFINE_MAIN_TEST(to_hex)
