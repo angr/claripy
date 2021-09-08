96f49e0b6e819c7f30810daeda3624f09f2d0619
Ruoyu <wuruoyu@me.com>
2021-09-08 15:43:44 -0400

    fix variadic args

--- claripy/ast/base.py
+++ claripy/ast/base.py
@@ -1137,7 +1137,7 @@ def simplify(e):
         s._uninitialized = e.uninitialized
         s._uc_alloc_depth = e._uc_alloc_depth
         s._simplified = Base.FULL_SIMPLIFY
-        s = s.annotate(e.annotations)
+        s = s.annotate(*e.annotations)
 
         return s
 
