54c26bd3ffaa2785f51d2f0c1570c1a971a0d92c
hwu71 <hwu71@binghamton.edu>
2021-10-08 17:43:35 -0400

    Small update for func args

--- claripy/ast/func.py
+++ claripy/ast/func.py
@@ -10,14 +10,14 @@ class Func(Base):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.func_name = self.args[0]
-        self.func_args = self.args
-        args_type = [type(arg) for arg in self.func_args]
+        self.func_name = self.op
+        self.args = self.args
+        args_type = [type(arg) for arg in self.args]
         self.func_op = operations.op(self.func_name, args_type, BV, bound=False, calc_length=lambda *a: 64)
         #operations.backend_func_operations.add(self.op)
 
-    def func_name(self):
+    def get_func_name(self):
         return self.func_name
 
 
-FUNC = operations.op('FUNC', BV, BV, bound=False)
+#FUNC = operations.op('FUNC', BV, BV, bound=False)
