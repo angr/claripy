72d269523210c28f9968b9950a1592c9c52f5113
zwimer <zwimer@gmail.com>
2021-10-13 18:23:13 -0700

    create SPAV method added to API

--- native/src/api.cpp
+++ native/src/api.cpp
@@ -15,6 +15,8 @@ extern "C" {
 /*                        Type Declarations                         */
 /********************************************************************/
 
+static_assert(std::is_same_v<SIZE_T, Constants::UInt>, "SIZE_T needs to be changed");
+
 extern "C" {
 
 // These types should be structs which contain only a single shared pointer named ptr
@@ -58,9 +60,14 @@ namespace Private {
 
     static_assert(std::is_same_v<Map<Annotation::BasePtr>, ClaricppAnnotation>, "f");
     /** Heap cache function */
-    template <typename In, typename Out = Map<In>> static inline Out *to_c(In &&x) {
-        static thread_local Utils::CHeapCache<In, Out> cache {};
+    template <typename In> static inline Map<In> *to_c(In &&x) {
+        static thread_local Utils::CHeapCache<In, Map<In>> cache {};
         return cache.move_to_heap(std::forward<In>(x));
+    }
+    /** Heap cache function with emplacement */
+    template <typename In, typename... Args> static inline Map<In> *emplace_to_c(Args &&...args) {
+        static thread_local Utils::CHeapCache<In, Map<In>> cache {};
+        return cache.emplace_on_heap(std::forward<Args>(args)...);
     }
 } // namespace Private
 
@@ -70,76 +77,22 @@ namespace Private {
 
 extern "C" {
 
-/** Return a new Annotation::Base */
 ClaricppAnnotation *claricpp_annotation_new_base() {
     return Private::to_c(Annotation::factory<Annotation::Base>());
 }
 
-/** Return a new Annotation::SimplificationAvoidance */
 ClaricppAnnotation *claricpp_annotation_new_simplification_avoicance() {
     return Private::to_c(Annotation::factory<Annotation::SimplificationAvoidance>());
 }
-/** Create an Expression::SPAV from a list of annotations */
-// ClaricppSPAV * claricpp_annotation_create_spav() {
-// }
-}
 
-#if 0
-// Forward declarations
-namespace Expression {
-class Base;
-using BasePtr = std::shared_ptr<const Base>;
-} // namespace Expression
-
-
-namespace C_API {
-
-/** Annotation base pointer pointer abbreviation */
-using AnPtrPtr = Annotation::BasePtr *;
-
-namespace Private {
-    /** Expression heap cache */
-    thread_local Utils::CHeapCache<Expression::BasePtr> expression_heap_cache {};
-
-    Create::EBasePtr claricpp_abs_helper(Constants::CTSC<AnPtrPtr> annotations,
-                                         const Constants::UInt size,
-                                         Constants::CTSC<Create::EBasePtr> x) {
-        if (size == 0) {
-            return Create::abs(*x);
-        }
-        else {
-            Annotation::Vec::RawVec vec;
-            vec.reserve(size);
-            for (Constants::UInt i { 0 }; i < size; ++i) {
-                vec.emplace_back(*(annotations[i]));
-            }
-            return Create::abs(*x, std::make_shared<Annotation::Vec>(std::move(vec)));
-        }
+ClaricppSPAV *claricpp_annotation_create_spav(const ClaricppAnnotation *const *const list,
+                                              const SIZE_T len) {
+    Annotation::Vec::RawVec raw;
+    raw.reserve(len);
+    for (SIZE_T i = 0; i < len; ++i) {
+        raw.emplace_back(list[i]->ptr);
     }
-
-}; // namespace Private
-
-/** Enforce compatability with the C ABI
- *  Note: Defining extern "C" within a namespace defines the functions
- *  both within and outside of the namespace
- */
-extern "C" {
-
-/** For freeing expressions */
-void claricpp_expression_destructor(Expression::BasePtr *const ptr) {
-    Private::expression_heap_cache.free(ptr);
-}
-
-/** Create an ABS Expression with arguments: x
- *  @param annotations: An array of size AnPtrs
- *  @param size: The number of elements in annotations
- *  @param x: The operand to ABS
- */
-Create::EBasePtr *claricpp_abs(Constants::CTSC<AnPtrPtr> annotations,
-                               const Constants::UInt size,
-                               Constants::CTSC<Create::EBasePtr> x) {
-    return Private::expression_heap_cache.move_to_heap(
-        Private::claricpp_abs_helper(annotations, size, x));
+    using CV = Utils::InternalType<Annotation::SPAV>;
+    return Private::to_c(std::make_shared<CV>(std::move(raw)));
 }
 }
-#endif
--- native/src/api.h
+++ native/src/api.h
@@ -12,6 +12,12 @@
 /** Holds an Annotation::BasePtr reference */
 struct ClaricppAnnotation;
 
+/** Holds an Annotation::SPAV reference */
+struct ClaricppSPAV;
+
+/** Define size_t without polluting the global namespace */
+#define SIZE_T unsigned long long
+
 /********************************************************************/
 /*                            Annotation                            */
 /********************************************************************/
@@ -21,6 +27,12 @@ ClaricppAnnotation * claricpp_annotation_new_base();
 
 /** Return a new Annotation::SimplificationAvoidance */
 ClaricppAnnotation * claricpp_annotation_new_simplification_avoicance();
+
+/** Create an Annotation::SPAV from a list of annotations
+ *  @param list An array of ClaricppAnnotation pointers
+ *  @param len The length of list
+ */
+ClaricppSPAV * claricpp_annotation_create_spav(const ClaricppAnnotation * const * const list, const SIZE_T len);
 
 /********************************************************************/
 /*                            Expression                            */
