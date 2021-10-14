e68fedf53a60856107d625de50034ca3dac70d19
zwimer <zwimer@gmail.com>
2021-10-13 19:33:39 -0700

    API define symbol and most literals (not BigInt)

--- native/src/api.cpp
+++ native/src/api.cpp
@@ -3,6 +3,8 @@ extern "C" {
 #include "api.h"
 }
 
+// @todo: Handle exceptions
+
 #include "annotation.hpp"
 #include "backend.hpp"
 #include "create.hpp"
@@ -15,7 +17,11 @@ extern "C" {
 /*                        Type Declarations                         */
 /********************************************************************/
 
+static_assert(sizeof(std::size_t) == sizeof(Constants::UInt) && std::is_unsigned_v<std::size_t>,
+              "Constants::UInt needs to be changed");
 static_assert(std::is_same_v<SIZE_T, Constants::UInt>, "SIZE_T needs to be changed");
+static_assert(std::is_same_v<VS_T, PyObj::Base::Ref>, "VS_T needs to be changed");
+static_assert(std::is_same_v<HASH_T, Hash::Hash>, "HASH_T needs to be changed");
 
 extern "C" {
 
@@ -27,6 +33,10 @@ struct ClaricppAnnotation final {
 
 struct ClaricppSPAV final {
     Annotation::SPAV ptr;
+};
+
+struct ClaricppExpr final {
+    Expression::BasePtr ptr;
 };
 }
 
@@ -52,6 +62,8 @@ namespace Private {
     // Populate InternalMap
     MAP_ADD(ClaricppAnnotation)
     MAP_ADD(ClaricppSPAV)
+    MAP_ADD(ClaricppExpr)
+
 // Cleanup
 #undef MAP_ADD
 
@@ -95,4 +107,61 @@ ClaricppSPAV *claricpp_annotation_create_spav(const ClaricppAnnotation *const *c
     using CV = Utils::InternalType<Annotation::SPAV>;
     return Private::to_c(std::make_shared<CV>(std::move(raw)));
 }
+}
+
+/********************************************************************/
+/*                              Create                              */
+/********************************************************************/
+
+extern "C" {
+
+// Symbol
+
+ClaricppExpr *claricpp_create_symbol_bool(const char *const name) {
+    return Private::to_c(Create::symbol(std::string { name }));
+}
+
+/** A local macro used for consistency */
+#define CREATE_SYM(TYPE, NAME)                                                                    \
+    ClaricppExpr *claricpp_create_symbol_##NAME(const char *const name,                           \
+                                                const SIZE_T bit_length) {                        \
+        return Private::to_c(Create::symbol<Expression::TYPE>(std::string { name }, bit_length)); \
+    }
+
+CREATE_SYM(String, string);
+CREATE_SYM(VS, vs);
+CREATE_SYM(FP, fp);
+CREATE_SYM(BV, bv);
+
+// Cleanup
+#undef CREATE_SYM
+
+// Literal
+
+/** A local macro used for consistency */
+#define CREATE_LIT(TYPE, NAME)                                                                    \
+    ClaricppExpr *claricpp_create_literal_##NAME(const TYPE value) {                              \
+        return Private::to_c(Create::literal(value));                                             \
+    }
+
+CREATE_LIT(bool, bool);
+CREATE_LIT(char *const, string);
+CREATE_LIT(float, float);
+CREATE_LIT(double, double);
+
+ClaricppExpr *claricpp_create_literal_vs(const HASH_T hash, const VS_T value,
+                                         const SIZE_T bit_length) {
+    return Private::to_c(Create::literal(std::make_shared<PyObj::VS>(hash, value, bit_length)));
+}
+
+CREATE_LIT(uint8_t, u8);
+CREATE_LIT(uint16_t, u16);
+CREATE_LIT(uint32_t, u32);
+CREATE_LIT(uint64_t, u64);
+
+// @todo : BigInt
+// ClaricppExpr * claricpp_create_literal_bv_big_int(const ClaricppBigInt value);
+
+// Cleanup
+#undef CREATE_LIT
 }
--- native/src/api.h
+++ native/src/api.h
@@ -5,18 +5,28 @@
 #ifndef R_API_H_
 #define R_API_H_
 
+#include <stddef.h>
+#include <stdint.h>
+
 /********************************************************************/
 /*                    Forward Type Declarations                     */
 /********************************************************************/
 
-/** Holds an Annotation::BasePtr reference */
+/** Holds an Annotation::BasePtr */
 struct ClaricppAnnotation;
-
-/** Holds an Annotation::SPAV reference */
+/** Holds an Annotation::SPAV */
 struct ClaricppSPAV;
+/** Holds an Expression::Expr */
+struct ClaricppExpr;
+/** Holds a BigInt */
+struct ClaricppBigInt;
 
-/** Define size_t without polluting the global namespace */
+/** Define SIZE_T as Constants::UInt without polluting the global namespace */
 #define SIZE_T unsigned long long
+/** Define Hash::Hash without polluting the global namespace */
+#define HASH_T unsigned long long
+/** Define a type python can pass to represent a VS within claricpp */
+#define VS_T unsigned long long
 
 /********************************************************************/
 /*                            Annotation                            */
@@ -35,11 +45,100 @@ ClaricppAnnotation * claricpp_annotation_new_simplification_avoicance();
 ClaricppSPAV * claricpp_annotation_create_spav(const ClaricppAnnotation * const * const list, const SIZE_T len);
 
 /********************************************************************/
-/*                            Expression                            */
+/*                              Create                              */
 /********************************************************************/
 
+// Symbol
+
+/** Create an symbolic boolean expression
+ *  @param name The name of the symbol
+ */
+ClaricppExpr * claricpp_create_symbol_bool(const char * const name);
+
+/** Create an symbolic string expression
+ *  @param name The name of the symbol
+ *  @param bit_length The bit length of the symbol
+ */
+ClaricppExpr * claricpp_create_symbol_string(const char * const name, const SIZE_T bit_length);
+
+/** Create an symbolic VS expression
+ *  @param name The name of the symbol
+ *  @param bit_length The bit length of the symbol
+ */
+ClaricppExpr * claricpp_create_symbol_vs(const char * const name, const SIZE_T bit_length);
+
+/** Create an symbolic FP expression
+ *  @param name The name of the symbol
+ *  @param bit_length The bit length of the symbol
+ */
+ClaricppExpr * claricpp_create_symbol_fp(const char * const name, const SIZE_T bit_length);
+
+/** Create an symbolic BV expression
+ *  @param name The name of the symbol
+ *  @param bit_length The bit length of the symbol
+ */
+ClaricppExpr * claricpp_create_symbol_bv(const char * const name, const SIZE_T bit_length);
+
+// Literal
+
+/** Create a literal bool expression
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_bool(const bool value);
+
+/** Create a literal string expression
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_string(const char * const value);
+
+/** Create a literal float expression
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_fp_float(const float value);
+
+/** Create a literal double expression
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_fp_double(const double value);
+
+/** Create a literal VS expression
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_vs(const HASH_T hash, const VS_T value, const SIZE_T bit_length);
+
+/** Create a literal uint8_t expression
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_bv_u8(const uint8_t value);
+
+/** Create a literal uint16_t expression
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_bv_u16(const uint16_t value);
+
+/** Create a literal uint32_t expression
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_bv_u32(const uint32_t value);
+
+/** Create a literal uint64_t expression
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_bv_u64(const uint64_t value);
+
+/** Create a literal uint64_t expression
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_bv_u64(const uint64_t value);
+
+/** Create a literal double expression
+ *  @todo : BigInt
+ *  @param value The data held by the literal
+ */
+ClaricppExpr * claricpp_create_literal_bv_big_int(const ClaricppBigInt value);
+
 /********************************************************************/
-/*                              Create                              */
+/*                            Expression                            */
 /********************************************************************/
 
 /********************************************************************/
