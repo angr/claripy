458b000d5f932d2bb82c748bbf6d6c9e4754c606
zwimer <zwimer@gmail.com>
2021-10-14 19:56:58 -0700

    Includes testlib

---
+++ native/tests/unit/testlib/testlib/includes.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief This file includes many includes that tests should have access to
+ * \ingroup testlib
+ */
+#ifndef R_UNIT_TESTLIB_TESTLIB_INCLUDES_HPP_
+#define R_UNIT_TESTLIB_TESTLIB_INCLUDES_HPP_
+
+// @todo : clean up other test case includes
+extern "C" {
+#include "api.h"
+};
+
+#endif
