9e58f8b3e91c3df9e88a8ae70908fc1cf41b7789
zwimer <zwimer@gmail.com>
2021-10-15 23:17:48 -0700

    Bool tactic do not rely on tl_ctx

--- native/src/backend/z3/bool_tactic.hpp
+++ native/src/backend/z3/bool_tactic.hpp
@@ -5,42 +5,38 @@
 #ifndef R_BACKEND_Z3_BOOLTACTIC_HPP_
 #define R_BACKEND_Z3_BOOLTACTIC_HPP_
 
-#include "tl_ctx.hpp"
+#include <z3++.h>
 
-#include "../../utils.hpp"
+
 namespace Backend::Z3 {
 
     /** This class defines tactics used to simplify z3 bools */
     class BoolTactic {
       public:
+        /** Constructor */
+        inline BoolTactic(z3::context &c) : ctx { c }, tactics { mk_tactics(c) } {}
         /** Run the tactics on the given z3::expr */
         z3::expr operator()(const z3::expr &in) {
             auto in2 = in.simplify();
-            z3::goal goal { Private::tl_ctx };
+            z3::goal goal { ctx };
             goal.add(in2);
             const auto ar { tactics.apply(goal) };
             return to_expr(ar);
         }
 
       private:
-        /** The tactics contained by this class */
-        z3::tactic tactics { mk_tactics() };
-
         /** Used to create the tactics contained by this class */
-        static inline z3::tactic mk_tactics() {
-            return z3::tactic(Private::tl_ctx, "simplify") &
-                   z3::tactic(Private::tl_ctx, "propagate-ineqs") &
-                   z3::tactic(Private::tl_ctx, "propagate-values") &
-                   z3::tactic(Private::tl_ctx, "unit-subsume-simplify") &
-                   z3::tactic(Private::tl_ctx, "aig");
+        static inline z3::tactic mk_tactics(z3::context &c) {
+            return z3::tactic(c, "simplify") & z3::tactic(c, "propagate-ineqs") &
+                   z3::tactic(c, "propagate-values") & z3::tactic(c, "unit-subsume-simplify") &
+                   z3::tactic(c, "aig");
         }
-
         /** Convert a z3::apply_result a z3::expr */
-        static inline z3::expr to_expr(const z3::apply_result &ar) {
+        inline z3::expr to_expr(const z3::apply_result &ar) {
             const auto len { ar.size() };
             switch (len) {
                 case 0:
-                    return Private::tl_ctx.bool_val(false);
+                    return ctx.bool_val(false);
                 case 1:
                     return ar[0].as_expr();
                 // len > 1; or each goal as expressions
@@ -53,6 +49,11 @@ namespace Backend::Z3 {
                 }
             };
         }
+
+        /** The z3 context to use */
+        z3::context &ctx;
+        /** The tactics contained by this class */
+        z3::tactic tactics;
     };
 
 } // namespace Backend::Z3
