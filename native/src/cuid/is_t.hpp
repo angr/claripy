f25038b94272381ad4bd3d4e65800db362a077da
zwimer <zwimer@gmail.com>
2021-10-21 15:45:18 -0700

    Expr::is_any_t added

--- native/src/cuid/is_t.hpp
+++ native/src/cuid/is_t.hpp
@@ -39,6 +39,29 @@ namespace CUID {
         return is_t<T, false, Base>(x);
     }
 
+    namespace Private {
+        /** Return true if x is a Next or in Allowed */
+        template <typename Base, bool AllowKin, typename Next, typename... Allowed>
+        bool is_any(const std::shared_ptr<Base> &x) {
+            if constexpr (sizeof...(Allowed) > 0) {
+                return is_t<Next, AllowKin, Base>(x) || is_any<Base, AllowKin, Allowed...>(x);
+            }
+            return is_t<Next, AllowKin>(x);
+        }
+    } // namespace Private
+
+    /** Return true if x is in TS */
+    template <typename Base, bool AllowKin, typename Next, typename... Allowed>
+    bool is_any_t(const std::shared_ptr<Base> &x) {
+        return Private::is_any<Base, AllowKin, Next, Allowed...>(x);
+    }
+
+    /** Return true if x is a Next or in Allowed */
+    template <typename Base, typename Next, typename... Allowed>
+    bool is_any_t(const std::shared_ptr<Base> &x) {
+        return is_any_t<Base, false, Next, Allowed...>(x);
+    }
+
 } // namespace CUID
 
 #endif
