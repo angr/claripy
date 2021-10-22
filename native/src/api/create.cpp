4585c84bacc090c88e8c1bc001d36e476f6ff717
zwimer <zwimer@gmail.com>
2021-10-22 14:41:21 -0700

    Indent extern blocks via clang-format

--- native/src/api/create.cpp
+++ native/src/api/create.cpp
@@ -17,13 +17,13 @@ static inline auto make(const F &f, ClaricppSPAV &spav, Args &&...args) {
 
 extern "C" {
 
-// Symbol
+    // Symbol
 
-ClaricppExpr claricpp_create_symbol_bool(PyStr name, ClaricppSPAV spav) {
-    static constexpr Expr::BasePtr (*const f)(std::string &&,
-                                              Annotation::SPAV &&) { Create::symbol };
-    return make(f, spav, std::string { name });
-}
+    ClaricppExpr claricpp_create_symbol_bool(PyStr name, ClaricppSPAV spav) {
+        static constexpr Expr::BasePtr (*const f)(std::string &&,
+                                                  Annotation::SPAV &&) { Create::symbol };
+        return make(f, spav, std::string { name });
+    }
 
 /** A local macro used for consistency */
 #define CREATE_SYM(TYPE, NAME)                                                                    \
@@ -32,10 +32,10 @@ ClaricppExpr claricpp_create_symbol_bool(PyStr name, ClaricppSPAV spav) {
         return make(Create::symbol<Expr::TYPE>, spav, std::string { name }, bit_length);          \
     }
 
-CREATE_SYM(String, string);
-CREATE_SYM(VS, vs);
-CREATE_SYM(FP, fp);
-CREATE_SYM(BV, bv);
+    CREATE_SYM(String, string);
+    CREATE_SYM(VS, vs);
+    CREATE_SYM(FP, fp);
+    CREATE_SYM(BV, bv);
 
 // Cleanup
 #undef CREATE_SYM
@@ -49,74 +49,76 @@ CREATE_SYM(BV, bv);
         return make(f, spav, value);                                                              \
     }
 
-CREATE_LIT(bool, bool);
-CREATE_LIT(float, fp_float);
-CREATE_LIT(double, fp_double);
-CREATE_LIT(uint8_t, bv_u8);
-CREATE_LIT(uint16_t, bv_u16);
-CREATE_LIT(uint32_t, bv_u32);
-CREATE_LIT(uint64_t, bv_u64);
+    CREATE_LIT(bool, bool);
+    CREATE_LIT(float, fp_float);
+    CREATE_LIT(double, fp_double);
+    CREATE_LIT(uint8_t, bv_u8);
+    CREATE_LIT(uint16_t, bv_u16);
+    CREATE_LIT(uint32_t, bv_u32);
+    CREATE_LIT(uint64_t, bv_u64);
 
 // Cleanup
 #undef CREATE_LIT
 
-ClaricppExpr claricpp_create_literal_string(PyStr str, ClaricppSPAV spav) {
-    static constexpr Expr::BasePtr (*const f)(std::string &&,
-                                              Annotation::SPAV &&) { Create::literal };
-    return make(f, spav, std::string { str });
-}
+    ClaricppExpr claricpp_create_literal_string(PyStr str, ClaricppSPAV spav) {
+        static constexpr Expr::BasePtr (*const f)(std::string &&,
+                                                  Annotation::SPAV &&) { Create::literal };
+        return make(f, spav, std::string { str });
+    }
 
-ClaricppExpr claricpp_create_literal_vs(const HASH_T hash, const VS_T value,
-                                        const SIZE_T bit_length, ClaricppSPAV spav) {
-    static constexpr Expr::BasePtr (*const f)(PyObj::VSPtr &&,
-                                              Annotation::SPAV &&) { Create::literal };
-    return make(f, spav, std::make_shared<PyObj::VS>(hash, value, bit_length));
-}
+    ClaricppExpr claricpp_create_literal_vs(const HASH_T hash, const VS_T value,
+                                            const SIZE_T bit_length, ClaricppSPAV spav) {
+        static constexpr Expr::BasePtr (*const f)(PyObj::VSPtr &&,
+                                                  Annotation::SPAV &&) { Create::literal };
+        return make(f, spav, std::make_shared<PyObj::VS>(hash, value, bit_length));
+    }
 
-ClaricppExpr claricpp_create_literal_bv_big_int_mode_str(PyStr value, const SIZE_T bit_length,
-                                                         ClaricppSPAV spav) {
-    static constexpr Expr::BasePtr (*const f)(BigInt &&, Annotation::SPAV &&) { Create::literal };
-    return make(f, spav, BigInt { BigInt::Str { value }, bit_length });
-}
+    ClaricppExpr claricpp_create_literal_bv_big_int_mode_str(PyStr value, const SIZE_T bit_length,
+                                                             ClaricppSPAV spav) {
+        static constexpr Expr::BasePtr (*const f)(BigInt &&,
+                                                  Annotation::SPAV &&) { Create::literal };
+        return make(f, spav, BigInt { BigInt::Str { value }, bit_length });
+    }
 
-ClaricppExpr claricpp_create_literal_bv_big_int_mode_int(PyStr value, const SIZE_T bit_length,
-                                                         ClaricppSPAV spav) {
-    static constexpr Expr::BasePtr (*const f)(BigInt &&, Annotation::SPAV &&) { Create::literal };
-    return make(f, spav, BigInt { BigInt::Int { value }, bit_length });
-}
+    ClaricppExpr claricpp_create_literal_bv_big_int_mode_int(PyStr value, const SIZE_T bit_length,
+                                                             ClaricppSPAV spav) {
+        static constexpr Expr::BasePtr (*const f)(BigInt &&,
+                                                  Annotation::SPAV &&) { Create::literal };
+        return make(f, spav, BigInt { BigInt::Int { value }, bit_length });
+    }
 
-// Non Trivial
+    // Non Trivial
 
-ClaricppExpr claricpp_create_extract(const SIZE_T high, const SIZE_T low, const ClaricppExpr from,
-                                     ClaricppSPAV spav) {
-    return make(Create::extract, spav, high, low, API::to_cpp_ref(from));
-}
+    ClaricppExpr claricpp_create_extract(const SIZE_T high, const SIZE_T low,
+                                         const ClaricppExpr from, ClaricppSPAV spav) {
+        return make(Create::extract, spav, high, low, API::to_cpp_ref(from));
+    }
 
-ClaricppExpr claricpp_create_if(const ClaricppExpr cond, const ClaricppExpr left,
-                                const ClaricppExpr right, ClaricppSPAV spav) {
-    return make(Create::if_, spav, API::to_cpp_ref(cond), API::to_cpp_ref(left),
-                API::to_cpp_ref(right));
-}
+    ClaricppExpr claricpp_create_if(const ClaricppExpr cond, const ClaricppExpr left,
+                                    const ClaricppExpr right, ClaricppSPAV spav) {
+        return make(Create::if_, spav, API::to_cpp_ref(cond), API::to_cpp_ref(left),
+                    API::to_cpp_ref(right));
+    }
 
-// Trivial unary
+    // Trivial unary
 
-ClaricppExpr claricpp_create_abs(const ClaricppExpr x, ClaricppSPAV spav) {
-    return make(Create::abs, spav, API::to_cpp_ref(x));
-}
+    ClaricppExpr claricpp_create_abs(const ClaricppExpr x, ClaricppSPAV spav) {
+        return make(Create::abs, spav, API::to_cpp_ref(x));
+    }
 
-ClaricppExpr claricpp_create_neg(const ClaricppExpr x, ClaricppSPAV spav) {
-    return make(Create::neg, spav, API::to_cpp_ref(x));
-}
+    ClaricppExpr claricpp_create_neg(const ClaricppExpr x, ClaricppSPAV spav) {
+        return make(Create::neg, spav, API::to_cpp_ref(x));
+    }
 
-ClaricppExpr claricpp_create_not(const ClaricppExpr x, ClaricppSPAV spav) {
-    return make(Create::not_, spav, API::to_cpp_ref(x));
-}
+    ClaricppExpr claricpp_create_not(const ClaricppExpr x, ClaricppSPAV spav) {
+        return make(Create::not_, spav, API::to_cpp_ref(x));
+    }
 
-ClaricppExpr claricpp_create_invert(const ClaricppExpr x, ClaricppSPAV spav) {
-    return make(Create::invert, spav, API::to_cpp_ref(x));
-}
+    ClaricppExpr claricpp_create_invert(const ClaricppExpr x, ClaricppSPAV spav) {
+        return make(Create::invert, spav, API::to_cpp_ref(x));
+    }
 
-ClaricppExpr claricpp_create_reverse(const ClaricppExpr x, ClaricppSPAV spav) {
-    return make(Create::reverse, spav, API::to_cpp_ref(x));
-}
+    ClaricppExpr claricpp_create_reverse(const ClaricppExpr x, ClaricppSPAV spav) {
+        return make(Create::reverse, spav, API::to_cpp_ref(x));
+    }
 }
