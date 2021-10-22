0cbd0f570ee8546580312982acdbb6ba21792f1a
hwu71 <hwu71@binghamton.edu>
2021-10-21 21:19:58 -0400

    Add ret_size when creating a Function object.

--- claripy/ast/func.py
+++ claripy/ast/func.py
@@ -9,11 +9,13 @@ l = logging.getLogger("claripy.ast.func")
 class Func(Base):
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        #super().__init__(*args, **kwargs)
+        self.ret_size = self._ret_size if self._ret_size is not None else 32
+
         self.func_name = self.op
         self.args = self.args
         args_type = [type(arg) for arg in self.args]
-        self.func_op = operations.op(self.func_name, args_type, BV, bound=False, calc_length=lambda *a: 64)
+        self.func_op = operations.op(self.func_name, args_type, BV, bound=False, calc_length=lambda *a: self.ret_size)
         #operations.backend_func_operations.add(self.op)
 
     def get_func_name(self):
