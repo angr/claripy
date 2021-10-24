fe7d800f062f99555c998881643fb1fcfe442e8a
zwimer <zwimer@gmail.com>
2021-10-23 21:52:20 -0700

    Run before / after main test improvements / added

---
+++ native/tests/unit/util/run_after_main.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * \ingroup unittest
+ */
+#include "testlib.hpp"
+
+
+/** A function which fails this tese case after main */
+[[noreturn]] static void err() {
+    // Print backtrace
+    try {
+        UNITTEST_ERR("This should have been disabled");
+    }
+    catch (UnitTest::TestLib::Error &) {
+    }
+    // Exit 1 since raising the exception after main won't change main's exit code
+    std::exit(1);
+}
+
+/** A list of variables to be used by run after main checks */
+std::vector<Util::RunOnDestruction<decltype(err)>> errors {};
+
+// Prevent errors from triggering after main
+// Note that we do each one twice to check for namespace collisions
+UTILS_RUN_STATEMENT_AFTER_MAIN(errors[0].disable());
+UTILS_RUN_STATEMENT_AFTER_MAIN(errors[1].disable());
+UTILS_RUN_FUNCTION_AFTER_MAIN([]() { errors[2].disable(); });
+UTILS_RUN_FUNCTION_AFTER_MAIN([]() { errors[3].disable(); });
+
+
+/** Test running statements after main */
+void run_after_main() {
+    for (int i { 0 }; i < 4; ++i) {
+        errors.emplace_back(err);
+        UNITTEST_ASSERT(errors.back().status()); // Ensure each ROD is enabled
+    }
+    UNITTEST_ASSERT(errors.size() == 4); // Sanity check
+}
+
+// Define the test
+UNITTEST_DEFINE_MAIN_TEST(run_after_main)
---
+++ native/tests/unit/util/run_before_main.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * \ingroup unittest
+ */
+#include "testlib.hpp"
+
+
+// Run Before Main
+
+/** A global variable */
+static std::set<int> test_set {};
+
+// Insert values to test_set
+// Note that we do each one twice to check for namespace collisions
+UTILS_RUN_STATEMENT_BEFORE_MAIN(test_set.insert(0));
+UTILS_RUN_STATEMENT_BEFORE_MAIN(test_set.insert(1));
+UTILS_RUN_FUNCTION_BEFORE_MAIN([]() { test_set.insert(2); });
+UTILS_RUN_FUNCTION_BEFORE_MAIN([]() { test_set.insert(3); });
+
+
+/** Test running statements before main */
+void run_before_main() {
+    UNITTEST_ASSERT(test_set.size() == 4);
+    const std::set<int> desired { 0, 1, 2, 3 };
+    UNITTEST_ASSERT(test_set == desired);
+}
+
+// Define the test
+UNITTEST_DEFINE_MAIN_TEST(run_before_main)
