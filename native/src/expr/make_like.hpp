a0f6ed65579ace2de62c7e5d5d486a9bc34432f0
zwimer <zwimer@gmail.com>
2021-10-27 17:56:02 -0700

    make_like fix

--- native/src/expr/make_like.hpp
+++ native/src/expr/make_like.hpp
@@ -6,8 +6,6 @@
 #define R_EXPR_MAKELIKE_HPP_
 
 #include "base.hpp"
-#include "factory.hpp"
-#include "instantiables.hpp"
 
 
 namespace Expr {
@@ -15,14 +13,7 @@ namespace Expr {
     /** Copy the expr, but use the newly provided annotation vector
      *  in may not be nullptr
      */
-    inline Expr::BasePtr make_like(const Expr::BasePtr &in, Annotation::SPAV &&sp) {
-        UTILS_AFFIRM_NOT_NULL_DEBUG(in);
-        if (in->cuid == Bool::static_cuid) {
-            return ::Expr::factory<Bool>(in->symbolic, Op::BasePtr { in->op }, std::move(sp));
-        }
-        return factory_cuid(in->cuid, in->symbolic, Op::BasePtr { in->op },
-                            Expr::get_bit_length(in), std::move(sp));
-    }
+    BasePtr make_like(const BasePtr &in, Annotation::SPAV &&sp);
 
 } // namespace Expr
 
