a749567f545bdcaf983af0bbca7995d172aa4ba4
zwimer <zwimer@gmail.com>
2021-11-02 18:05:44 -0700

    API support array return types

--- native/src/api/constants.h
+++ native/src/api/constants.h
@@ -13,24 +13,33 @@
 #include "../mode.h"
 
 
-/** Used to declare a C wrapper for a C++ type */
-#define DECLARE_WRAPPER(NAME)                                                                      \
-    /** A C wrapper for a C++ type */                                                              \
-    struct NAME {                                                                                  \
-        void *ptr;                                                                                 \
-    }
-
-DECLARE_WRAPPER(ClaricppAnnotation);
-DECLARE_WRAPPER(ClaricppSPAV);
-DECLARE_WRAPPER(ClaricppExpr);
-DECLARE_WRAPPER(ClaricppBackend);
-DECLARE_WRAPPER(ClaricppSolver);
+// Macros
 
 /** Return the type of an input array of type T */
 #define ARRAY_IN(T) const T *const
 
-/** The type a Python string argument is */
-typedef ARRAY_IN(char) PyStr;
+/** An array of X; can only be used by types that have corresponding array types */
+#define ARRAY_OUT(X) X##OutArray
+
+/** A local macro used to declare a C wrapper for a C++ type */
+#define DECLARE_WRAPPER(NAME)                                                                      \
+    /** A C wrapper for a C++ type */                                                              \
+    struct NAME {                                                                                  \
+        /** An obscure point to a C++ type */                                                      \
+        void *ptr;                                                                                 \
+    }
+
+/** A local macro used to declare an out array of a C type */
+#define DECLARE_OUT_ARRAY(NAME)                                                                    \
+    /** The wrapper struct for the array */                                                        \
+    struct ARRAY_OUT(NAME) {                                                                       \
+        /** An array of C objects */                                                               \
+        struct NAME *arr;                                                                          \
+        /** The length of the array */                                                             \
+        SIZE_T len;                                                                                \
+    }
+
+// Primitives
 
 /** Define UINT as UInt without polluting the global namespace */
 #define UINT unsigned long long
@@ -45,8 +54,29 @@ typedef ARRAY_IN(char) PyStr;
 
 /** Define a boolean type */
 #define BOOL int_fast8_t
+/** C TRUE has a value of 1, like C++ true when cast to an int */
 #define TRUE 1
+/** C FALSE has a value of 0, like C++ false when cast to an int */
 #define FALSE 0
+
+// C Struct Types
+
+DECLARE_WRAPPER(ClaricppAnnotation);
+DECLARE_WRAPPER(ClaricppSPAV);
+DECLARE_WRAPPER(ClaricppExpr);
+DECLARE_WRAPPER(ClaricppBackend);
+DECLARE_WRAPPER(ClaricppSolver);
+
+DECLARE_OUT_ARRAY(ClaricppExpr);
+
+// Cleanup
+#undef DECLARE_WRAPPER
+#undef DECLARE_OUT_ARRAY
+
+// Other C types
+
+/** The type a Python string argument is */
+typedef ARRAY_IN(char) PyStr;
 
 /** Claricpp rounding modes
  *  See mode/fp/rounding.h for more info
@@ -57,8 +87,5 @@ typedef enum { MODE_FP_ROUNDING_VALS(ClaricppRm) } ClaricppRM;
  *  See mode/big_int.h for more info
  */
 typedef enum { MODE_BIGINT_VALS(ClaricppBim) } ClaricppBIM;
-
-// Cleanup
-#undef DECLARE_WRAPPER
 
 #endif
--- native/src/api/cpp.hpp
+++ native/src/api/cpp.hpp
@@ -7,27 +7,31 @@
 #define R_API_CPP_HPP_
 
 extern "C" {
-#include "c.h"
+#include "../api.h"
 };
 #include "../backend.hpp"
 
 // @todo: Handle exceptions
 
 // Static checks
+
 /** A local macro used for static tests */
 #define SAME_U(A, B) (sizeof(A) == sizeof(B) && std::is_unsigned_v<A> && std::is_unsigned_v<B>)
+
 static_assert(SAME_U(UINT, UInt), "UINT needs to be changed");
 static_assert(SAME_U(std::size_t, UInt), "UInt needs to be changed");
 static_assert(SAME_U(SIZE_T, UInt), "UInt needs to be changed");
 static_assert(SAME_U(VS_T, PyObj::Base::Ref), "VS_T needs to be changed");
 static_assert(SAME_U(HASH_T, Hash::Hash), "HASH_T needs to be changed");
-// Cleanup
-#undef SAME_U
+
 static_assert(std::is_same_v<Z3U, unsigned>, "Z3U needs to be changed");
 static_assert(std::is_same_v<CCSC, PyStr>, "PyStr needs to be changed");
 static_assert(std::is_same_v<PyStr, ARRAY_IN(char)>, "ARRAY_IN needs to be changed");
 static_assert((FALSE == false) && (false == FALSE) && (TRUE == true) && (true == TRUE),
               "BOOL values need to be fixed");
+
+// Cleanup
+#undef SAME_U
 
 /********************************************************************/
 /*                               C++                                */
@@ -38,6 +42,8 @@ namespace API {
     namespace Private {
         /** Bidirectionally maps between C++ and C types */
         template <typename T> struct InternalMap;
+        /** Maps a C type to a C array type */
+        template <typename T> struct InternalArrMap;
         /** Bidirectionally maps between C++ and C types
          *  Warning: Enums are assumed to have the same values between both
          */
@@ -53,6 +59,13 @@ namespace API {
     template <> struct InternalMap<CPPTYPE> final { using Result = CTYPE; };                       \
     template <> struct InternalMap<CTYPE> final { using Result = CPPTYPE; };
 
+/** A local macro used to add an array Map entry
+ *  Requires C type be in InternalMap
+ */
+#define ARR_MAP_ADD(CTYPE)                                                                         \
+    static_assert(Util::TD::true_<InternalMap<CTYPE>::Result>, "CTYPE not in InternalMap");        \
+    template <> struct InternalArrMap<CTYPE> final { using Result = ARRAY_OUT(CTYPE); };
+
 /** A local macro used to add an Enum Map entry */
 #define ENUM_MAP_ADD(CTYPE, CPPTYPE)                                                               \
     static_assert(std::is_enum_v<CTYPE>, "Must be an enum");                                       \
@@ -67,11 +80,15 @@ namespace API {
         MAP_ADD(ClaricppBackend, std::shared_ptr<::Backend::Base>);
         MAP_ADD(ClaricppSolver, std::shared_ptr<z3::solver>);
 
+        ARR_MAP_ADD(ClaricppExpr);
+
         ENUM_MAP_ADD(ClaricppRM, Mode::FP::Rounding);
         ENUM_MAP_ADD(ClaricppBIM, Mode::BigInt);
 
 // Cleanup
 #undef MAP_ADD
+#undef ARR_MAP_ADD
+#undef ENUM_MAP_ADD
 
         /** A shortcut used to access InternalMap */
         template <typename T> using Map = typename InternalMap<T>::Result;
@@ -144,12 +161,41 @@ namespace API {
             std::forward<Args>(args)...) };
     }
 
+    /** Convert a C++ vector to a C array */
+    template <typename InCpp> static inline auto to_arr(std::vector<InCpp> &&arr) {
+        using CType = Private::Map<InCpp>;
+        typename Private::InternalArrMap<CType>::Result ret;
+        // Array allocation
+        ret.len = arr.size();
+        Util::Log::debug("Allocating an array C types of length: ", ret.len);
+        ret.arr = Util::Safe::malloc<CType>(ret.len);
+        // Array population
+        for (SIZE_T i { 0 }; i < ret.len; ++i) {
+            ret.arr[i] = API::to_c(std::move(arr[i]));
+        }
+        // Cleanup and return
+        arr.clear();
+        return ret;
+    }
+
     // Cleanup
 
     /** Heap cache free function */
     template <typename InC> static inline void free(InC &x) {
         Private::cache<Private::Map<InC>>.free(&to_cpp(x));
         x.ptr = nullptr;
+    }
+
+    /** Array free function */
+    template <typename InC> static inline void free_array(InC &x) {
+        using CType = std::remove_pointer_t<decltype(x.arr)>;
+        static_assert(Util::TD::true_<Private::Map<CType>>, "Unknown array type");
+        for (SIZE_T i { 0 }; i < x.len; ++i) {
+            API::free(x.arr[i]);
+        }
+        std::free(x.arr);
+        x.arr = nullptr;
+        x.len = 0;
     }
 
     // Rounding mode
