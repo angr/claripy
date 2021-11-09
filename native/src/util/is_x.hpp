dcd5d43f7452c645ab2e9d8d4f066eb2fac7e1bf
zwimer <zwimer@gmail.com>
2021-11-08 19:45:08 -0700

    Util is_x

---
+++ native/src/util/is_x.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * \ingroup util
+ * @brief Defines is_x and related functions
+ */
+#ifndef R_UTIL_ISX_HPP_
+#define R_UTIL_ISX_HPP_
+
+#include <memory>
+#include <type_traits>
+
+
+namespace Util {
+
+    namespace Private {
+        /** Returns true if T is a shared pointer */
+        template <template <typename...> typename, typename> struct IsX : std::false_type {};
+        /** Returns true if the passed type is a shared pointer */
+        template <template <typename...> typename Container, typename... Args>
+        struct IsX<Container, Container<Args...>> : std::true_type {};
+    } // namespace Private
+
+    /** Return true if T is an X */
+    template <template <typename...> typename X, typename T>
+    UTILS_ICCBOOL is_x { Private::IsX<X, T>::value };
+
+    // Common uses
+
+    /** Return true if T is a shared_ptr */
+    template <typename T> UTILS_ICCBOOL is_shared_ptr { is_x<std::shared_ptr, T> };
+
+} // namespace Util
+#endif
