1f74d1a3d05c0a82dcfea044cf4aaa0e6b521f28
zwimer <zwimer@gmail.com>
2021-11-16 21:10:18 -0700

    Type Pair added

---
+++ native/src/util/type/pair.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * \ingroup util
+ * @brief This file defines type pair type
+ */
+#ifndef R_UTIL_TYPE_PAIR_HPP_
+#define R_UTIL_TYPE_PAIR_HPP_
+
+namespace Util::Type {
+
+    /** A type which is a pair of two other types */
+    template <typename A, typename B> struct Pair {
+        /** The first type */
+        using First = A;
+        /** The second type */
+        using Second = B;
+    };
+
+} // namespace Util::Type
+
+#endif
