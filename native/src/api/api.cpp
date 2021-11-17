8e79dde346b9a7a2b2037e25fa3b153123be8645
zwimer <zwimer@gmail.com>
2021-11-17 16:19:17 -0700

    API free support improved

--- native/src/api/api.cpp
+++ native/src/api/api.cpp
@@ -16,19 +16,39 @@
 #define DEFINE_ARR_FREE_FUNC(TYPE, NAME)                                                           \
     void claricpp_free_array_##NAME(ARRAY_OUT(TYPE) *const c_array) {                              \
         UTIL_AFFIRM_NOT_NULL_DEBUG(c_array);                                                       \
-        API::free_array(*c_array);                                                                 \
+        API::free<true>(*c_array);                                                                 \
+    }
+
+/** A local macro used for consistency */
+#define DEFINE_DOUBLE_ARR_FREE_FUNC(TYPE, NAME)                                                    \
+    void claricpp_free_double_array_##NAME(DOUBLE_ARRAY_OUT(TYPE) *const c_array) {                \
+        UTIL_AFFIRM_NOT_NULL_DEBUG(c_array);                                                       \
+        API::free<2>(*c_array);                                                                    \
     }
 
 extern "C" {
+    // Unions
+    DEFINE_FREE_FUNC(ClaricppPrim, prim);
+    DEFINE_FREE_FUNC(ClaricppArg, arg);
+
+    // Structs
     DEFINE_FREE_FUNC(ClaricppAnnotation, annotation);
     DEFINE_FREE_FUNC(ClaricppSPAV, spav);
     DEFINE_FREE_FUNC(ClaricppExpr, expr);
     DEFINE_FREE_FUNC(ClaricppBackend, backend);
     DEFINE_FREE_FUNC(ClaricppSolver, solver);
 
+    // Arrays
+    DEFINE_ARR_FREE_FUNC(ClaricppAnnotation, annotation);
     DEFINE_ARR_FREE_FUNC(ClaricppExpr, expr);
+    DEFINE_ARR_FREE_FUNC(ClaricppPrim, prim);
+    DEFINE_ARR_FREE_FUNC(ClaricppArg, arg);
+
+    // Doubles Arrays
+    DEFINE_DOUBLE_ARR_FREE_FUNC(ClaricppPrim, prim);
 }
 
 // Cleanup
 #undef DEFINE_FREE_FUNC
 #undef DEFINE_ARR_FREE_FUNC
+#undef DEFINE_DOUBLE_ARR_FREE_FUNC
--- native/src/api/api.h
+++ native/src/api/api.h
@@ -23,16 +23,38 @@
      */                                                                                            \
     void claricpp_free_array_##NAME(ARRAY_OUT(TYPE) *const c_array); // Yes, ARRAY_OUT
 
+/** A local macro used for consistency */
+#define DECLARE_DOUBLE_ARR_FREE_FUNC(TYPE, NAME)                                                   \
+    /** Frees every array object, array, iteself, then nulls c_array                               \
+     *  @param c_array The double array to have its contents and itself freed then nulled          \
+     */                                                                                            \
+    void claricpp_free_double_array_##NAME(DOUBLE_ARRAY_OUT(TYPE) *const c_array); // Yes, ARRAY_OUT
+
+// Free functions
+
+// Unions
+DECLARE_FREE_FUNC(ClaricppPrim, prim);
+DECLARE_FREE_FUNC(ClaricppArg, arg);
+
+// Structs
 DECLARE_FREE_FUNC(ClaricppAnnotation, annotation);
 DECLARE_FREE_FUNC(ClaricppSPAV, spav);
 DECLARE_FREE_FUNC(ClaricppExpr, expr);
 DECLARE_FREE_FUNC(ClaricppBackend, backend);
 DECLARE_FREE_FUNC(ClaricppSolver, solver);
 
+// Arrays
+DECLARE_ARR_FREE_FUNC(ClaricppAnnotation, annotation);
 DECLARE_ARR_FREE_FUNC(ClaricppExpr, expr);
+DECLARE_ARR_FREE_FUNC(ClaricppPrim, prim);
+DECLARE_ARR_FREE_FUNC(ClaricppArg, arg);
+
+// Double Arrays
+DECLARE_DOUBLE_ARR_FREE_FUNC(ClaricppPrim, prim);
 
 // Cleanup
 #undef DECLARE_FREE_FUNC
 #undef DECLARE_ARR_FREE_FUNC
+#undef DECLARE_DOUBLE_ARR_FREE_FUNC
 
 #endif
