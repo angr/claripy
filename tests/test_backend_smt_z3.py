a8601286c9928f1bfd397cab719ab0204a363ba5
ASUAD\aganes17 <adersh.g.97@gmail.com>
2021-11-24 15:44:52 -0700

    skip a test due to problem with pysmt

--- tests/test_backend_smt_z3.py
+++ tests/test_backend_smt_z3.py
@@ -3,12 +3,12 @@ from unittest import skip
 import claripy
 from claripy.backends.backend_smtlib_solvers.z3_popen import SolverBackendZ3
 import common_backend_smt_solver
-from common_backend_smt_solver import if_installed
+
 
 
 class SmtLibSolverTest_Z3(common_backend_smt_solver.SmtLibSolverTestBase):
     # Skip these test for now because of a problem with pysmt
-    @if_installed
+    @skip
     def get_solver(self):
         backend = SolverBackendZ3(daggify=True)
         solver = claripy.SolverStrings(backend=backend)
