f1addb069e8056e528c868ec6c0538ba06d06a57
zwimer <zwimer@gmail.com>
2021-12-10 02:14:19 -0700

    Free API handle exceptions

--- native/src/api/free.cpp
+++ native/src/api/free.cpp
@@ -8,22 +8,28 @@
 /** A local macro used for consistency */
 #define DEFINE_FREE_FUNC(TYPE, NAME)                                                               \
     void claricpp_free_##NAME(TYPE *const c_object) {                                              \
+        API_FUNC_START                                                                             \
         UTIL_ASSERT_NOT_NULL_DEBUG(c_object);                                                      \
         API::free(*c_object);                                                                      \
+        API_FUNC_END_NO_RETURN                                                                     \
     }
 
 /** A local macro used for consistency */
 #define DEFINE_ARR_FREE_FUNC(TYPE, NAME)                                                           \
     void claricpp_free_array_##NAME(ARRAY_OUT(TYPE) *const c_array) {                              \
+        API_FUNC_START                                                                             \
         UTIL_ASSERT_NOT_NULL_DEBUG(c_array);                                                       \
         API::free<true>(*c_array);                                                                 \
+        API_FUNC_END_NO_RETURN                                                                     \
     }
 
 /** A local macro used for consistency */
 #define DEFINE_DOUBLE_ARR_FREE_FUNC(TYPE, NAME)                                                    \
     void claricpp_free_double_array_##NAME(DOUBLE_ARRAY_OUT(TYPE) *const c_array) {                \
+        API_FUNC_START                                                                             \
         UTIL_ASSERT_NOT_NULL_DEBUG(c_array);                                                       \
         API::free<2>(*c_array);                                                                    \
+        API_FUNC_END_NO_RETURN                                                                     \
     }
 
 extern "C" {
