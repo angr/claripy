303e0900c2251d640ddc4b950b42c253d68f78a1
zwimer <zwimer@gmail.com>
2021-12-10 02:13:21 -0700

    Annotation API handle exceptions

--- native/src/api/annotation.cpp
+++ native/src/api/annotation.cpp
@@ -10,11 +10,15 @@
 extern "C" {
 
     ClaricppAnnotation claricpp_annotation_new_base() {
+        API_FUNC_START
         return API::to_c(Annotation::factory<Annotation::Base>());
+        API_FUNC_END
     }
 
     ClaricppAnnotation claricpp_annotation_new_simplification_avoidance() {
+        API_FUNC_START
         return API::to_c(Annotation::factory<Annotation::SimplificationAvoidance>());
+        API_FUNC_END
     }
 
     ClaricppSPAV claricpp_annotation_create_empty_spav() { return { nullptr }; }
@@ -24,6 +28,7 @@ extern "C" {
         if (UNLIKELY(len == 0)) {
             return claricpp_annotation_create_empty_spav();
         }
+        API_FUNC_START
         UTIL_ASSERT_NOT_NULL_DEBUG(list);
         Annotation::Vec::RawVec raw;
         raw.reserve(len);
@@ -33,26 +38,33 @@ extern "C" {
         }
         using CV = Util::Type::Internal<Annotation::SPAV>;
         return API::to_c(std::make_shared<CV>(std::move(raw)));
+        API_FUNC_END
     }
 
     ARRAY_OUT(ClaricppAnnotation) claricpp_annotation_spav_to_array(const ClaricppSPAV spav) {
         if (spav.ptr == nullptr) {
             return { .arr = nullptr, .len = 0 };
         }
+        API_FUNC_START
         return API::copy_to_arr(API::to_cpp_ref(spav).vec);
+        API_FUNC_END
     }
 
     SIZE_T claricpp_annotation_spav_len(const ClaricppSPAV spav) {
         if (spav.ptr == nullptr) {
             return 0;
         }
+        API_FUNC_START
         return API::to_cpp_ref(spav).vec.size();
+        API_FUNC_END
     }
 
     ClaricppAnnotation claricpp_annotation_spav_get(const ClaricppSPAV spav, const SIZE_T i) {
+        API_FUNC_START
 #ifdef DEBUG
         UTIL_ASSERT(Util::Err::Index, i < API::to_cpp_ref(spav).vec.size(), "Index out of bounds");
 #endif
         return API::copy_to_c(API::to_cpp_ref(spav).vec[i]);
+        API_FUNC_END
     }
 }
