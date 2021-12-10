d3d82bab2bcc0912f55b52244cf53f4b77768819
zwimer <zwimer@gmail.com>
2021-12-09 21:07:39 -0700

    Util affirm -> assert macros

---
+++ native/src/util/assert.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * \ingroup util
+ * @brief This file defines a macro which asserts a condition, and if false throws an exception
+ */
+#ifndef R_UTIL_ASSERT_HPP_
+#define R_UTIL_ASSERT_HPP_
+
+#include "throw.hpp"
+
+
+/** A convenient assertion, msg = WHOAMI
+ *  We do this instead of a function to keep stack traces clean and reduce binary sizes
+ */
+#define UTIL_ASSERT_EMPTY(E_TYPE, COND)                                                            \
+    {                                                                                              \
+        if (UNLIKELY(!(COND))) {                                                                   \
+            UTIL_THROW_EMPTY(E_TYPE);                                                              \
+        }                                                                                          \
+    }
+
+/** A convenient assertion, prepends WHOAMI to ...
+ *  We do this instead of a function to keep stack traces clean and reduce binary sizes
+ */
+#define UTIL_ASSERT(E_TYPE, COND, ...)                                                             \
+    {                                                                                              \
+        if (UNLIKELY(!(COND))) {                                                                   \
+            UTIL_THROW(E_TYPE, __VA_ARGS__);                                                       \
+        }                                                                                          \
+    }
+
+
+#endif
---
+++ native/src/util/assert_not_null_debug.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * \ingroup util
+ * @brief This file defines a macro to affirm something is not nullptr when DEBUG is defined
+ */
+#ifndef R_UTIL_ASSERTNOTNULLDEBUG_HPP_
+#define R_UTIL_ASSERTNOTNULLDEBUG_HPP_
+
+#include "assert.hpp"
+#include "err.hpp"
+
+
+#ifdef DEBUG
+
+    /** A macro that null checks (X) iff DEBUG is defined */
+    #define UTIL_ASSERT_NOT_NULL_DEBUG(X)                                                          \
+        UTIL_ASSERT(::Util::Err::Null, (X) != nullptr, "Nullptr detected.");
+
+#else
+
+    /** A macro that nullchecks (X) iff DEBUG is defined */
+    #define UTIL_ASSERT_NOT_NULL_DEBUG(X)
+
+#endif
+
+#endif
