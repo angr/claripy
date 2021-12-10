0015572158cb82401117f301c9aaed2a2554fc20
zwimer <zwimer@gmail.com>
2021-12-10 04:26:45 -0700

    API::claricpp_has_exception update docs to reflect reality

--- native/src/api/general.h
+++ native/src/api/general.h
@@ -14,14 +14,14 @@
 void claricpp_init_for_python_usage();
 
 /** Returns true if and only if the previous API function failed with an exception
- *  If this fails, the program will crash as we have no way of functionally using claricpp
+ *  This function will not override the saved exception, on failure the program will crash
  *  @return true if and only if the previous API function failed with an exception
  */
 BOOL claricpp_has_exception();
 
 /** Returns the C++ exception Claricpp threw during the previous API function call
  *  This function should only be called if claricpp_has_exception() is true
- *  If this function fails, the failire exception is returned but the saved exception is not cleared
+ *  If this function fails, the failure exception is returned but the saved exception is not cleared
  *  @return The C++ exception Claricpp threw during the previous API function call
  */
 ClaricppException claricpp_get_exception();
