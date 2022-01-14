ee7cb001ae9634d352b7b856e3031137eff0d864
zwimer <zwimer@gmail.com>
2022-01-14 14:51:30 -0700

    Add type info python API

--- native/python/create.py
+++ native/python/create.py
@@ -12,23 +12,23 @@ from functools import cache, cached_property
 empty = AnnotationSPAV()
 
 
-def bool_s(name: bytes, spav = None):
+def bool_s(name: bytes, spav = None) -> Expr:
     raw = (empty if spav is None else spav).raw
     return Expr(claricpp.claricpp_create_symbol_bool(name, raw))
 
-def bv_s(name: bytes, bit_length: int, spav = None):
+def bv_s(name: bytes, bit_length: int, spav = None) -> Expr:
     raw = (empty if spav is None else spav).raw
     return Expr(claricpp.claricpp_create_symbol_bv(name, bit_length, raw))
 
-def fp_s(name: bytes, bit_length: int, spav = None):
+def fp_s(name: bytes, bit_length: int, spav = None) -> Expr:
     raw = (empty if spav is None else spav).raw
     return Expr(claricpp.claricpp_create_symbol_fp(name, bit_length, raw))
 
-def bool_v(value: bool, spav = None):
+def bool_v(value: bool, spav = None) -> Expr:
     raw = (empty if spav is None else spav).raw
     return Expr(claricpp.claricpp_create_literal_bool(value, raw))
 
-def bv_v(value: bytes, bit_length: int, spav = None):
+def bv_v(value: bytes, bit_length: int, spav = None) -> Expr:
     raw = (empty if spav is None else spav).raw
     if type(value) == bytes:
         expr = claricpp.claricpp_create_literal_bv_big_int_mode_str(value, bit_length, raw)
@@ -44,10 +44,10 @@ def bv_v(value: bytes, bit_length: int, spav = None):
         expr = claricpp.claricpp_create_literal_bv_big_int_mode_int(str(value).encode(), bit_length, raw)
     return Expr(expr)
 
-def fp_v(value: float, bit_length: int = 64, spav = None):
+def fp_v(value: float, bit_length: int = 64, spav = None) -> Expr:
     raw = (empty if spav is None else spav).raw
     return Expr(claricpp.claricpp_create_literal_fp(value, bit_length, raw))
 
-def if_(cond: Expr, left: Expr, right: Expr, spav = None):
+def if_(cond: Expr, left: Expr, right: Expr, spav = None) -> Expr:
     raw = (empty if spav is None else spav).raw
     return Expr(claricpp.claricpp_create_if(cond.raw, left.raw, right.raw, raw))
