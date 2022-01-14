8c87807125bd012ef22d4c5fc68bf453968e8d28
zwimer <zwimer@gmail.com>
2022-01-14 15:12:58 -0700

    Format python API via black + make test.py executable

--- native/python/test.py
+++ native/python/test.py
@@ -1,26 +1,28 @@
+#!/usr/bin/env python3
+
 from create import *
-from z3 import *
+from z3 import Z3
 
 
 try:
     tru = bool_v(True)
     bl = 32
     bv1 = bv_v(1, bl)
-    bv0 = bv_v(b'0', bl)
+    bv0 = bv_v(b"0", bl)
 
     ii = if_(tru, bv1, bv0)
 
     z3 = Z3()
     simp = z3.simplify(ii)
 
-    print('Start with: if(true) 1 else 0')
+    print("Start with: if(true) 1 else 0")
     print(repr(ii))
-    print('\nSimplify with z3:')
+    print("\nSimplify with z3:")
     print(repr(simp))
 
-    print('\nif args: ')
+    print("\nif args: ")
     print(ii.args)
-    print('\nSucess')
+    print("\nSucess")
 
 # except ClaricppException as e:
 #     print('\nCaught')
@@ -28,7 +30,7 @@ try:
 #     print('\nDone')
 
 finally:
-    print('\nEnd')
+    print("\nEnd")
 
 # import IPython
 # IPython.embed()
