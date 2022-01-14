4fb18d585bed63718c1f007b712cf850dbd29644
zwimer <zwimer@gmail.com>
2022-01-14 02:29:48 -0700

    cmake find_required_library

--- native/cmake/find_required.cmake
+++ native/cmake/find_required.cmake
@@ -1,6 +1,13 @@
 # This file defines find_required_program
 # This is the same as 3.18+'s find_program(REQUIRED)
 
+macro(find_required_library VARI FIRST)
+    find_library("${VARI}" NAMES "${FIRST}" ${ARGN})
+    if ("${${VARI}}" STREQUAL "${VARI}-NOTFOUND")
+        message(FATAL_ERROR "Could not find library: ${FIRST}" ${ARGN})
+    endif()
+endmacro()
+
 macro(find_required_program VARI FIRST)
     find_program("${VARI}" NAMES "${FIRST}" ${ARGN})
     if ("${${VARI}}" STREQUAL "${VARI}-NOTFOUND")
