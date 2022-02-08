143b31fab328677b3c2b7733e84b99da17dd8782
zwimer <zwimer@gmail.com>
2022-02-07 18:42:39 -0700

    Backward allow dynamic adding of source root paths

--- native/src/util/backtrace/backward.cpp
+++ native/src/util/backtrace/backward.cpp
@@ -11,15 +11,15 @@
 // @todo: Add segfault handling
 
 
-static inline void init() {
-#ifdef SOURCE_ROOT_FOR_BACKTRACE
-    backward::SourceFile::add_paths_to_env_variable_impl(SOURCE_ROOT_FOR_BACKTRACE);
-#endif
+void Util::Backtrace::backward_add_source_root(CCSC native) {
+    backward::SourceFile::add_paths_to_env_variable_impl(native);
 }
 
 void Util::Backtrace::backward(std::ostream &o, const UInt ignore_frames,
                                const int16_t max_frames) noexcept {
-    UTIL_DOONCE(init());
+#ifdef SOURCE_ROOT_FOR_BACKTRACE
+    UTIL_DOONCE(backward_add_source_root(SOURCE_ROOT_FOR_BACKTRACE));
+#endif
     namespace B = backward;
     // Backtrace
     B::StackTrace st;
--- native/src/util/backtrace/backward.hpp
+++ native/src/util/backtrace/backward.hpp
@@ -13,6 +13,11 @@
 
 namespace Util::Backtrace {
 
+    /** Add a root directory to look for source files in
+     *  This likely should be the path the `native` directory
+     */
+    void backward_add_source_root(CCSC native);
+
     /** Save a backtrace to o using Backward */
     void backward(std::ostream &o, const UInt ignore_frames = 0,
                   const int16_t max_frames = 0x1000) noexcept;
