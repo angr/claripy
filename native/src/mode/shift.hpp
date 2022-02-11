af569a95b1f15c27dba643660a0138653df460e6
zwimer <zwimer@gmail.com>
2022-02-11 13:45:39 -0700

    Shift mode print out name via << operator

--- native/src/mode/shift.hpp
+++ native/src/mode/shift.hpp
@@ -10,6 +10,20 @@ namespace Mode {
     /** A mask used to describe a type of bit shift */
     enum class Shift { Left, LogicalRight, ArithmeticRight };
 
+    /** Stream operator */
+    inline std::ostream &operator<<(std::ostream &os, const Shift &m) {
+        switch (m) {
+            case Shift::Left:
+                return os << "Left";
+            case Shift::LogicalRight:
+                return os << "Logical-Right";
+            case Shift::ArithmeticRight:
+                return os << "Arithmetic-Right";
+            default:
+                return os << "Invalid";
+        }
+    }
+
 } // namespace Mode
 
 #endif
