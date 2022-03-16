fb43125ed035fe309a8193962f930c80cba6dec6
zwimer <zwimer@gmail.com>
2022-03-15 18:01:43 -0700

    Change binder generation settings

---
+++ native/src/api/headers.hpp
@@ -0,0 +1,19 @@
+#ifndef R_API_HEADERS_HPP_
+#define R_API_HEADERS_HPP_
+
+#include "../annotation.hpp"
+#include "../backend.hpp"
+#include "../create.hpp"
+#include "../error.hpp"
+#include "../expr.hpp"
+#include "../mode.hpp"
+#include "../op.hpp"
+#include "../py_obj.hpp"
+
+/*
+ TODO: use pyrosetta stl bindings ?
+ In source: replace pybind11 stl bindings header with binder's
+ In config file: +binder std::vector binder::vector_binder
+*/
+
+#endif
