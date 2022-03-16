6db248b0f241a6ad508b3c35d126d4b4529ebc8d
Kevin Phoenix <kevin@kphoenix.us>
2022-03-15 18:24:44 -0700

    Add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 import os
 
-try:
-    from setuptools import setup
-    from setuptools import find_packages
-    packages = find_packages()
-except ImportError:
-    from distutils.core import setup
-    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
-
-if bytes is str:
-    raise Exception("This module is designed for python 3 only. Please install an older version to use python 2.")
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
     version = f.read().strip()
