6a8a90078f5af04b0afcc0c7e0ff68d74539870e
zwimer <zwimer@gmail.com>
2022-03-25 14:46:29 -0700

    Cross platform support tweak

--- native/src/mode/sign.hpp
+++ native/src/mode/sign.hpp
@@ -12,13 +12,15 @@ namespace Mode::Sign {
 
     /** An Sign enum with an underlying signed value where non-signed-ness is disallowed
      *  The underlying values of the enum values are 1 and -1
+     *  signed specifier is required for cross-platform support
      */
-    enum class FP : char { Minus = -1, Plus = 1 };
+    enum class FP : signed char { Minus = -1, Plus = 1 };
 
     /** An Sign enum with an underlying signed value where non-signed-ness is allowed
      *  The underlying values of the enum values are 1, -1, and 0
+     *  signed specifier is required for cross-platform support
      */
-    enum class Real : char { Minus = -1, None = 0, Plus = 1 };
+    enum class Real : signed char { Minus = -1, None = 0, Plus = 1 };
 
     /** Convert an FP to a Real */
     constexpr Real to_real(const FP f) { return static_cast<Real>(Util::to_underlying(f)); }
