1706f91b4ef6749658ea3495d0377e3bf1b40c42
zwimer <zwimer@gmail.com>
2022-03-30 13:33:38 -0700

    API constants

---
+++ native/src/api/constants.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @brief This file defines constants used throughout the API
+ */
+#ifndef R_API_CONSTANTS_HPP_
+#define R_API_CONSTANTS_HPP_
+
+#include <pybind11/pybind11.h>
+
+namespace API::Binder {
+    /** A copy of a typedef from autogen.cpp that is needed elsewhere */
+    using ModuleGetter = std::function<pybind11::module &(std::string const &)>;
+} // namespace API::Binder
+
+#endif
