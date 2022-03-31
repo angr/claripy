21724eae563af1761b1f15b0e3f79791c2452fe8
zwimer <zwimer@gmail.com>
2022-03-30 17:21:12 -0700

    Bug fix

---
+++ native/src/simplify/simplify.cpp
@@ -0,0 +1,3 @@
+#include "simplify.hpp"
+
+thread_local Simplify::Manager Simplify::manager {};
--- native/src/simplify/simplify.hpp
+++ native/src/simplify/simplify.hpp
@@ -10,15 +10,19 @@
 
 namespace Simplify {
     /** thread local Simplify Manager */
-    static thread_local Manager manager {};
+    extern thread_local Manager manager;
 
     /** Simplifies a given Expr */
-    inline Expr::BasePtr simplify(const Expr::BasePtr &e) { return manager.simplify(e); }
+    inline Expr::BasePtr simplify(const Expr::BasePtr &e) {
+        return manager.simplify(e);
+    }
 
     /** Cache a simplification result done outside of simplify()
      *  This will override any result previously stored by simplify
      **/
-    inline void cache(const Hash::Hash h, const Expr::BasePtr &e) { return manager.cache(h, e); }
+    inline void cache(const Hash::Hash h, const Expr::BasePtr &e) {
+        return manager.cache(h, e);
+    }
 
     static_assert(std::is_same_v<Func, decltype(simplify)>, "simplify of wrong type");
 } // namespace Simplify
