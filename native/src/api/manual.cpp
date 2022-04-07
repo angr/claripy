8b01e2d967bf88ed88c088c0acb74050b59adacb
zwimer <zwimer@gmail.com>
2022-04-07 11:25:02 -0700

    Log info at exit

--- native/src/api/manual.cpp
+++ native/src/api/manual.cpp
@@ -4,10 +4,26 @@
 #include "log.hpp"
 #include "simplify.hpp"
 
+#include "../util.hpp"
+
+
+/** Safely log a message */
+static inline void slog(CCSC msg) noexcept {
+    try {
+        Util::Log::info(msg);
+    }
+    catch (...) {
+        UTIL_NEW_FALLBACK_ERROR_LOG("Logging failed. Message: ").log(msg);
+    }
+}
 
 void API::bind_manual(pybind11::module_ &root_module, Binder::ModuleGetter &m) {
+    register_at_exit([]() noexcept {
+        slog("C++/python decoupled; finalizers now safe to run");
+    }); // TODO: make custom function so orders can be set?
     API::bind_exceptions(root_module);
     API::bind_simplify_init(m);
     API::bind_log_init(m);
+    register_at_exit([]() noexcept { slog("Running C++ exit functions to decouple from python"); });
     // TODO: python operators; this should probably be fixed in python, not C++
 }
