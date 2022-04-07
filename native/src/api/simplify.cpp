23c3b6ecba38278cb1fd0cbb9dd4590dac2a97f6
zwimer <zwimer@gmail.com>
2022-04-07 11:24:03 -0700

    rm simplify callback at exit

--- native/src/api/simplify.cpp
+++ native/src/api/simplify.cpp
@@ -5,7 +5,13 @@
 #include <pybind11/functional.h>
 
 
+/** Clear the stored python simplifier */
+static void clear_py_simp() noexcept {
+    Simplify::manager.set_python_simplifier(nullptr); // nullptr_t override is noexcept
+}
+
 void API::bind_simplify_init(API::Binder::ModuleGetter &m) {
+    register_at_exit(clear_py_simp);
     m("API").def(
         "set_python_simplifier",
         [](const std::function<Simplify::Func> &s) { Simplify::manager.set_python_simplifier(s); },
