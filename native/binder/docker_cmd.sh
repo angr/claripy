52f04cecbcdab1923a75c0123dcab26c465108a2
zwimer <zwimer@gmail.com>
2022-04-11 19:07:56 -0700

    Binder automation

---
+++ native/binder/docker_cmd.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eux
+
+# Args
+DEBUG_MODE="${1}"
+Z3_HEADERS="${2}"
+NATIVE="${3}"
+OUTD="${4}"
+
+# Bindings
+API_D="${NATIVE}/src/api/"
+/usr/local/bin/binder \
+	--config="${NATIVE}/src/api/clari.conf"  \
+	\
+	-v \
+	--root-module clari \
+	--prefix "${OUTD}"  \
+	"${API_D}/headers.hpp" \
+	-- \
+	-isystem "${Z3_HEADERS}" \
+	-isystem "${NATIVE}/boost/" \
+	-isystem "${NATIVE}/gmp/include/" \
+	-isystem "${NATIVE}/backward-cpp" \
+	\
+	-DDEBUG="${DEBUG_MODE}" \
+	-DDEBUGMODE="${DEBUG_MODE}" \
+	\
+	-ffile-prefix-map="${NATIVE}/"=/ \
+	-Werror \
+	-Wno-error=cpp \
+	-std=c++17
+
---
+++ native/binder/gen-bindings.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -eux
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Checks
+which realpath > /dev/null
+which python3 > /dev/null
+
+# Config
+NATIVE="$(realpath "${MY_DIR}/..")"
+CMD_SH="${MY_DIR}/docker_cmd.sh"
+BINDER="${NATIVE}/src/api/binder"
+Z3_HEADERS="$(python3 -c 'import z3, os; print(os.path.dirname(z3.__file__))')/include"
+
+# Prep
+echo "Removing old bindings"
+rm -rf "${BINDER}" || true
+mkdir "${BINDER}"
+
+# Gen
+echo "Creating new bindings"
+DOCKER_CMD_SH="/cmd.sh"
+DOCKER_Z3_HEADERS="/z3-headers"
+docker run --rm \
+	`# Mount in everything` \
+	-v "${CMD_SH}:${DOCKER_CMD_SH}:ro" \
+	-v "${Z3_HEADERS}:/${DOCKER_Z3_HEADERS}:ro" \
+	-v "${NATIVE}:${NATIVE}:ro" \
+	-v "${BINDER}:${BINDER}" \
+ 	-it "zwimer/binder:14" \
+	`# Run binder` \
+	"${DOCKER_CMD_SH}" \
+	0 "${DOCKER_Z3_HEADERS}" "${NATIVE}" "${BINDER}"
+echo "Bindings generated"
