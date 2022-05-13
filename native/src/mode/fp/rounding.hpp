81291e95776ba00f9317faff5e5ef7f4c85d1264
zwimer <zwimer@gmail.com>
2022-05-12 18:59:46 -0700

    rounding << operator

--- native/src/mode/fp/rounding.hpp
+++ native/src/mode/fp/rounding.hpp
@@ -5,6 +5,10 @@
 #ifndef R_SRC_MODE_FP_ROUNDING_HPP_
 #define R_SRC_MODE_FP_ROUNDING_HPP_
 
+#include "../../util.hpp"
+
+#include <ostream>
+
 namespace Mode::FP {
     /** FP modes supported by claripy */
     enum class Rounding {
@@ -14,6 +18,10 @@ namespace Mode::FP {
         TowardsPositiveInf,
         TowardsNegativeInf
     };
+    /** Rounding mode stream operator */
+    inline std::ostream &operator<<(std::ostream &o, const Rounding &r) {
+        return o << Util::to_underlying(r);
+    }
 } // namespace Mode::FP
 
 #endif
