94e9cd0dd2c73331f1fef2a28948d81ff2b03329
zwimer <zwimer@gmail.com>
2022-05-16 18:32:20 -0700

    More integration of clari

--- claripy/native/legacy_module.py
+++ claripy/native/legacy_module.py
@@ -4,17 +4,31 @@ from collections import defaultdict
 from enum import Enum
 import re
 
+# Note: we do not make things on import!
+# LegacySetup may change functions!
+# We instead expose an init function
+
+def _bvv_wrapper(a1, bl, *args):
+    a1 %= (2**bl) # TODO: maybe move to C++?
+    if a1 >= 2**64:
+        a1 = str(a1)
+    return clari.Create.literal_bv(a1, bl, *args)
+
 
 def _gen_ctor_type_map():
     Create = clari.Create
     Op = clari.Op
     m = {  # Non trivial translations
-        # TODO: populate
         "ZeroExt": (Create.zero_ext, Op.ZeroExt,),
         "SignExt": (Create.sign_ext, Op.SignExt,),
         "LShR": (Create.shift_logical_right, Op.ShiftLogicalRight,),
         "__rshift__": (Create.shift_arithmetic_right, Op.ShiftArithmeticRight,),
         "__lshift__": (Create.shift_l, Op.ShiftLeft,),
+
+        "fpToUBV": (Create.FP.to_bv_unsigned, Op.FP.ToBVUnsigned),
+        "fpToIEEEBV": (Create.FP.to_ieee_bv, Op.FP.ToIEEEBV),
+
+        "IntToStr": (Create.String.from_int, Op.String.FromInt,),
     }
     # Types
     types = ["Bool", "BV", "FP", "String", "VS"]
@@ -22,13 +36,11 @@ def _gen_ctor_type_map():
     get_func = lambda t, p: (getattr(Create, fname(p, t)), getattr(Op, p))
     m.update({(i + "V"): get_func(i, "Literal") for i in types})
     m.update({(i + "S"): get_func(i, "Symbol") for i in types})
+    m["BVV"] = (_bvv_wrapper, m["BVV"][1])
     return m
-
-_ctor_map = _gen_ctor_type_map()
 
 # Trivial lookups can work if the name is tweaked
 _conv_map = {
-    # TODO: populate
     "__ne__": "neq",
     "__mod__": "UMod",
     "__floordiv__": "UDiv",
@@ -36,6 +48,16 @@ _conv_map = {
     "__ge__": "UGE",
     "__lt__": "ULT",
     "__le__": "ULE",
+
+    "fpIsNaN": "fpIs_nan",
+
+    "StrConcat": "Concat",
+    "StrIndexOf": "StrIndex_Of",
+    "StrIsDigit": "StrIs_Digit",
+    "StrPrefixOf": "StrPrefix_Of",
+    "StrSuffixOf": "StrSuffix_Of",
+    "StrToInt": "StrTo_Int",
+    "StrSubstr": "StrSub_string",
 }
 
 def _gen_op_map():
@@ -56,11 +78,19 @@ def _gen_op_map():
     op_m.update(op_to_py_op)
     return op_m
 
-_op_map = _gen_op_map()
+def _bimap(*x):
+    return {i: k for i, k in x}, {k: i for i, k in x},
 
-# Exports
+def _gen_rms():
+    return _bimap(
+        ('RM_RNE', clari.Mode.FP.Rounding.NearestTiesEven,),
+        ('RM_RNA', clari.Mode.FP.Rounding.NearestTiesAwayFromZero,),
+        ('RM_RTZ', clari.Mode.FP.Rounding.TowardsZero,),
+        ('RM_RTP', clari.Mode.FP.Rounding.TowardsPositiveInf,),
+        ('RM_RTN', clari.Mode.FP.Rounding.TowardsNegativeInf,),
+    )
 
-def py_op_to_ctor_type(real_py_op):
+def _py_op_to_ctor_type(real_py_op):
     if real_py_op in _ctor_map:
         return _ctor_map[real_py_op]
     py_op = _conv_map[real_py_op] if real_py_op in _conv_map else real_py_op
@@ -86,27 +116,45 @@ def py_op_to_ctor_type(real_py_op):
             if hasattr(Create, fn):
                 return getattr(Create, fn)
     # Wrapper
-    def _try_ns(py_op, x):
+    def _try_ns(py_op, x, ns):
         if not py_op.startswith(x):
             return None, None
-        c = getattr(clari.Create, x.upper())
-        o = getattr(clari.Op, x.upper())
+        c = getattr(clari.Create, ns)
+        o = getattr(clari.Op, ns)
         r = _f(py_op, c), _t(py_op, o)
         if not all(r):
             r = _f(py_op[len(x):], c), _t(py_op[len(x):], o)
         return r
 
     # Try namespaces
-    r = _try_ns(py_op, "fp")
+    r = _try_ns(py_op, "fp", "FP")
     if all(r):
         return r
-    r = _try_ns(py_op, "string")
+    r = _try_ns(py_op, "Str", "String")
+    # r = _try_ns(py_op, "string")
     if all(r):
         return r
     r = _f(py_op, clari.Create), _t(py_op, clari.Op)
     if not all(r):
         raise RuntimeError(f"Could not translate op {real_py_op} to C++")
     return r
+
+_ctor_map = None
+_op_map = None
+_rms = None
+
+# Exports
+
+def conv_rm(x):
+    return _rms[1 if isinstance(x, clari.Mode.FP.Rounding) else 0][x]
+
+def init():
+    global _op_map
+    global _ctor_map
+    global _rms
+    _op_map = _gen_op_map()
+    _ctor_map = _gen_ctor_type_map()
+    _rms = _gen_rms()
 
 
 def expr_op_type_pair_to_py_op(expr_t, op_t):
@@ -119,16 +167,21 @@ def expr_to_py_op(expr):
     return expr_op_type_pair_to_py_op(type(expr), type(expr.op))
 
 
-from_rm = {
-    'RM_RNE' : clari.Mode.FP.Rounding.NearestTiesEven,
-    'RM_RNA' : clari.Mode.FP.Rounding.NearestTiesAwayFromZero,
-    'RM_RTZ' : clari.Mode.FP.Rounding.TowardsZero,
-    'RM_RTP' : clari.Mode.FP.Rounding.TowardsPositiveInf,
-    'RM_RTN' : clari.Mode.FP.Rounding.TowardsNegativeInf,
-}
-
 def create(op, args, *, length=None):
-    ctor, t = py_op_to_ctor_type(op)
+    # Get ctor and type
+    if op == "fpToFP":
+        if len(args) == 2:
+            ctor = clari.Create.FP.from_not_2s_complement_bv
+            t = clari.Op.FP.FromNot2sComplementBV
+        elif hasattr(args[1], 'sort'):
+            ctor = clari.Create.FP.from_fp
+            t = clari.Op.FP.FromFP
+        else:
+            ctor = clari.Create.FP.from_2s_complement_signed
+            t = clari.Op.FP.From2sComplementSigned
+    else:
+        ctor, t = _py_op_to_ctor_type(op)
+
     # Fix args
     if op == "BoolS":
         args = (args[0],)
@@ -143,19 +196,19 @@ def create(op, args, *, length=None):
             try:
                 return arg._native # @property might create attr, no hasattr check
             except AttributeError:
-                if issubclass(type(arg), Enum):
-                    return from_rm[arg.value]
+                if isinstance(arg, Enum):
+                    return conv_rm(arg.value)
                 return arg
 
         args = [fx(i) for i in args]
     # Order args
     if issubclass(t, clari.Op.AbstractFlat):
         args = (args,)
-    elif issubclass(t, clari.Op.UIntBinary):
+    elif issubclass(t, clari.Op.UIntBinary) and not op.startswith("Str"):
         args = args[::-1]
     elif issubclass(t, clari.Op.FP.ModeBinary):
         args = args[1:] + [args[0]]
     # Call ctor:
     return ctor(*args)
 
-__all__ = ("py_op_to_ctor_type", "expr_op_type_pair_to_py_op", "expr_to_py_op", "from_rm",)
+__all__ = ("init", "expr_op_type_pair_to_py_op", "expr_to_py_op", "conv_rm",)
--- claripy/native/legacy_setup.py
+++ claripy/native/legacy_setup.py
@@ -11,34 +11,26 @@ class LegacySetup(Setup):
         super().__init__(debug_mode=debug_mode)
 
     def define_members(self):
+        # Fix wonky
+        old_ls = clari.Create.literal_string
+        clari.Create.literal_string = lambda s, l: old_ls(s.ljust(l, "\0"))
+        old_u = clari.Create.union_
+        clari.Create.union_ = lambda *args: old_u(*(args[0] if len(args) == 1 else args))
+        # TODO: ^ check with fish
         super().define_members()
         clari.Legacy = legacy_module
         self._enable_args()
+        # Init the module
+        clari.Legacy.init()
+        del clari.Legacy.init
 
     @staticmethod
-    def _fix_arg(arg):
-        if type(arg) is clari.BigInt:
-            return int(arg.value)
-        return arg
-
-    @classmethod
-    def _enable_args(cls):
+    def _enable_args():
+        fix = lambda x: int(x.value) if isinstance(x, clari.BigInt) else x
         def _args(self):
             if self not in self._args_dict:  # Cache check
-                lst = [ cls._fix_arg(i) for i in self.op.python_children() ]
-                # Add extra size arg if needed
-                if type(self.op) is clari.Op.Literal:
-                    if type(self) is clari.Expr.FP:
-                        lst.append(FSORT_DOUBLE if len(self) == 64 else FSORT_FLOAT)
-                    elif type(self) is not clari.Expr.Bool:
-                        lst.append(len(self))
-                elif type(self.op) is clari.Op.Symbol:
-                    if type(self) is clari.Expr.String:
-                        lst.append(self._uninitialized)
-                    elif type(self) is clari.Expr.FP:
-                        lst.append(FSORT_DOUBLE if len(self) == 64 else FSORT_FLOAT)
-                # Save result
-                self._args_dict[self] = tuple(lst)
+                args = tuple([fix(i) for i in self.op.python_children()])
+                self._args_dict[self] = args
             return self._args_dict[self]
 
         # Claricpp objects are generally readonly, so we map weakrefs to cached vars
@@ -46,5 +38,3 @@ class LegacySetup(Setup):
         clari.Expr.Base.args = property(_args)
 
 __all__ = ('LegacySetup', 'clari',)
-
-from ..fp import FSORT_DOUBLE, FSORT_FLOAT
