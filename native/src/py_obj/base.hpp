8f19344166a09fa25835a7b93137f66003bcaa89
zwimer <zwimer@gmail.com>
2022-05-24 17:18:04 -0700

    py_obj refactor

--- native/src/py_obj/base.hpp
+++ native/src/py_obj/base.hpp
@@ -5,6 +5,8 @@
 #ifndef R_SRC_PYOBJ_BASE_HPP_
 #define R_SRC_PYOBJ_BASE_HPP_
 
+#include "../bit_length.hpp"
+#include "../has_repr.hpp"
 #include "../hash.hpp"
 #include "../macros.hpp"
 
@@ -16,7 +18,11 @@ namespace PyObj {
     /** A class containing a ref to some python object and a hash
      *  Warning: No virtual destructor; do *not* delete by base class pointer; avoid slicing!
      */
-    struct Base : public Hash::Hashed {
+    class Base : public Hash::Hashed, public HasRepr<Base> {
+      public:
+        /** Override repr stream */
+        virtual inline void repr_stream(std::ostream &o) const final { o << actual_repr(); }
+
       protected:
         /** Inherit constructor */
         using Hashed::Hashed;
@@ -24,6 +30,22 @@ namespace PyObj {
         inline ~Base() noexcept = default;
         // Rule of 5
         DEFINE_IMPLICITS_CONST_MEMBERS_ALL_NOEXCEPT(Base);
+        /** Call repr on the python object */
+        virtual std::string actual_repr() const = 0;
+    };
+
+    /** A sized python object
+     *  Warning: No virtual destructor; do *not* delete by base class pointer; avoid slicing!
+     */
+    class Sized : public Base, public BitLength {
+      protected:
+        /** Constructor */
+        explicit constexpr Sized(const Hash::Hash h, const U64 bl) noexcept
+            : Base { h }, BitLength { bl } {}
+        /** Protected destructor to prevent most slicing */
+        inline ~Sized() noexcept = default;
+        // Rule of 5
+        DEFINE_IMPLICITS_CONST_MEMBERS_ALL_NOEXCEPT(Sized);
     };
 
 } // namespace PyObj
--- native/src/py_obj/trivial.hpp
+++ native/src/py_obj/trivial.hpp
@@ -7,60 +7,37 @@
 
 #include "base.hpp"
 
-#include "../bit_length.hpp"
-
-
-// Macros
-
-
-#define M_NEW_PYOBJ_CLS(NAME)                                                                      \
-    struct NAME final : public Base, public BitLength {                                            \
-        CUID_DEFINE_MAYBE_UNUSED                                                                   \
-        /** Shared pointer to a const *this */                                                     \
-        using Ptr = std::shared_ptr<const NAME>;                                                   \
-        /** A non-templated public constructor (can be bound by API) */                            \
-        static inline Ptr factory(const Hash::Hash &h, const U64 bl) {                             \
-            return Ptr { new NAME { h, bl } };                                                     \
-        }                                                                                          \
-                                                                                                   \
-      private:                                                                                     \
-        /** Constructor */                                                                         \
-        explicit constexpr NAME(const Hash::Hash &h, const U64 bl) noexcept                        \
-            : Base { h }, BitLength { bl } {}                                                      \
-    };
-
-#define M_NEW_PYOBJ_FUNCS(NAME)                                                                    \
-    /** Equality operator */                                                                       \
-    constexpr bool operator==(const NAME &a, const NAME &b) {                                      \
-        return a.hash == b.hash;                                                                   \
-    };                                                                                             \
-    /** Not-equality operator */                                                                   \
-    constexpr bool operator!=(const NAME &a, const NAME &b) {                                      \
-        return not(a == b);                                                                        \
-    }                                                                                              \
-    /** Stream operator */                                                                         \
-    inline std::ostream &operator<<(std::ostream &os, const NAME &obj) {                           \
-        return os << #NAME "{hash = " << obj.hash << ", bit_length = " << obj.bit_length << "}";   \
-    }
 
 #define M_NEW_PYOBJ(NAME)                                                                          \
-    M_NEW_PYOBJ_CLS(NAME);                                                                         \
-    M_NEW_PYOBJ_FUNCS(NAME)
-
-
-// Non-macros
-
+    class NAME : public Sized {                                                                    \
+      public:                                                                                      \
+        CUID_DEFINE_MAYBE_UNUSED                                                                   \
+        using Ptr = std::shared_ptr<const NAME>;                                                   \
+                                                                                                   \
+      protected:                                                                                   \
+        /** Constructor */                                                                         \
+        using Sized::Sized;                                                                        \
+        /** Protected destructor to avoid slicing */                                               \
+        virtual inline ~NAME() noexcept = default;                                                 \
+        /* Rule of 5 */                                                                            \
+        DEFINE_IMPLICITS_CONST_MEMBERS_ALL_NOEXCEPT(NAME);                                         \
+    };
 
 namespace PyObj {
-    /** A VS PyObj for VSs */
+    /** A VS PyObj for VSs
+     *  Warning: Exactly one class should ever subclass this
+     *  We give this abstract object a CUID, it should be for the subclass
+     *  We do this so that the API can subclass this to hook it with python stuff
+     */
     M_NEW_PYOBJ(VSVS);
-    /** A VS PyObj for BVs */
+    /** A VS PyObj for BVs
+     *  Warning: Exactly one class should ever subclass this
+     *  We give this abstract object a CUID, it should be for the subclass
+     *  We do this so that the API can subclass this to hook it with python stuff
+     */
     M_NEW_PYOBJ(BVVS);
 } // namespace PyObj
 
-
 #undef M_NEW_PYOBJ
-#undef M_NEW_PYOBJ_CLS
-#undef M_NEW_PYOBJ_FUNCS
 
 #endif
