15cf64e19342b45214b0019985903615f68daf37
zwimer <zwimer@gmail.com>
2022-11-01 14:30:43 -0700

    test case improved

--- tests/test_z3.py
+++ tests/test_z3.py
@@ -14,8 +14,15 @@ class TestZ3(unittest.TestCase):
         assert z.max(x, solver=s) == rng[1]
 
         for i in range(rng[0], rng[1] + 1):
+            # ==
             assert z.min(x, solver=s, extra_constraints=(x == i,)) == i
             assert z.max(x, solver=s, extra_constraints=(x == i,)) == i
+            # <=
+            assert z.min(x, solver=s, extra_constraints=(x <= i,)) == rng[0]
+            assert z.max(x, solver=s, extra_constraints=(x <= i,)) == i
+            # >=
+            assert z.min(x, solver=s, extra_constraints=(x >= i,)) == i
+            assert z.max(x, solver=s, extra_constraints=(x >= i,)) == rng[1]
 
 if __name__ == '__main__':
     unittest.main()
