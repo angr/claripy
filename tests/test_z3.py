b279b77fa2b08b23fcfd8eb3dfcfc1847eb3c57f
Zachary Wimer <zwimer@users.noreply.github.com>
2022-11-10 15:18:04 -0700

    Condense min/max into singular function (#300)
    * Condense min/max into singular function + bug fix max using GT instead of GE
    
    * Simplify constraints
    
    * Make more pythonic + cleanup + lint

---
+++ tests/test_z3.py
@@ -0,0 +1,37 @@
+import unittest
+import claripy
+
+
+class TestZ3(unittest.TestCase):
+    """
+    A class used for testing z3
+    """
+    @staticmethod
+    def test_extrema():
+        """
+        Test the _extrema function within the z3 backend
+        """
+        z = claripy.backend_manager.backends.z3
+
+        s = z.solver()
+        x = claripy.BVS("x", 8)
+
+        rng = (0, 2**x.length - 1,)
+        assert z.satisfiable(solver=s)
+        assert z.min(x, solver=s) == rng[0]
+        assert z.max(x, solver=s) == rng[1]
+
+        for i in range(rng[0], rng[1] + 1):
+            # ==
+            assert z.min(x, solver=s, extra_constraints=(x == i,)) == i
+            assert z.max(x, solver=s, extra_constraints=(x == i,)) == i
+            # <=
+            assert z.min(x, solver=s, extra_constraints=(x <= i,)) == rng[0]
+            assert z.max(x, solver=s, extra_constraints=(x <= i,)) == i
+            # >=
+            assert z.min(x, solver=s, extra_constraints=(x >= i,)) == i
+            assert z.max(x, solver=s, extra_constraints=(x >= i,)) == rng[1]
+
+
+if __name__ == '__main__':
+    unittest.main()
