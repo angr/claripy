c13d3e4f17b822450e71038cb671565ef178b0d7
hwu71 <hwu71@binghamton.edu>
2023-06-23 23:08:12 -0400

    Fix a bug about ret_size in Func and MemoryLoad

--- claripy/ast/func.py
+++ claripy/ast/func.py
@@ -10,12 +10,12 @@ class Func(Base):
 
     def __init__(self, *args, **kwargs):
         #super().__init__(*args, **kwargs)
-        self.ret_size = self._ret_size if self._ret_size is not None else 32
+        self._ret_size = kwargs['_ret_size'] if '_ret_size' in kwargs else 32
 
         self.func_name = self.op
         self.args = self.args
         args_type = [type(arg) for arg in self.args]
-        self.func_op = operations.op(self.func_name, args_type, BV, bound=False, calc_length=lambda *a: self.ret_size)
+        self.func_op = operations.op(self.func_name, args_type, BV, bound=False, calc_length=lambda *a: self._ret_size)
         #operations.backend_func_operations.add(self.op)
 
     def get_func_name(self):
@@ -26,6 +26,6 @@ class Func(Base):
 
 class MemoryLoad(Base):
     def __init__(self,  *args, **kwargs):
-        self.ret_size = self._ret_size if self._ret_size is not None else 32
+        self._ret_size = kwargs['_ret_size'] if '_ret_size' in kwargs else 32
         self.args = self.args
-        self.op = operations.op('MemoryLoad', BV, BV, bound=False, calc_length=lambda *a: self.ret_size)
+        self.op = operations.op('MemoryLoad', BV, BV, bound=False, calc_length=lambda *a: self._ret_size)
