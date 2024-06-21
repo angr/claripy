e484bdbe239a34cac28cb89212b7f6cd8a98a975
Kevin Phoenix <kevin@kphoenix.us>
2024-06-20 18:12:59 -0700

    Move other Base members to rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-use std::collections::HashSet;
-
 use pyo3::{prelude::*, types::PyTuple};
 
-#[pyclass(subclass)]
+#[pyclass(subclass, weakref)]
 struct Base {
+    // Hashcons
     #[pyo3(get, set)]
     op: String,
     #[pyo3(get, set)]
@@ -16,6 +15,38 @@ struct Base {
     symbolic: bool,
     #[pyo3(get, set)]
     annotations: Py<PyTuple>,
+
+    // Not Hashcons
+    #[pyo3(get, set)]
+    simplifiable: Option<PyObject>,
+    #[pyo3(get, set)]
+    depth: Option<PyObject>,
+
+    #[pyo3(get, set)]
+    _hash: Option<PyObject>,
+    #[pyo3(get, set)]
+    _simplified: Option<PyObject>,
+    #[pyo3(get, set)]
+    _cache_key: Option<PyObject>,
+    #[pyo3(get, set)]
+    _cached_encoded_name: Option<PyObject>,
+    #[pyo3(get, set)]
+    _errored: Option<PyObject>,
+    #[pyo3(get, set)]
+    _eager_backends: Option<PyObject>,
+    #[pyo3(get, set)]
+    _excavated: Option<PyObject>,
+    #[pyo3(get, set)]
+    _burrowed: Option<PyObject>,
+    #[pyo3(get, set)]
+    _uninitialized: Option<PyObject>,
+    #[pyo3(get, set)]
+    _uc_alloc_depth: Option<PyObject>,
+    #[pyo3(get, set)]
+    _uneliminatable_annotations: Option<PyObject>,
+    #[pyo3(get, set)]
+    _relocatable_annotations: Option<PyObject>,
+
 }
 
 #[pymethods]
@@ -23,6 +54,7 @@ impl Base {
     #[new]
     #[pyo3(signature = (op, args, length, variables, symbolic, annotations))]
     fn new(
+        py: Python,
         op: String,
         args: Py<PyTuple>,
         length: PyObject,
@@ -37,6 +69,22 @@ impl Base {
             variables,
             symbolic,
             annotations,
+
+            simplifiable: None,
+            depth: None,
+
+            _hash: None,
+            _simplified: None,
+            _cache_key: None,
+            _cached_encoded_name: None,
+            _errored: None,
+            _eager_backends: None,
+            _excavated: None,
+            _burrowed: None,
+            _uninitialized: None,
+            _uc_alloc_depth: None,
+            _uneliminatable_annotations: None,
+            _relocatable_annotations: None,
         })
     }
 }
