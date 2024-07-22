86ea511ffd2486e0cd7b56e0bfea0c8f5957dd8c
Kevin Phoenix <kevin@kphoenix.us>
2024-07-22 16:56:17 -0700

    Remove leftover reference in __all__

--- claripy/backends/__init__.py
+++ claripy/backends/__init__.py
@@ -2,7 +2,4 @@ from .backend_z3 import BackendZ3
 from .backend_concrete import BackendConcrete
 from .backend_vsa import BackendVSA
 
-# If you need support for multiple solvers, please import claripy.backends.backend_smtlib_solvers by yourself
-# from .backend_smtlib_solvers import *
-
-__all__ = ["BackendZ3", "BackendZ3Parallel", "BackendConcrete", "BackendVSA"]
+__all__ = ["BackendZ3", "BackendConcrete", "BackendVSA"]
