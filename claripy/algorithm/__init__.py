864a23abf5aa023c282a52aeb6021d9870628263
Kevin Phoenix <kevin@kphoenix.us>
2024-10-03 12:27:26 -0700

    Move is_true and is_false to algorithm sub-package

--- claripy/algorithm/__init__.py
+++ claripy/algorithm/__init__.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
+from .bool_check import is_false, is_true
 from .simplify import simplify
 
-__all__ = ("simplify",)
+__all__ = (
+    "is_false",
+    "is_true",
+    "simplify",
+)
